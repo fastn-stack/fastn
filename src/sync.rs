ceb517d5aa079470d9e42602dca8fae7a22f29df
Arpita Jaiswal <ayushipujaa@gmail.com>
2021-12-03 13:02:04 +0530

    fpm sync 2 (#3)
    * working
    
    * changed message
    
    * not sure of this commit coz if new snapshot is not created for file, should we display it on fpm sync command run? This commit shows all files nonetheless
    
    * removed display for non modified files and skipped 05-fpm-dependency test
    
    * message changed and 02-hello-sync -> 06-hello-sync and 03-nested-document-sync -> 07-nested-document-sync
    
    * review changes
    
    * remove regex
    
    Co-authored-by: Amit Upadhyay <upadhyay@gmail.com>

--- src/sync.rs
+++ src/sync.rs
@@ -1,26 +1,76 @@
 pub async fn sync() {
-    let (_fpm_config, base_dir) = fpm::check().await;
+    let (fpm_config, base_dir) = fpm::check().await;
 
     std::fs::create_dir_all(format!("{}/.history", base_dir.as_str()).as_str())
         .expect("failed to create build folder");
 
     let timestamp = fpm::get_timestamp_nanosecond();
+
+    let mut modified_files = vec![];
     for doc in fpm::process_dir(base_dir.clone(), 0, base_dir) {
-        write(&doc, timestamp);
+        if let Some(file) = write(&doc, timestamp) {
+            modified_files.push(file);
+        }
+    }
+    if modified_files.is_empty() {
+        println!("Everything is upto date.");
+    } else {
+        println!(
+            "Repo for {} is github, directly syncing with .history.",
+            fpm_config.name
+        );
+        for file in modified_files {
+            println!("{}", file);
+        }
     }
 }
 
-fn write(doc: &fpm::Document, timestamp: u128) {
+fn write(doc: &fpm::Document, timestamp: u128) -> Option<String> {
     use std::io::Write;
 
     if doc.id.starts_with(".history") {
-        return;
+        return None;
     }
 
-    if doc.id.contains('/') {
-        let (dir, _) = doc.id.rsplit_once('/').unwrap();
+    let (path, doc_name) = if doc.id.contains('/') {
+        let (dir, doc_name) = doc.id.rsplit_once('/').unwrap();
         std::fs::create_dir_all(format!("{}/.history/{}", doc.base_path.as_str(), dir))
             .expect("failed to create directory folder for doc");
+        (
+            format!("{}/.history/{}", doc.base_path.as_str(), dir),
+            doc_name.to_string(),
+        )
+    } else {
+        (
+            format!("{}/.history", doc.base_path.as_str()),
+            doc.id.to_string(),
+        )
+    };
+
+    let files = std::fs::read_dir(&path).expect("Panic! Unable to process the directory");
+
+    let mut max_timestamp: Option<(String, String)> = None;
+    for n in files.flatten() {
+        let p = format!("{}/{}.", path, doc_name.replace(".ftd", ""));
+        let file = n.path().to_str().unwrap().to_string();
+        if file.starts_with(&p) {
+            let timestamp = file
+                .replace(&format!("{}/{}.", path, doc_name.replace(".ftd", "")), "")
+                .replace(".ftd", "");
+            if let Some((t, _)) = &max_timestamp {
+                if *t > timestamp {
+                    continue;
+                }
+            }
+            max_timestamp = Some((timestamp, file.to_string()));
+        }
+    }
+
+    if let Some((_, path)) = max_timestamp {
+        let existing_doc = std::fs::read_to_string(&path).expect("cant read file");
+        if doc.document.eq(&existing_doc) {
+            return None;
+        }
     }
 
     let new_file_path = format!(
@@ -33,8 +83,5 @@ fn write(doc: &fpm::Document, timestamp: u128) {
 
     f.write_all(doc.document.as_bytes())
         .expect("failed to write to .html file");
-    println!(
-        "Generated history [{}]",
-        format!("{}/{}", doc.base_path, doc.id)
-    );
+    Some(doc.id.to_string())
 }
