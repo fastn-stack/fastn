d3b23a98d38be0ae42047eb9c9127be5566974a6
Amit Upadhyay <upadhyay@gmail.com>
2021-12-12 19:01:12 +0530

    Dependency::process() should work from any folder now

--- src/dependency.rs
+++ src/dependency.rs
@@ -14,31 +14,34 @@ impl Dependency {
     pub async fn process(&self, base_dir: camino::Utf8PathBuf) -> fpm::Result<()> {
         use tokio::io::AsyncWriteExt;
         if base_dir.join(".packages").join(self.name.as_str()).exists() {
+            // TODO: in future we will check if we have a new version in the package's repo
+            //       for now assume if package exists we have the package. for now if you
+            //       want to update a package, delete the corresponding folder and latest
+            //       version will get downloaded.
             return Ok(());
         }
 
-        let download_url = match self.repo.as_str() {
-            "github" => {
-                format!(
-                    "https://github.com/{}/archive/refs/heads/main.zip",
-                    self.name
-                )
-            }
-            k => k.to_string(),
-        };
-
-        let response = reqwest::get(download_url).await?;
-
-        let download_path = format!("/tmp/{}.zip", self.name.replace("/", "__"));
-        let path = std::path::Path::new(download_path.as_str());
+        let path = camino::Utf8PathBuf::from(format!("/tmp/{}.zip", self.name.replace("/", "__")));
 
         {
+            let download_url = match self.repo.as_str() {
+                "github" => {
+                    format!(
+                        "https://github.com/{}/archive/refs/heads/main.zip",
+                        self.name
+                    )
+                }
+                k => k.to_string(),
+            };
+            let response = reqwest::get(download_url).await?;
             let mut file = tokio::fs::File::create(&path).await?;
+            // TODO: instead of reading the whole thing in memory use tokio::io::copy() somehow?
             let content = response.bytes().await?;
             file.write_all(&content).await?;
         }
 
         let file = std::fs::File::open(&path)?;
+        // TODO: switch to async_zip crate
         let mut archive = zip::ZipArchive::new(file)?;
         for i in 0..archive.len() {
             let mut c_file = archive.by_index(i).unwrap();
@@ -47,24 +50,20 @@ impl Dependency {
                 None => continue,
             };
             let out_path_without_folder = out_path.to_str().unwrap().split_once("/").unwrap().1;
-            let new_out_path = format!("{}/{}", self.name, out_path_without_folder);
-            let file_extract_path = std::path::Path::new(new_out_path.as_str());
+            let file_extract_path = base_dir
+                .join(".packages")
+                .join(self.name.as_str())
+                .join(out_path_without_folder);
             if (&*c_file.name()).ends_with('/') {
-                std::fs::create_dir_all(
-                    format!("./.packages/{}", file_extract_path.to_str().unwrap()).as_str(),
-                )?;
+                std::fs::create_dir_all(&file_extract_path)?;
             } else {
                 if let Some(p) = file_extract_path.parent() {
                     if !p.exists() {
-                        std::fs::create_dir_all(
-                            format!("./.packages/{}", &p.to_str().expect("")).as_str(),
-                        )?;
+                        std::fs::create_dir_all(p)?;
                     }
                 }
-                let mut outfile = std::fs::File::create(format!(
-                    "./.packages/{}",
-                    &file_extract_path.to_str().expect("")
-                ))?;
+                // Note: we will be able to use tokio::io::copy() with async_zip
+                let mut outfile = std::fs::File::create(file_extract_path)?;
                 std::io::copy(&mut c_file, &mut outfile)?;
             }
         }
