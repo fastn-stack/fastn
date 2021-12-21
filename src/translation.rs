eb5c044b23fa066b40309209ef124f63bccdcf80
Amit Upadhyay <upadhyay@gmail.com>
2021-12-21 14:18:34 +0530

    minor

--- src/translation.rs
+++ src/translation.rs
@@ -3,12 +3,12 @@ enum TranslatedDocument {
         original: fpm::File,
     },
     NeverMarked {
-        original: fpm::File,
-        translated: fpm::File,
+        original: fpm::File,   // main
+        translated: fpm::File, // fallback
     },
     Outdated {
-        original: fpm::File,
-        translated: fpm::File,
+        original: fpm::File,   // fallback
+        translated: fpm::File, // main
         last_marked_on: u128,
         original_latest: u128,
         translated_latest: u128,
