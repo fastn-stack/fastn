2e163a97621965955fae3d4ee1b92599b3daf626
Amit Upadhyay <upadhyay@gmail.com>
2021-12-29 10:09:49 +0530

    `fpm update` doesnt choke when run twice; better messaging

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -1,7 +1,19 @@
 pub async fn update(config: &fpm::Config) -> fpm::Result<()> {
-    std::fs::remove_dir_all(config.root.join(".packages"))?;
-    if fpm::Config::read().await.is_ok() {
-        println!("package updated successfully");
+    if let Err(e) = std::fs::remove_dir_all(config.root.join(".packages")) {
+        match e.kind() {
+            std::io::ErrorKind::NotFound => {}
+            _ => return Err(e.into()),
+        }
+    };
+
+    let c = fpm::Config::read().await?;
+    if c.dependencies.is_empty() {
+        println!("No dependencies to update.")
+    } else if c.dependencies.len() == 1 {
+        println!("Updated the package dependency.")
+    } else {
+        println!("Updated {} dependencies.", c.dependencies.len())
     }
+
     Ok(())
 }
