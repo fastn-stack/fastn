7e6b61f350d069578969031784637ac4ecb2bc81
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-01-09 01:40:27 +0530

    store default as resolve_name

--- src/variable.rs
+++ src/variable.rs
@@ -164,9 +164,15 @@ impl PropertyValue {
         }
 
         fn get_parts(s: &str) -> ftd::p1::Result<(String, Option<String>)> {
-            Ok(if s.contains('.') {
-                let (p1, p2) = ftd::p2::utils::split(s.to_string(), ".")?;
-                (p1, Some(p2))
+            let mut part1 = "".to_string();
+            let mut pattern_to_split_at = s.to_string();
+            if let Some((p1, p2)) = s.split_once('#') {
+                part1 = p1.to_string();
+                pattern_to_split_at = p2.to_string();
+            }
+            Ok(if pattern_to_split_at.contains('.') {
+                let (p1, p2) = ftd::p2::utils::split(pattern_to_split_at, ".")?;
+                (format!("{}{}", part1, p1), Some(p2))
             } else {
                 (s.to_string(), None)
             })
