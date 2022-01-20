c1030eb88acf4f462da57c8131fb0f0eed7535d5
Shobhit <shobhit@fifthtry.com>
2022-01-20 12:07:55 +0530

    --pre-release flag for install.sh to allow the pre-production binary

--- install-fpm.sh
+++ install-fpm.sh
@@ -1,9 +1,16 @@
 # usr/bin/sh
+
+if [[ $* == *--pre-release* ]]; then
+    URL="https://api.github.com/repos/fifthtry/fpm/releases"
+else
+    URL="https://api.github.com/repos/fifthtry/fpm/releases/latest"
+fi
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    curl -s https://api.github.com/repos/fifthtry/fpm/releases/latest | grep ".*\/releases\/download\/.*\/fpm_macos.*" | cut -d : -f 2,3 | xargs -I % curl -O -J -L %
+    curl -s $URL | grep ".*\/releases\/download\/.*\/fpm_macos.*" | head -1 | cut -d : -f 2,3 | xargs -I % curl -O -J -L %
     mv fpm_macos_x86_64 /usr/local/bin/fpm
 else
-    curl -s https://api.github.com/repos/fifthtry/fpm/releases/latest | grep ".*\/releases\/download\/.*\/fpm_linux.*" | cut -d : -f 2,3 | xargs -I % curl -O -J -L %
+    curl -s $URL | grep ".*\/releases\/download\/.*\/fpm_linux.*" | head -2 | cut -d : -f 2,3 | xargs -I % curl -O -J -L %
     mv fpm_linux_musl_x86_64 /usr/local/bin/fpm
     mv fpm_linux_musl_x86_64.d /usr/local/bin/fpm.d
 fi
