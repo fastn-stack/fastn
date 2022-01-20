a105d7ae89fe221b4c6e0c53e6bea9e8439427d6
Shobhit <shobhit@fifthtry.com>
2022-01-20 12:29:54 +0530

    Add some basic echo

--- install-fpm.sh
+++ install-fpm.sh
@@ -2,15 +2,17 @@
 
 if [[ $* == *--pre-release* ]]; then
     URL="https://api.github.com/repos/fifthtry/fpm/releases"
+    echo "Downloading the latest pre-release binaries"
 else
     URL="https://api.github.com/repos/fifthtry/fpm/releases/latest"
+    echo "Downloading the latest production ready binaries"
 fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    curl -s $URL | grep ".*\/releases\/download\/.*\/fpm_macos.*" | head -1 | cut -d : -f 2,3 | xargs -I % curl -O -J -L %
+    curl -s $URL | grep ".*\/releases\/download\/.*\/fpm_macos.*" | head -1 | cut -d : -f 2,3 | tee /dev/tty | xargs -I % curl -O -J -L %
     mv fpm_macos_x86_64 /usr/local/bin/fpm
 else
-    curl -s $URL | grep ".*\/releases\/download\/.*\/fpm_linux.*" | head -2 | cut -d : -f 2,3 | xargs -I % curl -O -J -L %
+    curl -s $URL | grep ".*\/releases\/download\/.*\/fpm_linux.*" | head -2 | cut -d : -f 2,3 | tee /dev/tty | xargs -I % curl -O -J -L %
     mv fpm_linux_musl_x86_64 /usr/local/bin/fpm
     mv fpm_linux_musl_x86_64.d /usr/local/bin/fpm.d
 fi
