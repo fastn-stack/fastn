2758c96d1815eed6c8d27545e8842e48fd5e747b
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-02-11 00:22:38 +0530

    Fix: Check for optional condition

--- src/condition.rs
+++ src/condition.rs
@@ -8,9 +8,9 @@ impl Condition {
     pub fn is_true(&self, data: &ftd::DataDependenciesMap) -> bool {
         if let Some(ftd::Data { value: v, .. }) = data.get(self.variable.as_str()) {
             return if self.value.eq("$IsNull$") {
-                v.is_empty()
+                v.is_empty() || v.eq("null")
             } else if self.value.eq("$IsNotNull$") {
-                !v.is_empty()
+                !v.is_empty() && !v.eq("null")
             } else {
                 v == &self.value
             };
