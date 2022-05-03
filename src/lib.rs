5bcccb151ccd36a07d9406dd1f6561503ce72b5e
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-05-03 18:07:33 +0530

    Re-enabling the dark-mode

--- src/lib.rs
+++ src/lib.rs
@@ -69,8 +69,7 @@ fn ftd_js() -> String {
     if fpm::utils::is_test() {
         return "FTD_JS".to_string();
     }
-    // ftd::js()  // Temporarily using a copy till ftd is fixed
-    include_str!("../ftd.js").into()
+    ftd::js()
 }
 
 fn ftd_css() -> &'static str {
