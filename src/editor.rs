e9cda3e39b8f01306902240c1b26872f9ee55f0f
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-07-30 01:52:03 +0530

    Working on editor

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,67 @@
 use itertools::Itertools;
 use std::io::{Read, Write};
 
+#[macro_export]
+macro_rules! prompt {
+    ($output:expr, $args:tt) => {
+        prompt!($output, $args, callback = |&_, _, _| {})
+    };
+    ($output:expr, $args:tt /*$($args:tt)**/, callback = $callback:expr) => {{
+        let output:&mut Output = $output;
+        let mut input = String::with_capacity(32);
+        loop {
+            output.status_message.set_message(format!($args, input));
+            // output.status_message.set_message(format!($($args)*, input));
+            output.refresh_screen()?;
+            let key_event = Reader.read_key()?;
+            match key_event {
+                crossterm::event::KeyEvent {
+                    code: crossterm::event::KeyCode::Enter,
+                    modifiers: crossterm::event::KeyModifiers::NONE
+                } => {
+                    if !input.is_empty() {
+                        output.status_message.set_message(String::new());
+                        $callback(output, &input, crossterm::event::KeyCode::Esc);
+                        break;
+                    }
+                }
+                crossterm::event::KeyEvent {
+                    code: crossterm::event::KeyCode::Esc,
+                    ..
+                } => {
+                    output.status_message.set_message(String::new());
+                    input.clear();
+                    $callback(output, &input, crossterm::event::KeyCode::Esc);
+                    break;
+                }
+                crossterm::event::KeyEvent {
+                    code: crossterm::event::KeyCode::Backspace | crossterm::event::KeyCode::Delete,
+                    modifiers: crossterm::event::KeyModifiers::NONE,
+                } =>  {
+                    input.pop();
+                }
+                crossterm::event::KeyEvent {
+                    code: code @ (crossterm::event::KeyCode::Char(..) | crossterm::event::KeyCode::Tab),
+                    modifiers: crossterm::event::KeyModifiers::NONE | crossterm::event::KeyModifiers::SHIFT,
+                } => input.push(match code {
+                        crossterm::event::KeyCode::Tab => '\t',
+                        crossterm::event::KeyCode::Char(ch) => ch,
+                        _ => unreachable!(),
+                    }),
+
+                _=> {}
+            }
+            $callback(output, &input, key_event.code);
+        }
+        if input.is_empty() { None } else { Some (input) }
+    }};
+}
+
 const TAB_STOP: usize = 8;
+const HELP_TEXT: &'static str =
+    "HELP: Ctrl-H = Help | Ctrl-S = Save | Ctrl-Q = Quit | Ctrl-F = Find";
+const QUIT_TIMES: u8 = 2;
+
 struct CleanUp;
 
 impl Drop for CleanUp {
@@ -11,6 +71,7 @@ impl Drop for CleanUp {
     }
 }
 
+#[derive(Clone)]
 struct CursorController {
     cursor_x: usize,
     cursor_y: usize,
@@ -124,7 +185,7 @@ impl StatusMessage {
     fn new(initial_message: String) -> Self {
         Self {
             message: Some(initial_message),
-            set_time: None,
+            set_time: Some(std::time::Instant::now()),
         }
     }
 
@@ -178,6 +239,25 @@ impl Row {
         self.row_content.insert(at, ch);
         EditorRows::render_row(self)
     }
+
+    fn delete_char(&mut self, at: usize) {
+        self.row_content.remove(at);
+        EditorRows::render_row(self)
+    }
+
+    fn get_row_content_x(&self, render_x: usize) -> usize {
+        let mut current_render_x = 0;
+        for (cursor_x, ch) in self.row_content.chars().enumerate() {
+            if ch == '\t' {
+                current_render_x += (TAB_STOP - 1) - (current_render_x % TAB_STOP);
+            }
+            current_render_x += 1;
+            if current_render_x > render_x {
+                return cursor_x;
+            }
+        }
+        0
+    }
 }
 
 struct EditorRows {
@@ -228,8 +308,10 @@ impl EditorRows {
         row.render = render;
     }
 
-    fn insert_row(&mut self) {
-        self.row_contents.push(Row::default());
+    fn insert_row(&mut self, at: usize, contents: String) {
+        let mut new_row = Row::new(contents, String::new());
+        EditorRows::render_row(&mut new_row);
+        self.row_contents.insert(at, new_row);
     }
 
     fn get_editor_row_mut(&mut self, at: usize) -> &mut Row {
@@ -238,6 +320,10 @@ impl EditorRows {
 
     fn get_render(&self, at: usize) -> &String {
         &self.row_contents[at].render
+    }
+
+    fn get_row(&self, at: usize) -> &String {
+        &self.row_contents[at].row_content
     }
 
     fn get_editor_row(&self, at: usize) -> &Row {
@@ -255,6 +341,13 @@ impl EditorRows {
             .map(|it| it.row_content.as_str())
             .collect::<Vec<&str>>()
             .join("\n");
+    }
+
+    fn join_adjacent_rows(&mut self, at: usize) {
+        let current_row = self.row_contents.remove(at);
+        let previous_row = self.get_editor_row_mut(at - 1);
+        previous_row.row_content.push_str(&current_row.row_content);
+        Self::render_row(previous_row);
     }
 }
 
@@ -303,6 +396,7 @@ struct Output {
     cursor_controller: CursorController,
     editor_rows: EditorRows,
     status_message: StatusMessage,
+    dirty: u64,
 }
 
 impl Output {
@@ -315,7 +409,8 @@ impl Output {
             editor_contents: EditorContents::new(),
             cursor_controller: CursorController::new(win_size),
             editor_rows: EditorRows::new(content, filename),
-            status_message: StatusMessage::new("HELP: Ctrl-S = Save | Ctrl-Q = Quit ".into()),
+            status_message: StatusMessage::new(HELP_TEXT.into()),
+            dirty: 0,
         }
     }
 
@@ -323,13 +418,14 @@ impl Output {
         self.editor_contents
             .push_str(&crossterm::style::Attribute::Reverse.to_string());
         let info = format!(
-            "{} -- {} lines",
+            "{} {} -- {} lines",
             self.editor_rows
                 .filename
                 .as_ref()
                 .and_then(|path| path.file_name())
                 .and_then(|name| name.to_str())
                 .unwrap_or("[No Name]"),
+            if self.dirty > 0 { "(modified)" } else { "" },
             self.editor_rows.number_of_rows()
         );
         let info_len = std::cmp::min(info.len(), self.win_size.0);
@@ -440,18 +536,102 @@ impl Output {
             .cursor_y
             .eq(&self.editor_rows.number_of_rows())
         {
-            self.editor_rows.insert_row()
+            self.editor_rows
+                .insert_row(self.editor_rows.number_of_rows(), String::new());
         }
         self.editor_rows
             .get_editor_row_mut(self.cursor_controller.cursor_y)
             .insert_char(self.cursor_controller.cursor_x, ch);
         self.cursor_controller.cursor_x += 1;
+        self.dirty += 1;
+    }
+
+    fn delete_char(&mut self) {
+        if self
+            .cursor_controller
+            .cursor_y
+            .eq(&self.editor_rows.number_of_rows())
+        {
+            return;
+        }
+
+        if self.cursor_controller.cursor_y == 0 && self.cursor_controller.cursor_x == 0 {
+            return;
+        }
+        let row = self
+            .editor_rows
+            .get_editor_row_mut(self.cursor_controller.cursor_y);
+        if self.cursor_controller.cursor_x > 0 {
+            row.delete_char(self.cursor_controller.cursor_x - 1);
+            self.cursor_controller.cursor_x -= 1;
+        } else {
+            let previous_row_content = self
+                .editor_rows
+                .get_row(self.cursor_controller.cursor_y - 1);
+            self.cursor_controller.cursor_x = previous_row_content.len();
+            self.editor_rows
+                .join_adjacent_rows(self.cursor_controller.cursor_y);
+            self.cursor_controller.cursor_y -= 1;
+        }
+        self.dirty += 1;
+    }
+
+    fn insert_newline(&mut self) {
+        if self.cursor_controller.cursor_x == 0 {
+            self.editor_rows
+                .insert_row(self.cursor_controller.cursor_y, String::new())
+        } else {
+            let current_row = self
+                .editor_rows
+                .get_editor_row_mut(self.cursor_controller.cursor_y);
+            let new_row_content = current_row.row_content[self.cursor_controller.cursor_x..].into();
+            current_row
+                .row_content
+                .truncate(self.cursor_controller.cursor_x);
+            EditorRows::render_row(current_row);
+            self.editor_rows
+                .insert_row(self.cursor_controller.cursor_y + 1, new_row_content);
+        }
+        self.cursor_controller.cursor_x = 0;
+        self.cursor_controller.cursor_y += 1;
+        self.dirty += 1;
+    }
+
+    fn find_callback(output: &mut Output, keyword: &str, key_code: crossterm::event::KeyCode) {
+        match key_code {
+            crossterm::event::KeyCode::Esc | crossterm::event::KeyCode::Enter => {}
+            _ => {
+                for i in 0..output.editor_rows.number_of_rows() {
+                    let row = output.editor_rows.get_editor_row(i);
+                    if let Some(index) = row.render.find(&keyword) {
+                        output.cursor_controller.cursor_y = i;
+                        output.cursor_controller.cursor_x = row.get_row_content_x(index);
+                        output.cursor_controller.row_offset = output.editor_rows.number_of_rows();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    fn find(&mut self) -> std::io::Result<()> {
+        let cursor_controller = self.cursor_controller.clone();
+        let search = prompt!(
+            self,
+            "Search: {} (ESC to cancel)",
+            callback = Output::find_callback
+        );
+        if search.is_none() {
+            self.cursor_controller = cursor_controller;
+        }
+        Ok(())
     }
 }
 
 struct Editor {
     reader: Reader,
     output: Output,
+    quit_times: u8,
 }
 
 impl Editor {
@@ -459,6 +639,7 @@ impl Editor {
         Self {
             reader: Reader,
             output: Output::new(content, filename),
+            quit_times: QUIT_TIMES,
         }
     }
 
@@ -467,7 +648,17 @@ impl Editor {
             crossterm::event::KeyEvent {
                 code: crossterm::event::KeyCode::Char('q'),
                 modifiers: crossterm::event::KeyModifiers::CONTROL,
-            } => return Ok(None),
+            } => {
+                if self.output.dirty > 0 && self.quit_times > 0 {
+                    self.output.status_message.set_message(format!(
+                        "WARNING!!! File has unsaved changes. Press Ctrl-Q {} more times to quit.",
+                        self.quit_times
+                    ));
+                    self.quit_times -= 1;
+                    return Ok(Some(self.output.editor_rows.saved_contents.clone()));
+                }
+                return Ok(None);
+            }
             crossterm::event::KeyEvent {
                 code:
                     direction @ (crossterm::event::KeyCode::Up
@@ -510,6 +701,33 @@ impl Editor {
                     "{} bytes is saved",
                     self.output.editor_rows.saved_contents.as_bytes().len()
                 ));
+                self.output.dirty = 0;
+            }
+            crossterm::event::KeyEvent {
+                code: crossterm::event::KeyCode::Char('h'),
+                modifiers: crossterm::event::KeyModifiers::CONTROL,
+            } => {
+                self.output.status_message.set_message(HELP_TEXT.into());
+            }
+            crossterm::event::KeyEvent {
+                code:
+                    key @ (crossterm::event::KeyCode::Backspace | crossterm::event::KeyCode::Delete),
+                modifiers: crossterm::event::KeyModifiers::NONE,
+            } => {
+                if matches!(key, crossterm::event::KeyCode::Delete) {
+                    self.output.move_cursor(crossterm::event::KeyCode::Right);
+                }
+                self.output.delete_char();
+            }
+            crossterm::event::KeyEvent {
+                code: crossterm::event::KeyCode::Enter,
+                modifiers: crossterm::event::KeyModifiers::NONE,
+            } => self.output.insert_newline(),
+            crossterm::event::KeyEvent {
+                code: crossterm::event::KeyCode::Char('f'),
+                modifiers: crossterm::event::KeyModifiers::CONTROL,
+            } => {
+                self.output.find()?;
             }
             crossterm::event::KeyEvent {
                 code: code @ (crossterm::event::KeyCode::Char(..) | crossterm::event::KeyCode::Tab),
@@ -522,6 +740,7 @@ impl Editor {
             }),
             _ => {}
         }
+        self.quit_times = QUIT_TIMES;
         Ok(Some(self.output.editor_rows.saved_contents.clone()))
     }
 
