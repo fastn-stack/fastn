8596e34a4fa38e746098aa81c5d0443ae9267a2d
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-08-19 13:31:54 +0530

    wip: merge cr into main

--- src/commands/merge.rs
+++ src/commands/merge.rs
@@ -133,37 +133,40 @@ async fn merge_main_into_cr(
                 .await?;
 
         // get corresponding track file
-        let track_file_edit = if let Some(file_edit) = cr_track_manifest.get(&filename) {
-            file_edit
-        } else {
-            // Added file in CR
-            if file_edit.is_deleted() {
-                continue;
-            }
-            let theirs_content_bytes =
-                tokio::fs::read(config.history_path(filename.as_str(), file_edit.version)).await?;
-            if sha2::Sha256::digest(&ours_content_bytes)
-                .eq(&sha2::Sha256::digest(theirs_content_bytes))
-            {
-                new_file_status.insert(
-                    cr_file_path.to_string(),
-                    fpm::sync_utils::FileStatus::Delete {
-                        path: cr_file_path.to_string(),
-                        version: cr_file_edit.version,
-                        status: fpm::sync_utils::Status::NoConflict,
-                    },
-                );
-            } else {
-                conflicted_file_status.push(fpm::sync_utils::FileStatus::Add {
-                    path: filename.to_string(),
-                    content: ours_content_bytes.clone(),
-                    status: fpm::sync_utils::Status::CloneAddedRemoteAdded(file_edit.version),
-                });
-            }
-            continue;
-        };
         let track_file_path_str =
             config.path_without_root(&config.track_dir().join(&cr_file_path))?;
+        let track_file_edit = match cr_track_manifest.get(&track_file_path_str) {
+            Some(file_edit) => file_edit,
+            _ => {
+                // Added file in CR
+                if file_edit.is_deleted() {
+                    continue;
+                }
+                let theirs_content_bytes =
+                    tokio::fs::read(config.history_path(filename.as_str(), file_edit.version))
+                        .await?;
+                if sha2::Sha256::digest(&ours_content_bytes)
+                    .eq(&sha2::Sha256::digest(theirs_content_bytes))
+                {
+                    new_file_status.insert(
+                        cr_file_path.to_string(),
+                        fpm::sync_utils::FileStatus::Delete {
+                            path: cr_file_path.to_string(),
+                            version: cr_file_edit.version,
+                            status: fpm::sync_utils::Status::NoConflict,
+                        },
+                    );
+                } else {
+                    conflicted_file_status.push(fpm::sync_utils::FileStatus::Add {
+                        path: filename.to_string(),
+                        content: ours_content_bytes.clone(),
+                        status: fpm::sync_utils::Status::CloneAddedRemoteAdded(file_edit.version),
+                    });
+                }
+                continue;
+            }
+        };
+
         let track_file_path =
             config.history_path(track_file_path_str.as_str(), track_file_edit.version);
 
@@ -307,6 +310,239 @@ async fn merge_cr_into_main(
     src: usize,
     _file: Option<&str>,
 ) -> fpm::Result<()> {
+    let remote_manifest: std::collections::BTreeMap<String, fpm::history::FileEdit> = config
+        .get_remote_manifest(true)
+        .await?
+        .into_iter()
+        .filter(|(k, _)| !(k.starts_with("-/") || k.starts_with(".tracks/")))
+        .collect();
+    let cr_manifest = config.get_cr_manifest(src).await?;
+    let (cr_track_manifest, cr_file_manifest) = cr_manifest.into_iter().fold(
+        (
+            std::collections::HashMap::new(),
+            std::collections::HashMap::new(),
+        ),
+        |(mut cr_track_status, mut cr_file_status), (file, file_edit)| {
+            if file.starts_with(".tracks/") {
+                cr_track_status.insert(file, file_edit);
+            } else {
+                cr_file_status.insert(file, file_edit);
+            }
+            (cr_track_status, cr_file_status)
+        },
+    );
+    let mut new_file_status: std::collections::BTreeMap<String, fpm::sync_utils::FileStatus> =
+        Default::default();
+    let mut conflicted_file_status = vec![];
+    let deleted_files_path = config.cr_deleted_file_path(src);
+    let deleted_files = config.path_without_root(&deleted_files_path)?;
+    for (cr_file_name, cr_file_edit) in cr_file_manifest {
+        if cr_file_name.eq(&deleted_files) {
+            // status for deleted files
+            let cr_deleted_files = tokio::fs::read_to_string(
+                config.history_path(cr_file_name.as_str(), cr_file_edit.version),
+            )
+            .await?;
+            let cr_deleted_list =
+                fpm::cr::resolve_cr_deleted(cr_deleted_files.as_str(), src).await?;
+
+            for cr_delete in cr_deleted_list {
+                let file_edit =
+                    if let Some(file_edit) = remote_manifest.get(cr_delete.filename.as_str()) {
+                        file_edit
+                    } else {
+                        return fpm::usage_error(format!(
+                            "Can't find history of `{}` which is marked to be deleted in CR#{}",
+                            cr_delete.filename, src
+                        ));
+                    };
+                if file_edit.is_deleted() {
+                    continue;
+                }
+                if !file_edit.version.eq(&cr_delete.version) {
+                    conflicted_file_status.push(fpm::sync_utils::FileStatus::Delete {
+                        path: cr_delete.filename.to_string(),
+                        version: cr_delete.version,
+                        status: fpm::sync_utils::Status::CloneDeletedRemoteEdited(
+                            file_edit.version,
+                        ),
+                    });
+                    continue;
+                }
+                new_file_status
+                    .insert(cr_delete.filename.to_string(), cr_delete.into_file_status());
+            }
+            continue;
+        }
+        let cr_file_path = config.history_path(cr_file_name.as_str(), cr_file_edit.version);
+        let cr_file_content = tokio::fs::read(&cr_file_path).await?;
+        let filename = fpm::cr::cr_path_to_file_name(src, cr_file_path.as_str())?;
+        let file_edit = if let Some(file_edit) = remote_manifest.get(filename.as_str()) {
+            file_edit
+        } else {
+            // Added file in CR
+            new_file_status.insert(
+                filename.to_string(),
+                fpm::sync_utils::FileStatus::Add {
+                    path: cr_file_name.to_string(),
+                    content: cr_file_content.clone(),
+                    status: fpm::sync_utils::Status::NoConflict,
+                },
+            );
+            continue;
+        };
+
+        let track_file_path_str =
+            config.path_without_root(&config.track_dir().join(&cr_file_name))?;
+        let track_file_edit = match cr_track_manifest.get(&track_file_path_str) {
+            Some(file_edit) if !file_edit.is_deleted() => file_edit,
+            _ => {
+                // Added file in CR
+                new_file_status.insert(
+                    filename.to_string(),
+                    fpm::sync_utils::FileStatus::Add {
+                        path: cr_file_name.to_string(),
+                        content: cr_file_content.clone(),
+                        status: fpm::sync_utils::Status::NoConflict,
+                    },
+                );
+                continue;
+            }
+        };
+
+        let ours_content_bytes =
+            tokio::fs::read(config.history_path(cr_file_path.as_str(), cr_file_edit.version))
+                .await?;
+
+        let track_file_path =
+            config.history_path(track_file_path_str.as_str(), track_file_edit.version);
+
+        let mut tracking_infos = fpm::track::get_tracking_info_(&track_file_path)
+            .await?
+            .into_iter()
+            .map(|v| (v.filename.to_string(), v))
+            .collect::<std::collections::HashMap<String, fpm::track::TrackingInfo>>();
+        let track_info = if let Some(track_info) = tracking_infos.get_mut(&filename) {
+            track_info
+        } else {
+            // Added file in CR
+            if file_edit.is_deleted() {
+                new_file_status.insert(
+                    filename.to_string(),
+                    fpm::sync_utils::FileStatus::Add {
+                        path: cr_file_name.to_string(),
+                        content: cr_file_content.clone(),
+                        status: fpm::sync_utils::Status::NoConflict,
+                    },
+                );
+            }
+            let theirs_content_bytes =
+                tokio::fs::read(config.history_path(filename.as_str(), file_edit.version)).await?;
+            if !sha2::Sha256::digest(&ours_content_bytes)
+                .eq(&sha2::Sha256::digest(theirs_content_bytes))
+            {
+                conflicted_file_status.push(fpm::sync_utils::FileStatus::Add {
+                    path: filename.to_string(),
+                    content: ours_content_bytes.clone(),
+                    status: fpm::sync_utils::Status::CloneAddedRemoteAdded(file_edit.version),
+                });
+            }
+            continue;
+        };
+
+        if file_edit.is_deleted() {
+            if !track_info.version.eq(&file_edit.version) {
+                conflicted_file_status.push(fpm::sync_utils::FileStatus::Update {
+                    path: filename.to_string(),
+                    content: ours_content_bytes.clone(),
+                    version: track_info.version,
+                    status: fpm::sync_utils::Status::CloneEditedRemoteDeleted(file_edit.version),
+                });
+            }
+            continue;
+        }
+
+        if track_info.version.eq(&file_edit.version) {
+            // Edited on cr wrt remote's latest version
+            new_file_status.insert(
+                filename.to_string(),
+                fpm::sync_utils::FileStatus::Update {
+                    path: filename,
+                    content: cr_file_content,
+                    version: track_info.version,
+                    status: fpm::sync_utils::Status::NoConflict,
+                },
+            );
+            continue;
+        }
+
+        let ancestor_content = if let Ok(content) =
+            tokio::fs::read_to_string(config.history_path(filename.as_str(), track_info.version))
+                .await
+        {
+            content
+        } else {
+            // binary file like images, can't resolve conflict
+            conflicted_file_status.push(fpm::sync_utils::FileStatus::Update {
+                path: filename.to_string(),
+                content: ours_content_bytes.clone(),
+                version: track_info.version,
+                status: fpm::sync_utils::Status::Conflict(file_edit.version),
+            });
+            continue;
+        };
+
+        let theirs_content =
+            tokio::fs::read_to_string(config.history_path(filename.as_str(), file_edit.version))
+                .await?;
+
+        let ours_content = String::from_utf8(ours_content_bytes.clone())?;
+
+        match diffy::MergeOptions::new()
+            .set_conflict_style(diffy::ConflictStyle::Merge)
+            .merge(&ancestor_content, &ours_content, &theirs_content)
+        {
+            Ok(data) => {
+                new_file_status.insert(
+                    filename.to_string(),
+                    fpm::sync_utils::FileStatus::Update {
+                        path: filename.to_string(),
+                        content: data.into_bytes(),
+                        version: cr_file_edit.version,
+                        status: fpm::sync_utils::Status::NoConflict,
+                    },
+                );
+            }
+            Err(_) => {
+                // Can't resolve conflict
+                conflicted_file_status.push(fpm::sync_utils::FileStatus::Update {
+                    path: filename.to_string(),
+                    content: ours_content_bytes.clone(),
+                    version: track_info.version,
+                    status: fpm::sync_utils::Status::Conflict(file_edit.version),
+                });
+                continue;
+            }
+        }
+    }
+
+    conflicted_file_status
+        .iter()
+        .map(|v| fpm::commands::sync_status::print_status(v, false))
+        .collect_vec();
+
+    if conflicted_file_status.is_empty() {
+        //TODO
+    }
+
+    Ok(())
+}
+
+/*async fn merge_cr_into_main(
+    config: &fpm::Config,
+    src: usize,
+    _file: Option<&str>,
+) -> fpm::Result<()> {
     let cr_status = config.get_cr_status(src).await?;
     let conflicted_file = cr_status.iter().find(|v| v.is_conflicted());
     if let Some(conflicted_file) = conflicted_file {
@@ -424,4 +660,4 @@ async fn merge_cr_into_main(
 
     fpm::commands::sync2::sync(config, changed_files).await?;
     Ok(())
-}
+}*/
