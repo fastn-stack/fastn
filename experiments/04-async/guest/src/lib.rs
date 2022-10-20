1fbc7e07a8b221eaf7ff4c287bf0d25762b78c12
Amit Upadhyay <upadhyay@gmail.com>
2022-10-20 20:01:27 +0530

    usize-size and wasm-returning-a-string experiments

--- experiments/04-async/guest/src/lib.rs
+++ experiments/04-async/guest/src/lib.rs
@@ -5,7 +5,12 @@ extern "C" {
 #[no_mangle]
 pub extern "C" fn sum(x: i32) -> i32 {
     x + unsafe {
-        from_host(10);
+        from_host(10, 20);
         10
     }
 }
+
+/// lbp = length bytes pointer. length is u32, big endian.
+///
+/// string's length is length
+fn string_to_lbp(s: String) -> u32 {}
