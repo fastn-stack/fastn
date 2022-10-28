5a001a9770ee3eca5707a6c988ae0c8df0ff08c4
abrark <abrark.asahi@gmail.com>
2022-10-28 19:19:53 +0530

    sitemap/mount-point: mount-point working but breaking other things

--- src/config/utils.rs
+++ src/config/utils.rs
@@ -14,3 +14,18 @@ pub(crate) fn find_root_for_file(
         None
     }
 }
+
+pub async fn fpm_doc(path: &camino::Utf8Path) -> fpm::Result<ftd::p2::Document> {
+    {
+        let doc = tokio::fs::read_to_string(path);
+        let lib = fpm::FPMLibrary::default();
+        match fpm::doc::parse_ftd("FPM", doc.await?.as_str(), &lib) {
+            Ok(v) => Ok(v),
+            Err(e) => {
+                return Err(fpm::Error::PackageError {
+                    message: format!("failed to parse FPM.ftd 3: {:?}", &e),
+                });
+            }
+        }
+    }
+}
