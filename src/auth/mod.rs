5b954d403067736162dd6299eb8d9528f6670c7d
AbrarNitk <abrar.nitk@gmail.com>
2022-11-09 12:56:10 +0530

    auth/integration: removed extra code

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -3,9 +3,3 @@ pub(crate) mod github;
 pub(crate) mod gmail;
 pub(crate) mod slack;
 pub(crate) mod telegram;
-
-pub(crate) use github::auth;
-pub(crate) use github::get_identity;
-pub(crate) use github::index;
-pub(crate) use github::login;
-pub(crate) use github::logout;
