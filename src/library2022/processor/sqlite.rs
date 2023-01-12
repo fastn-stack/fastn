f9b491f63f6fb1ef546c12e7d2b1234a415efdf7
AbrarNitk <abrar.nitk@gmail.com>
2023-01-12 14:46:06 +0530

    fpm-processor/sqlite: added some more code

--- src/library2022/processor/sqlite.rs
+++ src/library2022/processor/sqlite.rs
@@ -1,8 +1,46 @@
 pub async fn process<'a>(
     value: ftd::ast::VariableValue,
-    _kind: ftd::interpreter2::Kind,
+    kind: ftd::interpreter2::Kind,
     doc: &ftd::interpreter2::TDoc<'a>,
     config: &fpm::Config,
 ) -> ftd::interpreter2::Result<ftd::interpreter2::Value> {
+    tokio::task::block_in_place(move || processor_(value, kind, doc, config))
+}
+
+pub fn processor_<'a>(
+    value: ftd::ast::VariableValue,
+    kind: ftd::interpreter2::Kind,
+    doc: &ftd::interpreter2::TDoc<'a>,
+    config: &fpm::Config,
+) -> ftd::interpreter2::Result<ftd::interpreter2::Value> {
+    let (headers, body) = match value.get_record(doc.name) {
+        Ok(val) => (val.2.to_owned(), val.3.to_owned()),
+        Err(e) => return Err(e.into()),
+    };
+
+    let sqlite_database =
+        match headers.get_optional_string_by_key("db", doc.name, value.line_number())? {
+            Some(k) => k,
+            None => {
+                return ftd::interpreter2::utils::e2(
+                    "`db` is not specified".to_string(),
+                    doc.name,
+                    value.line_number(),
+                )
+            }
+        };
+
+    let query = match body {
+        Some(b) => b.value.as_str(),
+        None => {
+            return ftd::interpreter2::utils::e2(
+                "$processor: `package-query` query is not specified in the processor body$"
+                    .to_string(),
+                doc.name,
+                value.line_number(),
+            )
+        }
+    };
+
     Ok(ftd::interpreter2::Value::Boolean { value: false })
 }
