11ab31e31f21c2c366a46a3ae43885a70df49131
AbrarNitk <abrar.nitk@gmail.com>
2023-01-16 10:58:55 +0530

    tracing: added tracing on interpreter functions

--- src/interpreter2/main2.rs
+++ src/interpreter2/main2.rs
@@ -680,6 +680,7 @@ impl InterpreterState {
         Ok(ftd::interpreter2::StateWithThing::new_continue())
     }
 
+    #[tracing::instrument(skip_all)]
     pub fn continue_after_import(
         mut self,
         module: &str,
@@ -696,6 +697,7 @@ impl InterpreterState {
         self.continue_processing()
     }
 
+    #[tracing::instrument(skip_all)]
     pub fn continue_after_processor(
         mut self,
         value: ftd::interpreter2::Value,
@@ -741,6 +743,7 @@ impl InterpreterState {
         self.continue_processing()
     }
 
+    #[tracing::instrument(skip_all)]
     pub fn continue_after_variable(
         mut self,
         module: &str,
@@ -775,12 +778,15 @@ pub fn interpret<'a>(id: &'a str, source: &'a str) -> ftd::interpreter2::Result<
     interpret_with_line_number(id, source, 0)
 }
 
+#[tracing::instrument(skip_all)]
 pub fn interpret_with_line_number<'a>(
     id: &'a str,
     source: &'a str,
     line_number: usize,
 ) -> ftd::interpreter2::Result<Interpreter> {
     use itertools::Itertools;
+
+    tracing::info!(msg = "ftd: interpreting", doc = id);
 
     let mut s = InterpreterState::new(id.to_string());
     s.parsed_libs.insert(
