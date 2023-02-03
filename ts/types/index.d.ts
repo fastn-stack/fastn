a5d8298c859275c2a1851c28291d2a4cccf5be80
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-02-03 15:26:50 +0530

    Added set_list default function

--- ts/types/index.d.ts
+++ ts/types/index.d.ts
@@ -21,6 +21,7 @@ declare global {
         set_value_by_id(id: string, variable: string, value: any): any;
         get_value(id: string, variable: string): any;
         is_empty(str: any): boolean;
+        set_list(array: any[], value: any[], args: any, data: any, id: string): any[];
         append(array: any[], value: any, args: any, data: any, id: string): any[];
         clear(array: any[], args: any, data: any, id: string): any[];
         insert_at(array: any[], value: any, idx: number, args: any, data: any, id: string): any[];
