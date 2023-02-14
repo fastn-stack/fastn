0d50def367e28c4f3cb0f22e4469d0f64ffb8ce8
AbrarNitk <abrar.nitk@gmail.com>
2023-02-14 21:49:07 +0530

    fixed the fastn release url in install.sh

--- install.sh
+++ install.sh
@@ -56,10 +56,10 @@ setup() {
     done
 
     if [[ $PRE_RELEASE ]]; then
-        URL="https://api.github.com/repos/ftd-lang/fastn/releases"
+        URL="https://api.github.com/repos/ƒastn-stack/fastn/releases"
         echo "Downloading the latest pre-release binaries"
     else
-        URL="https://api.github.com/repos/ftd-lang/fastn/releases/latest"
+        URL="https://api.github.com/repos/ƒastn-stack/fastn/releases/latest"
         echo "Downloading the latest production ready binaries"
     fi
 
