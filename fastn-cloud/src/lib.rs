3c78a362e3d71086471ba9284fdc8caf12d74417
AbrarNitk <abrar.nitk@gmail.com>
2023-02-22 20:11:00 +0530

    create api steps

--- fastn-cloud/src/lib.rs
+++ fastn-cloud/src/lib.rs
@@ -5,6 +5,13 @@ pub enum CreateError {}
 pub enum UpdateError {}
 
 pub async fn create() -> Result<(), CreateError> {
+    // call fastn build
+    // read the content of the .build folder
+    // pass this content to tejar and create two files LIST and DATA
+    // call /api/create/ by passing the content of the LIST and META
+    // call /api/upload-new-package by passing the missing entries and DATA
+    // save package key and at home folder
+    // show the subdomain to user or open browser directly
     println!("publish-static create called");
     Ok(())
 }
