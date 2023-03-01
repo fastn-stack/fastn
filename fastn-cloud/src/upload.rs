997d071c0ec8c263345b01edb2bf71983b787d37
AbrarNitk <abrar.nitk@gmail.com>
2023-03-01 12:24:56 +0530

    fastn-cli/code-cleanup: publish-static code cleanup

--- fastn-cloud/src/upload.rs
+++ fastn-cloud/src/upload.rs
@@ -42,7 +42,7 @@ pub async fn upload(
     cw_id: &str,
 ) -> Result<(), fastn_cloud::UploadError> {
     let sid: Sid = serde_json::from_str(sid)?;
-    let (list_file, data_file) = fastn_cloud::create::tejar_create(root).await?;
+    let (list_file, data_file) = tejar_create(root).await?;
     let list_content = tokio::fs::read_to_string(list_file.as_path()).await?;
     // TODO: missing files handle sid
     println!("Getting Missing Files");
@@ -54,13 +54,12 @@ pub async fn upload(
     .await?;
 
     println!("Filtering Missing Hashes hashes");
-    let (missing_hashes_list, missing_hashes_content) = fastn_cloud::create::get_missing_checksums(
+    let (missing_hashes_list, missing_hashes_content) = get_missing_checksums(
         list_content.as_str(),
         missing_files_api_resp.missing_hashes.as_slice(),
         data_file.as_path(),
     )
-    .await
-    .unwrap();
+    .await?;
 
     println!("Uploading Missing List Content");
     let resp = upload_api(
@@ -107,4 +106,64 @@ pub async fn upload_api(
     let response: UploadAPIResponse =
         fastn_cloud::http::put("/api/upload/", body, &headers, &query).await?;
     Ok(response)
+}
+
+pub async fn get_missing_checksums(
+    list_content: &str,
+    missing_hashes: &[String],
+    data_file: &camino::Utf8Path,
+) -> Result<(String, Vec<u8>), fastn_cloud::UploadError> {
+    let list = tejar::read::reader(list_content)?.list;
+    let mut data = vec![];
+    let mut new_list = String::new();
+    let mut offset = 0;
+    for checksum in missing_hashes {
+        let record = list.iter().find(|r| r.checksum.eq(checksum)).unwrap();
+        let mut file_data =
+            read_with_offset(data_file, record.offset as u64, record.file_size as usize)
+                .await
+                .unwrap();
+        let list_record = tejar::create::ListRecord {
+            data_file_name: record.data_file_name.to_string(),
+            file_name: record.file_name.to_string(),
+            content_type: record.content_type.to_string(),
+            compression: "todo!".to_string(), // TODO:
+            start: offset,
+            size: record.file_size,
+            timestamp: record.timestamp,
+            checksum: record.checksum.to_string(),
+        };
+        offset += record.file_size;
+        data.append(&mut file_data);
+        new_list.push_str(list_record.to_string().as_str());
+    }
+    Ok((new_list, data))
+}
+
+pub async fn tejar_create(
+    root: &camino::Utf8Path,
+) -> Result<(camino::Utf8PathBuf, camino::Utf8PathBuf), tejar::error::CreateError> {
+    let files: _ = fastn_cloud::utils::walkdir_util(root)
+        .into_iter()
+        .map(|file| tejar::create::InputFile {
+            path: file.path.strip_prefix(root).unwrap().to_path_buf(),
+            content_type: file.content_type,
+            gzip: file.gzip,
+        })
+        .collect::<Vec<_>>();
+    tejar::create::create(root, files.as_slice())
+}
+
+pub async fn read_with_offset(
+    path: &camino::Utf8Path,
+    offset: u64,
+    size: usize,
+) -> Result<Vec<u8>, tokio::io::Error> {
+    use tokio::io::AsyncReadExt;
+    use tokio::io::AsyncSeekExt;
+    let mut file = tokio::fs::File::open(path).await?;
+    file.seek(tokio::io::SeekFrom::Start(offset)).await?;
+    let mut buffer = vec![0u8; size];
+    file.read_exact(&mut buffer).await?;
+    Ok(buffer)
 }
