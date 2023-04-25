599809cfe6f703a2b9dba1e19efb8c02c79b7a9e
Amit Upadhyay <upadhyay@gmail.com>
2023-04-25 13:39:21 +0530

    started some test

---
+++ fastn-package/tests/package_name.rs
@@ -0,0 +1,20 @@
+#[test]
+fn fastn_package_test() {
+    use fastn_package::initializer::test::*;
+
+    futures::executor::block_on(async {
+        match dbg!(fastn_package::initialize(TestInitializer::new()).await) {
+            Err(InitialisePackageError::FastnFTDError { source }) => match source {
+                FastnFTDError::ReadFTDFile { source } => match source {
+                    FileAsStringError::FileDoesNotExist { name, source } => {
+                        assert_eq!(name, "FASTN.ftd");
+                        assert_eq!(source.kind(), std::io::ErrorKind::NotFound);
+                    }
+                    _ => panic!("unexpected error"),
+                },
+                _ => panic!("unexpected error"),
+            },
+            _ => panic!("unexpected error"),
+        }
+    });
+}
