503f3bb8cd7e1abacc4adc72f39e2da110f8ca21
Amit Upadhyay <upadhyay@gmail.com>
2023-04-25 14:01:58 +0530

    started fastn-issues crate

--- fastn-package/src/initialize.rs
+++ fastn-package/src/initialize.rs
@@ -1,21 +1,7 @@
-#[derive(thiserror::Error, Debug)]
-pub enum InitialisePackageError {
-    #[error("fastn.ftd error: {source}")]
-    FastnFTDError {
-        #[from]
-        source: FastnFTDError,
-    },
-    #[error("db initialisation error: {source}")]
-    InitializeDBError {
-        #[from]
-        source: fastn_package::sqlite::InitializeDBError,
-    },
-}
-
 /// re_initialise() is called when any file is changed in the package
 pub async fn re_initialise(
     i: impl fastn_package::initializer::Initializer,
-) -> Result<(), InitialisePackageError> {
+) -> Result<(), fastn_issues::initialization::InitializePackageError> {
     if let Some(v) = fastn_package::FTD_CACHE.get() {
         let mut v = v.write().await;
         v.clear();
@@ -28,36 +14,17 @@ pub async fn re_initialise(
 /// initialise() is called on application start
 pub async fn initialize(
     i: impl fastn_package::initializer::Initializer,
-) -> Result<(), InitialisePackageError> {
+) -> Result<(), fastn_issues::initialization::InitializePackageError> {
     fastn_package::FTD_CACHE
         .get_or_init(|| async { tokio::sync::RwLock::new(std::collections::HashMap::new()) })
         .await;
     re_initialise(i).await
 }
 
-#[derive(thiserror::Error, Debug)]
-pub enum FastnFTDError {
-    #[error("Can't read FASTN.ftd: {source}")]
-    ReadFTDFile {
-        #[from]
-        source: fastn_package::initializer::FileAsStringError,
-    },
-    #[error("Cant parse FASTN.ftd: {source}")]
-    ParseFASTNFile {
-        #[from]
-        source: fastn_package::old_fastn::OldFastnParseError,
-    },
-    #[error("Cant store package name: {source}")]
-    StorePackageName {
-        #[from]
-        source: StoreNameError,
-    },
-}
-
 async fn process_fastn_ftd(
     i: impl fastn_package::initializer::Initializer,
     conn: rusqlite::Connection,
-) -> Result<(), FastnFTDError> {
+) -> Result<(), fastn_issues::initialization::FastnFTDError> {
     let content = i.file_as_string("FASTN.ftd").await?;
     let fastn_doc = fastn_package::old_fastn::parse_old_fastn(content.as_str())?;
     store_name(conn, fastn_doc).await?;
@@ -65,19 +32,10 @@ async fn process_fastn_ftd(
     todo!()
 }
 
-#[derive(thiserror::Error, Debug)]
-pub enum StoreNameError {
-    #[error("Cant get package name from FASTN.ftd: {source}")]
-    CantGetPackageName {
-        #[from]
-        source: fastn_package::old_fastn::GetNameError,
-    },
-}
-
 async fn store_name(
     _conn: rusqlite::Connection,
     fastn_doc: ftd::ftd2021::p2::Document,
-) -> Result<(), StoreNameError> {
+) -> Result<(), fastn_issues::initialization::StoreNameError> {
     let _name = fastn_package::old_fastn::get_name(fastn_doc)?;
     // TODO: insert package name to main_package table
     todo!()
