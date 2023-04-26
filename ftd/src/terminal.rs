f79191f03444bfac9330d4d06f273183ee35dc55
Amit Upadhyay <upadhyay@gmail.com>
2023-04-26 18:35:52 +0530

    parse_ftd_document()

--- ftd/src/terminal.rs
+++ ftd/src/terminal.rs
@@ -1,80 +1,38 @@
 use dioxus_html::EventData;
-use dioxus_native_core::{
-    node::TextNode,
-    prelude::*,
-    real_dom::{NodeImmutable, NodeTypeMut},
-    NodeId,
-};
+use dioxus_native_core::{node::TextNode, prelude::*, real_dom::NodeImmutable, NodeId};
 use rink::{render, Config, Driver};
 use std::rc::Rc;
 use std::sync::{Arc, RwLock};
 
-#[derive(Default)]
-struct Counter {
-    count: usize,
-    counter_id: NodeId,
-    button_id: NodeId,
-}
+struct Document {}
 
-impl Counter {
-    fn create(mut root: NodeMut) -> Self {
-        let mut myself = Self::default();
+impl Document {
+    fn parse_ftd_document() -> ftd::node::NodeData {
+        let doc = ftd::test_helper::ftd_v2_interpret_helper("foo", "-- ftd.text: hello world")
+            .unwrap_or_else(|e| panic!("{:?}", e));
+        let executor =
+            ftd::executor::ExecuteDoc::from_interpreter(doc).unwrap_or_else(|e| panic!("{:?}", e));
+        ftd::node::NodeData::from_rt(executor)
+    }
+
+    fn create(mut root: NodeMut, _node: ftd::node::NodeData) -> Self {
+        let myself = Document {};
 
         let root_id = root.id();
         let rdom = root.real_dom_mut();
 
-        // create the counter
-        let count = myself.count;
-        myself.counter_id = rdom
+        let id = rdom
             .create_node(NodeType::Text(TextNode::new("count.to_string(), this is a long long long text, lets see if they know how to wrap text etc.".to_string())))
             .id();
-        let mut button = rdom.create_node(NodeType::Element(ElementNode {
-            tag: "div".to_string(),
-            attributes: [
-                ("display".to_string().into(), "flex".to_string().into()),
-                (
-                    ("background-color", "style").into(),
-                    format!("rgb({}, {}, {})", count * 10, 0, 0,).into(),
-                ),
-                (("width", "style").into(), "100%".to_string().into()),
-                (("height", "style").into(), "100%".to_string().into()),
-                (("flex-direction", "style").into(), "row".to_string().into()),
-                (
-                    ("justify-content", "style").into(),
-                    "center".to_string().into(),
-                ),
-                (("align-items", "style").into(), "center".to_string().into()),
-            ]
-            .into_iter()
-            .collect(),
-            ..Default::default()
-        }));
-        button.add_event_listener("click");
-        // button.add_event_listener("wheel");
-        button.add_child(myself.counter_id);
-        myself.button_id = button.id();
-        rdom.get_mut(root_id).unwrap().add_child(myself.button_id);
+        rdom.get_mut(root_id).unwrap().add_child(id);
 
         myself
     }
 }
 
-impl Driver for Counter {
-    fn update(&mut self, rdom: &Arc<RwLock<RealDom>>) {
-        // update the counter
-        let mut rdom = rdom.write().unwrap();
-        let mut node = rdom.get_mut(self.button_id).unwrap();
-        if let NodeTypeMut::Element(mut el) = node.node_type_mut() {
-            el.set_attribute(
-                ("background-color", "style"),
-                format!("rgb({}, {}, {})", self.count * 10, 0, 0,),
-            );
-        }
-        let mut text = rdom.get_mut(self.counter_id).unwrap();
-        let type_mut = text.node_type_mut();
-        if let NodeTypeMut::Text(mut text) = type_mut {
-            *text = self.count.to_string();
-        }
+impl Driver for Document {
+    fn update(&mut self, _: &Arc<RwLock<RealDom>>) {
+        println!("Document.update()");
     }
 
     fn handle_event(
@@ -85,11 +43,12 @@ impl Driver for Counter {
         _: Rc<EventData>,
         _: bool,
     ) {
-        // when a click or wheel event is fired, increment the counter
-        self.count += 1;
+        println!("Document.handle_event()");
     }
 
     fn poll_async(&mut self) -> std::pin::Pin<Box<dyn futures::Future<Output = ()> + '_>> {
+        println!("Document.poll_async()");
+        // leaving this as is for now.
         Box::pin(async move { tokio::time::sleep(std::time::Duration::from_millis(1000)).await })
     }
 }
@@ -98,7 +57,7 @@ pub fn run() {
     render(Config::new(), |rdom, _, _| {
         let mut rdom = rdom.write().unwrap();
         let root = rdom.root_id();
-        Counter::create(rdom.get_mut(root).unwrap())
+        Document::create(rdom.get_mut(root).unwrap(), Document::parse_ftd_document())
     })
     .unwrap();
 }
