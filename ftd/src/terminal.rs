f7b97ef3784c591bad7d45c64c0b6462cc87d088
Arpita Jaiswal <ayushipujaa@gmail.com>
2023-04-28 18:03:07 +0530

    Feature: node to terminal node (#900)
    * Working terminal.ftd
    
    * Working terminal.ftd
    
    * Formatting fixes

--- ftd/src/terminal.rs
+++ ftd/src/terminal.rs
@@ -21,36 +21,10 @@ impl Document {
     fn create(mut root: NodeMut, node: ftd::node::Node) -> Self {
         let myself = Document {};
 
-        /*let mut attributes: rustc_hash::FxHashMap<OwnedAttributeDiscription, OwnedAttributeValue> = Default::default();
-                for (k, v) in &node.attrs {
-                    dbg!(&k, &v);
-                    if let Some(ref v) = v.value {
-                        attributes.insert(k.to_string().into(), v.to_string().into());
-                    }
-                }
-
-                for (k, v) in &node.style {
-                    dbg!(&k, &v);
-                    if let Some(ref v) = v.value {
-                        attributes.insert((k.as_str(), "style").into(), v.to_string().into());
-                    }
-                }
-                let nn= rdom.create_node(NodeType::Element(ElementNode {
-                    tag: node.node,
-                    attributes,
-                    ..Default::default()
-                }));
-        */
-
-        dbg!(&node);
-
         let root_id = root.id();
         let rdom = root.real_dom_mut();
 
-        let terminal_node = dbg!(node.to_terminal_node(rdom).id());
-        // let id = rdom
-        //     .create_node(NodeType::Text(TextNode::new("count.to_string(), this is a long long long text, lets see if they know how to wrap text etc.".to_string())))
-        //     .id();
+        let terminal_node = node.to_terminal_node(rdom).id();
 
         rdom.get_mut(root_id).unwrap().add_child(terminal_node);
 
@@ -98,14 +72,12 @@ impl ftd::node::Node {
         }
 
         for (k, v) in &self.attrs {
-            dbg!(&k, &v);
             if let Some(ref v) = v.value {
                 attributes.insert(k.to_string().into(), v.to_string().into());
             }
         }
 
         for (k, v) in &self.style {
-            dbg!(&k, &v);
             if let Some(ref v) = v.value {
                 attributes.insert((k.as_str(), "style").into(), v.to_string().into());
             }
