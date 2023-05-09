fe2d5d0c191061f9eb9ab2e93eca20ee06e1e9b4
Amit Upadhyay <upadhyay@gmail.com>
2023-05-09 17:05:59 +0530

    removed import

--- fastn-surface/src/wgpu/render.rs
+++ fastn-surface/src/wgpu/render.rs
@@ -1,34 +1,27 @@
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-
 pub fn render(mut w: fastn_surface::Document) {
     env_logger::init();
 
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let event_loop = winit::event_loop::EventLoop::new();
+    let window = winit::window::WindowBuilder::new().build(&event_loop).unwrap();
     let window_size = window.inner_size();
 
     w.layout(window_size.width, window_size.height);
 
-
     event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
+        winit::event::Event::WindowEvent {
             ref event,
             window_id,
         } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
+            winit::event::WindowEvent::CloseRequested
+            | winit::event::WindowEvent::KeyboardInput {
                 input:
-                KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                winit::event::KeyboardInput {
+                    state: winit::event::ElementState::Pressed,
+                    virtual_keycode: Some(winit::event::VirtualKeyCode::Escape),
                     ..
                 },
                 ..
-            } => *control_flow = ControlFlow::Exit,
+            } => *control_flow = winit::event_loop::ControlFlow::Exit,
             _ => {}
         },
         _ => {}
