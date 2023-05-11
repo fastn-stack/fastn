5ccea8c4f1cea1aa277ddc715ebb73be2f3775b4
Amit Upadhyay <upadhyay@gmail.com>
2023-05-11 15:31:19 +0530

    wip

--- fastn-surface/src/wgpu/render.rs
+++ fastn-surface/src/wgpu/render.rs
@@ -9,6 +9,7 @@ struct State {
     window: winit::window::Window,
     clear_color: wgpu::Color,
     render_pipeline: wgpu::RenderPipeline,
+    render_pipeline2: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -123,6 +124,46 @@ impl State {
             multiview: None,
         });
 
+        let shader2 = device.create_shader_module(wgpu::include_wgsl!("shader2.wgsl"));
+
+        let render_pipeline2 = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader2,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader2,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+
         State {
             surface,
             device,
@@ -131,6 +172,7 @@ impl State {
             window,
             config,
             render_pipeline,
+            render_pipeline2,
             clear_color: wgpu::Color {
                 r: 0.2,
                 g: 0.2,
@@ -160,12 +202,12 @@ impl State {
         let view = output
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
+
         let mut encoder = self
             .device
             .create_command_encoder(&wgpu::CommandEncoderDescriptor {
                 label: Some("Render Encoder"),
             });
-
         {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
@@ -181,6 +223,8 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
+            render_pass.set_pipeline(&self.render_pipeline2);
             render_pass.draw(0..3, 0..1);
         }
 
