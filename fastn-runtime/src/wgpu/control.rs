9a94058d691c94f9ce78e420d3a266c4b87cc3b0
Amit Upadhyay <upadhyay@gmail.com>
2023-05-13 10:20:26 +0530

    cargo fmt

--- fastn-runtime/src/wgpu/control.rs
+++ fastn-runtime/src/wgpu/control.rs
@@ -1,9 +1,11 @@
-impl From<fastn_runtime::ControlFlow> for  winit::event_loop::ControlFlow {
-    fn from(value: fastn_runtime::ControlFlow ) -> Self {
+impl From<fastn_runtime::ControlFlow> for winit::event_loop::ControlFlow {
+    fn from(value: fastn_runtime::ControlFlow) -> Self {
         match value {
             fastn_runtime::ControlFlow::Exit => winit::event_loop::ControlFlow::ExitWithCode(0),
             fastn_runtime::ControlFlow::WaitForEvent => winit::event_loop::ControlFlow::Wait,
-            fastn_runtime::ControlFlow::WaitForEventTill(value) => winit::event_loop::ControlFlow::WaitUntil(value),
+            fastn_runtime::ControlFlow::WaitForEventTill(value) => {
+                winit::event_loop::ControlFlow::WaitUntil(value)
+            }
         }
     }
 }
