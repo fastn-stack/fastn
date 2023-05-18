a3ccd0de4c989f1128966bd21cb64f8ef9bab62c
Amit Upadhyay <upadhyay@gmail.com>
2023-05-18 13:08:38 +0530

    added fastn.{root_container,add_child}() methods in wasm

--- fastn-runtime/src/main.rs
+++ fastn-runtime/src/main.rs
@@ -3,16 +3,17 @@ async fn main() {
     let document = fastn_runtime::Document::new(
         r#"
         (module
-            (import "" "" (func $create_column (result externref)))
+            (import "fastn" "create_column" (func $create_column (result externref)))
+            (import "fastn" "root_container" (func $root_container (result externref)))
+
+            ;; fastn.add_child(parent: NodeKey, child: NodeKey)
+            (import "fastn" "add_child" (func $add_child (param externref externref)))
 
             (func (export "main")
                 ;; -- ftd.column:
                 (call $create_column)
-                (drop)
-
-                ;; -- ftd.column:
-                (call $create_column)
-                (drop)
+                (call $root_container)
+                (call $add_child)
             )
         )
     "#,
