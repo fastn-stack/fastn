443e538b5155a0352c1f7be5e75066acbf5971f2
Amit Upadhyay <upadhyay@gmail.com>
2023-05-18 15:39:45 +0530

    set height and width of column from wasm

--- fastn-runtime/src/main.rs
+++ fastn-runtime/src/main.rs
@@ -5,15 +5,18 @@ async fn main() {
         (module
             (import "fastn" "create_column" (func $create_column (result externref)))
             (import "fastn" "root_container" (func $root_container (result externref)))
+            (import "fastn" "set_column_width_px" (func $set_column_width_px (param externref i32)))
+            (import "fastn" "set_column_height_px" (func $set_column_height_px (param externref i32)))
 
             ;; fastn.add_child(parent: NodeKey, child: NodeKey)
             (import "fastn" "add_child" (func $add_child (param externref externref)))
 
-            (func (export "main")
+            (func (export "main") (local $column externref)
                 ;; -- ftd.column:
-                (call $root_container)
-                (call $create_column)
-                (call $add_child)
+                (local.set $column (call $create_column))
+                (call $add_child (call $root_container) (local.get $column))
+                (call $set_column_width_px (local.get $column) (i32.const 600))
+                (call $set_column_height_px (local.get $column) (i32.const 400))
             )
         )
     "#,
