8996db0a0437688794a398f6ec5e93b397df221c
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-05-22 19:39:58 +0530

    Fix: create-db.sql

--- fastn-package/create-db.sql
+++ fastn-package/create-db.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS main_package;
+DROP TABLE IF EXISTS static_files;
 
 CREATE TABLE main_package (
     name TEXT NOT NULL PRIMARY KEY
@@ -7,5 +8,5 @@ CREATE TABLE main_package (
 CREATE TABLE static_files (
     name TEXT NOT NULL PRIMARY KEY,
     content_type TEXT NOT NULL,
-    content_hash TEXT NULL,
+    content_hash TEXT NULL
 ) WITHOUT ROWID;
