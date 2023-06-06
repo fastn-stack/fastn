2ec41ccd1985265f5930b9e3aacf27e4001181b8
Amit Upadhyay <upadhyay@gmail.com>
2023-06-06 18:57:41 +0530

    wasmfmt dependency gone

--- fastn-wasm/src/memory.rs
+++ fastn-wasm/src/memory.rs
@@ -6,7 +6,17 @@ pub struct Memory {
 
 impl Memory {
     pub fn to_doc(&self) -> pretty::RcDoc<'static> {
-        todo!()
+        let limits_wat = self.limits.to_wat();
+        let shared = if self.shared {
+            " shared".to_string()
+        } else {
+            String::new()
+        };
+
+        fastn_wasm::named(
+            "memory",
+            Some(pretty::RcDoc::text(format!("{}{}", limits_wat, shared))),
+        )
     }
 
     pub fn to_wat(&self) -> String {
@@ -18,36 +28,26 @@ impl Memory {
         };
         format!("(memory {}{})", limits_wat, shared)
     }
-
-    #[cfg(test)]
-    pub fn to_wat_formatted(&self) -> String {
-        wasmfmt::fmt(
-            &self.to_wat(),
-            wasmfmt::Options {
-                resolve_names: false,
-            },
-        )
-        .replace("\t", "    ")
-    }
 }
 
 #[cfg(test)]
 mod test {
+    #[track_caller]
+    fn e(f: fastn_wasm::Memory, s: &str) {
+        let g = fastn_wasm::encode_new(&vec![fastn_wasm::Ast::Memory(f)]);
+        println!("got: {}", g);
+        println!("expected: {}", s);
+        assert_eq!(g, s);
+    }
+
     #[test]
     fn test() {
-        assert_eq!(
+        e(
             fastn_wasm::Memory {
                 shared: false,
                 limits: fastn_wasm::Limits { min: 2, max: None },
-            }
-            .to_wat_formatted(),
-            indoc::indoc!(
-                r#"
-                (module
-                    (memory 2)
-                )
-            "#
-            )
+            },
+            "(module (memory 2))",
         );
     }
 }
