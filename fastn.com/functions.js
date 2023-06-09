bc9243765e510267ea94d1804f98b230ea6791c2
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-06-10 03:35:34 +0530

    minor changes

--- fastn.com/functions.js
+++ fastn.com/functions.js
@@ -9,3 +9,20 @@ function add_sub(checked, a, answer) {
         return checked -answer;
     }
 }
+
+function submit_correct_answer(correct, number_correct, answers) {
+    if (number_correct == answers) {
+        return correct + 1;
+    } else {
+        return correct;
+    }
+}
+
+
+function submit_wrong_answer(wrong, number_correct, answers) {
+    if (number_correct == answers) {
+        return wrong;
+    } else {
+        return wrong + 1;
+    }
+}
