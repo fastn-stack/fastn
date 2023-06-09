fa175dcc49b33e27dcc58764a7fc37008c04b55a
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-06-10 03:00:45 +0530

    minor changes

--- fastn.com/functions.js
+++ fastn.com/functions.js
@@ -1,3 +1,11 @@
 function show_alert(a) {
     alert(a);
 }
+
+function add_sub(checked, a, answer) {
+    if (a) {
+        return checked +answer;
+    } else {
+        return checked -answer;
+    }
+}
