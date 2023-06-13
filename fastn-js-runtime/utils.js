966a2fc922b55fc8a6d42aa5cc0630d7fad9c13d
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-06-14 00:39:50 +0530

    Working: 08-dynamic-dom-1.html

--- fastn-js-runtime/utils.js
+++ fastn-js-runtime/utils.js
@@ -21,29 +21,53 @@ window.fastn_utils = {
         }
     },
     deepEqual(obj1, obj2) {
+        // Check for strict equality
         if (obj1 === obj2) {
             return true;
         }
-        // Check if the objects are of the same type
+        // Check for non-object types and null values
         if (typeof obj1 !== 'object' || obj1 === null || typeof obj2 !== 'object' || obj2 === null) {
             return false;
         }
-
-        // Check if the objects have the same keys
+        // Check for equal number of keys
         const keys1 = Object.keys(obj1);
         const keys2 = Object.keys(obj2);
         if (keys1.length !== keys2.length) {
             return false;
         }
-
-        // Check if the values of the keys are deeply equal
+        // Recursively compare values of each key
         for (let key of keys1) {
             if (!this.deepEqual(obj1[key], obj2[key])) {
                 return false;
             }
         }
-
         // Objects are deeply equal
         return true;
+    },
+
+    getAllMutables(obj) {
+        const nodes = [];
+
+        function traverse(obj) {
+            if (obj instanceof fastn.mutableClass) {
+                nodes.push(obj);
+            }
+
+            if (typeof obj === 'object' && obj !== null) {
+                for (let key in obj) {
+                    traverse(obj[key]);
+                }
+            }
+        }
+
+        traverse(obj);
+
+        return nodes;
+    },
+
+    newMutables(oldValue, newValue) {
+        const oldMutables = new Set(fastn_utils.getAllMutables(oldValue));
+        const newMutables = fastn_utils.getAllMutables(newValue).filter(mutable => !oldMutables.has(mutable));
+        return newMutables;
     }
 }
