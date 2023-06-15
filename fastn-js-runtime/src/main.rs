08ca94b9a86ad3a657cdec64580129b644e20046
Amit Upadhyay <upadhyay@gmail.com>
2023-06-15 18:31:26 +0530

    rquickjs 0.2 is not working, using 0.1

--- fastn-js-runtime/src/main.rs
+++ fastn-js-runtime/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
-    fastn_js_runtime::ssr();
+    let start = std::time::Instant::now();
+    println!("{}", fastn_js_runtime::ssr(js()));
+    println!("elapsed: {:?}", start.elapsed());
 }
 
-fn js() {
-
+fn js() -> &'static str {
+    return r#"
+        function main (root) {
+            let number = 10;
+            let i = fastn_dom.createKernel(root, fastn_dom.ElementKind.Integer);
+            i.setStaticProperty(fastn_dom.PropertyKind.IntegerValue, number);
+            i.done();
+        }
+    "#;
 }
