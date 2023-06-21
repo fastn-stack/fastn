66ac23705488c221403e02a6fb0313547dbd4023
Amit Upadhyay <upadhyay@gmail.com>
2023-06-21 15:32:59 +0530

    Statement -> ComponentStatement

--- fastn-js/src/component.rs
+++ fastn-js/src/component.rs
@@ -1,10 +1,10 @@
 pub struct Component {
     name: String,
     params: Vec<String>,
-    pub body: Vec<fastn_js::Statement>,
+    pub body: Vec<fastn_js::ComponentStatement>,
 }
 
-pub fn component0(name: &str, body: Vec<fastn_js::Statement>) -> fastn_js::Ast {
+pub fn component0(name: &str, body: Vec<fastn_js::ComponentStatement>) -> fastn_js::Ast {
     fastn_js::Ast::Component(Component {
         name: name.to_string(),
         params: vec!["parent".to_string()],
@@ -12,7 +12,11 @@ pub fn component0(name: &str, body: Vec<fastn_js::Statement>) -> fastn_js::Ast {
     })
 }
 
-pub fn component1(name: &str, arg1: &str, body: Vec<fastn_js::Statement>) -> fastn_js::Ast {
+pub fn component1(
+    name: &str,
+    arg1: &str,
+    body: Vec<fastn_js::ComponentStatement>,
+) -> fastn_js::Ast {
     fastn_js::Ast::Component(Component {
         name: name.to_string(),
         params: vec!["parent".to_string(), arg1.to_string()],
@@ -24,7 +28,7 @@ pub fn component2(
     name: &str,
     arg1: &str,
     arg2: &str,
-    body: Vec<fastn_js::Statement>,
+    body: Vec<fastn_js::ComponentStatement>,
 ) -> fastn_js::Ast {
     fastn_js::Ast::Component(Component {
         name: name.to_string(),
