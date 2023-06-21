4ffec2885e9cb61034a9c2b2e2c8a35914e1ad53
Amit Upadhyay <upadhyay@gmail.com>
2023-06-21 20:01:11 +0530

    all to_js stuff in to_js module

--- fastn-js/src/component.rs
+++ fastn-js/src/component.rs
@@ -1,6 +1,6 @@
 pub struct Component {
-    name: String,
-    params: Vec<String>,
+    pub name: String,
+    pub params: Vec<String>,
     pub body: Vec<fastn_js::ComponentStatement>,
 }
 
@@ -35,63 +35,4 @@ pub fn component2(
         params: vec!["parent".to_string(), arg1.to_string(), arg2.to_string()],
         body,
     })
-}
-
-impl Component {
-    pub fn to_js(&self) -> pretty::RcDoc<'static> {
-        pretty::RcDoc::text("function")
-            .append(pretty::RcDoc::space())
-            .append(pretty::RcDoc::text(self.name.clone()))
-            .append(pretty::RcDoc::text("("))
-            .append(
-                pretty::RcDoc::intersperse(
-                    self.params
-                        .iter()
-                        .map(|v| pretty::RcDoc::text(v.to_string())),
-                    pretty::RcDoc::text(",").append(pretty::RcDoc::space()),
-                )
-                .nest(4)
-                .group(),
-            )
-            .append(pretty::RcDoc::text(")"))
-            .append(pretty::RcDoc::softline_())
-            .append(
-                pretty::RcDoc::softline()
-                    .append(pretty::RcDoc::text("{"))
-                    .append(pretty::RcDoc::softline_())
-                    .append(
-                        pretty::RcDoc::intersperse(
-                            self.body.iter().map(|v| v.to_js()),
-                            pretty::RcDoc::softline(),
-                        )
-                        .group()
-                        .nest(4),
-                    )
-                    .append(pretty::RcDoc::softline_())
-                    .append(pretty::RcDoc::text("}"))
-                    .group(),
-            )
-    }
-}
-
-#[cfg(test)]
-#[track_caller]
-pub fn e(f: fastn_js::Ast, s: &str) {
-    let g = fastn_js::to_js(&vec![f]);
-    println!("got: {}", g);
-    println!("expected: {}", s);
-    assert_eq!(g, s);
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_func() {
-        let func = fastn_js::component0("foo", vec![]);
-        fastn_js::component::e(func, "function foo(parent) {}");
-        let func = fastn_js::component1("foo", "p", vec![]);
-        fastn_js::component::e(func, "function foo(parent, p) {}");
-        let func = fastn_js::component2("foo", "p", "q", vec![]);
-        fastn_js::component::e(func, "function foo(parent, p, q) {}");
-    }
 }
--- fastn-js/src/to_js.rs
+++ fastn-js/src/to_js.rs
@@ -50,6 +50,43 @@ impl fastn_js::ComponentStatement {
     }
 }
 
+impl fastn_js::Component {
+    pub fn to_js(&self) -> pretty::RcDoc<'static> {
+        pretty::RcDoc::text("function")
+            .append(pretty::RcDoc::space())
+            .append(pretty::RcDoc::text(self.name.clone()))
+            .append(pretty::RcDoc::text("("))
+            .append(
+                pretty::RcDoc::intersperse(
+                    self.params
+                        .iter()
+                        .map(|v| pretty::RcDoc::text(v.to_string())),
+                    pretty::RcDoc::text(",").append(pretty::RcDoc::space()),
+                )
+                .nest(4)
+                .group(),
+            )
+            .append(pretty::RcDoc::text(")"))
+            .append(pretty::RcDoc::softline_())
+            .append(
+                pretty::RcDoc::softline()
+                    .append(pretty::RcDoc::text("{"))
+                    .append(pretty::RcDoc::softline_())
+                    .append(
+                        pretty::RcDoc::intersperse(
+                            self.body.iter().map(|v| v.to_js()),
+                            pretty::RcDoc::softline(),
+                        )
+                        .group()
+                        .nest(4),
+                    )
+                    .append(pretty::RcDoc::softline_())
+                    .append(pretty::RcDoc::text("}"))
+                    .group(),
+            )
+    }
+}
+
 impl fastn_js::MutableVariable {
     pub fn to_js(&self) -> pretty::RcDoc<'static> {
         pretty::RcDoc::text("let")
@@ -67,5 +104,124 @@ impl fastn_js::MutableVariable {
                 pretty::RcDoc::text(self.value.clone())
             })
             .append(pretty::RcDoc::text(");"))
+    }
+}
+
+impl fastn_js::StaticVariable {
+    pub fn to_js(&self) -> pretty::RcDoc<'static> {
+        pretty::RcDoc::text("let")
+            .append(pretty::RcDoc::space())
+            .append(pretty::RcDoc::text(self.name.clone()))
+            .append(pretty::RcDoc::space())
+            .append(pretty::RcDoc::text("="))
+            .append(pretty::RcDoc::space())
+            .append(if self.is_quoted {
+                pretty::RcDoc::text("\"")
+                    .append(pretty::RcDoc::text(self.value.replace("\n", "\\n")))
+                    .append(pretty::RcDoc::text("\""))
+            } else {
+                pretty::RcDoc::text(self.value.clone())
+            })
+            .append(pretty::RcDoc::text(";"))
+    }
+}
+
+impl fastn_js::UDF {
+    pub fn to_js(&self) -> pretty::RcDoc<'static> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+#[track_caller]
+pub fn e(f: fastn_js::Ast, s: &str) {
+    let g = fastn_js::to_js(&vec![f]);
+    println!("got: {}", g);
+    println!("expected: {}", s);
+    assert_eq!(g, s);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_func() {
+        let func = fastn_js::component0("foo", vec![]);
+        fastn_js::to_js::e(func, "function foo(parent) {}");
+        let func = fastn_js::component1("foo", "p", vec![]);
+        fastn_js::to_js::e(func, "function foo(parent, p) {}");
+        let func = fastn_js::component2("foo", "p", "q", vec![]);
+        fastn_js::to_js::e(func, "function foo(parent, p, q) {}");
+    }
+
+    #[test]
+    fn unquoted() {
+        fastn_js::to_js::e(
+            fastn_js::component0("foo", vec![fastn_js::mutable_unquoted("bar", "10")]),
+            r#"function foo(parent) {let bar = fastn.mutable(10);}"#,
+        );
+    }
+
+    #[test]
+    fn quoted() {
+        fastn_js::to_js::e(
+            fastn_js::component0("foo", vec![fastn_js::mutable_quoted("bar", "10")]),
+            r#"function foo(parent) {let bar = fastn.mutable("10");}"#,
+        );
+        fastn_js::to_js::e(
+            fastn_js::component0("foo", vec![fastn_js::mutable_quoted("bar", "hello world")]),
+            r#"function foo(parent) {let bar = fastn.mutable("hello world");}"#,
+        );
+        fastn_js::to_js::e(
+            fastn_js::component0("foo", vec![fastn_js::mutable_quoted("bar", "hello world, a long long long long long string which keeps going on and on and on and on till we run out of line space and still keeps going on and on")]),
+            indoc::indoc!(
+                r#"function foo(parent) {
+                let bar = fastn.mutable("hello world, a long long long long long string which keeps going on and on and on and on till we run out of line space and still keeps going on and on");
+                }"#),
+        );
+        fastn_js::to_js::e(
+            fastn_js::component0("foo", vec![fastn_js::mutable_quoted("bar", "hello\nworld")]),
+            r#"function foo(parent) {let bar = fastn.mutable("hello\nworld");}"#,
+        );
+        // std::fs::write(
+        //     "test.js",
+        //     r#"function foo(parent) {let bar = "hello\nworld";}"#,
+        // )
+        // .unwrap();
+    }
+
+    #[test]
+    fn static_unquoted() {
+        fastn_js::to_js::e(
+            fastn_js::component0("foo", vec![fastn_js::static_unquoted("bar", "10")]),
+            r#"function foo(parent) {let bar = 10;}"#,
+        );
+    }
+
+    #[test]
+    fn static_quoted() {
+        fastn_js::to_js::e(
+            fastn_js::component0("foo", vec![fastn_js::static_quoted("bar", "10")]),
+            r#"function foo(parent) {let bar = "10";}"#,
+        );
+        fastn_js::to_js::e(
+            fastn_js::component0("foo", vec![fastn_js::static_quoted("bar", "hello world")]),
+            r#"function foo(parent) {let bar = "hello world";}"#,
+        );
+        fastn_js::to_js::e(
+            fastn_js::component0("foo", vec![fastn_js::static_quoted("bar", "hello world, a long long long long long string which keeps going on and on and on and on till we run out of line space and still keeps going on and on")]),
+            indoc::indoc!(
+                r#"function foo(parent) {
+                let bar = "hello world, a long long long long long string which keeps going on and on and on and on till we run out of line space and still keeps going on and on";
+                }"#),
+        );
+        fastn_js::to_js::e(
+            fastn_js::component0("foo", vec![fastn_js::static_quoted("bar", "hello\nworld")]),
+            r#"function foo(parent) {let bar = "hello\nworld";}"#,
+        );
+        // std::fs::write(
+        //     "test.js",
+        //     r#"function foo(parent) {let bar = "hello\nworld";}"#,
+        // )
+        // .unwrap();
     }
 }
