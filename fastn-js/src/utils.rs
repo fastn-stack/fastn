921086f847f30dc9e14699c736b5085292998897
Amit Upadhyay <upadhyay@gmail.com>
2023-06-22 15:33:14 +0530

    removed uneeded function

--- fastn-js/src/utils.rs
+++ fastn-js/src/utils.rs
@@ -1,10 +1,3 @@
 pub(crate) fn is_kernel(s: &str) -> bool {
     ["ftd#text", "ftd#row", "ftd#column"].contains(&s)
 }
-
-#[cfg(test)]
-pub fn trim_all_lines(s: &str) -> String {
-    use itertools::Itertools;
-
-    s.split('\n').map(|v| v.trim()).join("\n")
-}
