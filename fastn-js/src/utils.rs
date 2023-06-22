9f70ccfc8cdf2c4adb3a6a10e3cae8656162b2b7
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-06-22 12:22:34 +0530

    Replace rquickjs library with quick-js

--- fastn-js/src/utils.rs
+++ fastn-js/src/utils.rs
@@ -2,6 +2,7 @@ pub(crate) fn is_kernel(s: &str) -> bool {
     ["ftd#text", "ftd#row", "ftd#column"].contains(&s)
 }
 
+#[cfg(test)]
 pub fn trim_all_lines(s: &str) -> String {
     use itertools::Itertools;
 
