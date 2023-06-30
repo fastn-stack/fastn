3cfe7a6ce85d57ba80f9a433bf49133668e32c68
Amit Upadhyay <upadhyay@gmail.com>
2023-06-30 12:59:53 +0530

    moved js files to js folder

--- fastn-js/src/lib.rs
+++ fastn-js/src/lib.rs
@@ -32,10 +32,10 @@ pub use udf::{udf0, udf1, udf2, udf_with_params, UDF};
 pub use udf_statement::UDFStatement;
 
 pub fn all_js() -> String {
-    let fastn_js = include_str!("../fastn.js");
-    let dom_js = include_str!("../dom.js");
-    let utils_js = include_str!("../utils.js");
-    let virtual_js = include_str!("../virtual.js");
-    let variables_js = include_str!("../variables.js");
+    let fastn_js = include_str!("../js/fastn.js");
+    let dom_js = include_str!("../js/dom.js");
+    let utils_js = include_str!("../js/utils.js");
+    let virtual_js = include_str!("../js/virtual.js");
+    let variables_js = include_str!("../js/variables.js");
     format!("{fastn_js}{dom_js}{utils_js}{virtual_js}{variables_js}")
 }
