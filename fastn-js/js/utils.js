5396df80212b9bee22c202ae903d0fedfbb8a703
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-07-03 21:50:44 +0530

    Minor fixes in js

--- fastn-js/js/utils.js
+++ fastn-js/js/utils.js
@@ -12,100 +12,18 @@ let fastn_utils = {
             node = "img";
         } else if (kind === fastn_dom.ElementKind.Div) {
             node = "div";
-            css = [];
         } else if (kind === fastn_dom.ElementKind.Text) {
             // css.push("ft_text");
         }
         return [node, css];
     },
+
     getStaticValue(obj) {
         if (obj instanceof fastn.mutableClass) {
            return this.getStaticValue(obj.get());
         } else {
            return obj;
         }
-    },
-    deepEqual(obj1, obj2) {
-        // Check for strict equality
-        if (obj1 === obj2) {
-            return true;
-        }
-        // Check for non-object types and null values
-        if (typeof obj1 !== 'object' || obj1 === null || typeof obj2 !== 'object' || obj2 === null) {
-            return false;
-        }
-        // Check for class instances
-        if (obj1.constructor !== obj2.constructor) {
-            return false;
-        }
-        // Check for equal number of keys
-        const keys1 = Object.keys(obj1);
-        const keys2 = Object.keys(obj2);
-        if (keys1.length !== keys2.length) {
-            return false;
-        }
-        // Recursively compare values of each key
-        for (let key of keys1) {
-            if (!this.deepEqual(obj1[key], obj2[key])) {
-                return false;
-            }
-        }
-        // Check for class instance variables
-        if (obj1 instanceof fastn.mutableClass && obj2 instanceof fastn.mutableClass) {
-            return obj1.equalMutable(obj2);
-        }
-        // Objects are deeply equal
-        return true;
-    },
-
-    /**
-     * Retrieves all mutables present in an object.
-     * This function recursively traverses the object and collects all instances
-     * of mutable classes, storing them in an array.
-     *
-     * @param {Object} obj - The object to traverse.
-     * @returns {Array} - An array containing all found mutables.
-     */
-    getAllMutables(obj) {
-        const nodes = [];
-
-        function traverse(obj) {
-            if (obj instanceof fastn.mutableClass) {
-                nodes.push(obj);
-            }
-
-            if (typeof obj === 'object' && obj !== null) {
-                for (let key in obj) {
-                    traverse(obj[key]);
-                }
-            }
-        }
-
-        traverse(obj);
-
-        return nodes;
-    },
-
-    /**
-     * This function compares the mutables found in both old and new values and returns
-     * an array of mutables that are present in the new value but not in the old value and
-     * also an array of mutables that are present in the old value but not in the new value
-     *
-     * @param {any} oldValue - The old value to compare.
-     * @param {any} newValue - The new value to compare.
-     * @returns {{newMutables: *[], oldMutables: *[]}} - An object containing 'newMutables' and 'oldMutables' arrays.
-     */
-    getNewAndOldMutables(oldValue, newValue) {
-        const oldMutables = this.getAllMutables(oldValue);
-        const newMutables = this.getAllMutables(newValue);
-
-        const newMutablesOnly = newMutables.filter(mutable => !oldMutables.includes(mutable));
-        const oldMutablesOnly = oldMutables.filter(mutable => !newMutables.includes(mutable));
-
-        return {
-            newMutables: newMutablesOnly,
-            oldMutables: oldMutablesOnly
-        };
     },
 
     getter(value) {
@@ -139,7 +57,7 @@ let fastn_utils = {
     getRoleValues(value) {
         return {
             "font-family": fastn_utils.getStaticValue(value.get("font_family")),
-            "letter-spacing":fastn_utils.getStaticValue(value.get("letter_spacing")),
+            "letter-spacing": fastn_utils.getStaticValue(value.get("letter_spacing")),
             "font-size": fastn_utils.getStaticValue(value.get("size")),
             "font-weight": fastn_utils.getStaticValue(value.get("weight")),
             "line-height": fastn_utils.getStaticValue(value.get("line_height")),
