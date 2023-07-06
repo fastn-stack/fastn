3c2bb50fcaf5be81300e30578bc8c5bda9a93c53
theharshsingh <harshsingh.js@gmail.com>
2023-07-06 14:15:41 +0530

    Fixed Semicolon Bug and Added String Support in extract_arguments function

--- fastn-core/src/library2022/processor/sql.rs
+++ fastn-core/src/library2022/processor/sql.rs
@@ -1,66 +1,85 @@
+const BACKSLASH: char = '\\';
+
+const SPECIAL_CHARS: [char; 9] = [BACKSLASH, '$', '/', ':', '"', ',', '\'', ';', ' '];
+
+// TODO: Can improve the performance
+// Maybe I should use RegEx?
+
 pub(crate) fn extract_arguments(query: &str) -> ftd::interpreter::Result<(String, Vec<String>)> {
     let chars: Vec<char> = query.chars().collect();
     let len = chars.len();
     let mut i = 0;
-    let mut found_eq = false;
+    let mut quote: Option<char> = None;
+    let mut quote_open = false;
     let mut escaped = false;
     let mut args: Vec<String> = Vec::new();
     let mut output_query = String::new();
 
     while i < len {
-        if chars[i] == '=' {
-            found_eq = true;
-        }
-
-        if chars[i] == '\\' {
+        if chars[i] == BACKSLASH {
             escaped = true;
-
             let mut escape_count = 0;
 
-            while i < len && chars[i] == '\\' {
+            while i < len && chars[i] == BACKSLASH {
                 escape_count += 1;
                 i += 1;
             }
 
             if escape_count % 2 == 0 {
-                output_query += &"\\".repeat(escape_count);
+                output_query += &BACKSLASH.to_string().repeat(escape_count);
                 escaped = false;
             }
         }
 
-        if found_eq && chars[i] == '$' && !escaped {
+        if let Some(quote_char) = quote {
+            if chars[i] == quote_char && !escaped {
+                if quote_open {
+                    quote_open = false;
+                    quote = None;
+                }
+            } else {
+                quote_open = true;
+                quote = Some(chars[i]);
+            }
+        }
+
+        if chars[i] == '$' && !escaped && !quote_open {
             let mut arg = String::new();
             i += 1;
 
-            while i < len && chars[i] != ' ' {
-                arg.push(chars[i]);
-                i += 1;
-            }
-
-            if !arg.is_empty() {
-                let index = args.iter().position(|x| x == &arg);
-
-                let gap = if i < len { " " } else { "" };
-
-                if let Some(idx) = index {
-                    output_query += &format!("${}{}", idx + 1, gap);
+            while i < len {
+                if SPECIAL_CHARS.contains(&chars[i]) {
+                    i -= 1;
+                    break;
                 } else {
-                    args.push(arg.clone());
-                    output_query += &format!("${}{}", args.len(), gap);
+                    arg.push(chars[i]);
+                    i += 1;
                 }
             }
 
-            found_eq = false;
+            if !arg.is_empty() {
+                if let Some(index) = args.iter().position(|x| x == &arg) {
+                    output_query += &format!("${}", index + 1);
+                } else {
+                    args.push(arg.clone());
+                    let index = args.len();
+                    output_query += &format!("${}", index);
+                }
+            }
         } else {
             if escaped {
-                output_query += "\\";
+                output_query += &BACKSLASH.to_string();
                 escaped = false;
             }
-
             output_query.push(chars[i]);
         }
 
         i += 1;
+    }
+
+    if quote_open {
+        // TODO: THROW SOME ERROR, A QUOTE WAS LEFT OPEN
+        println!("Quote Open");
     }
 
     Ok((output_query, args))
