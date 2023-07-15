42f1397335d134807b8180e0b6c40c3c5a393a2e
Amit Upadhyay <upadhyay@gmail.com>
2023-07-15 20:12:46 +0530

    wip checkin, some types borrowed from tracing-forest

--- fastn-observer/src/lib.rs
+++ fastn-observer/src/lib.rs
@@ -1,3 +1,17 @@
+extern crate self as fastn_observer;
+
+mod duration_display;
+mod field;
+mod layer;
+mod opened_span;
+mod tree;
+
+pub(crate) use duration_display::DurationDisplay;
+pub use field::{Field, FieldSet};
+pub use layer::Layer;
+pub use opened_span::OpenedSpan;
+pub use tree::{Event, Shared, Span, Tree};
+
 pub fn observe() {
     use tracing_subscriber::layer::SubscriberExt;
 
@@ -24,70 +38,4 @@ pub fn observe() {
 
 pub fn is_traced() -> bool {
     std::env::var("TRACING").is_ok() || std::env::args().any(|e| e == "--trace")
-}
-
-pub struct Layer {
-    start: std::time::Instant,
-}
-
-impl Layer {
-    pub fn new() -> Self {
-        Self {
-            start: std::time::Instant::now(),
-        }
-    }
-    fn dbg(&self, message: String) {
-        let duration = DurationDisplay(self.start.elapsed().as_nanos() as f64);
-        println!("{duration} {message}");
-    }
-}
-
-impl<S> tracing_subscriber::Layer<S> for Layer
-where
-    S: tracing::Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a> + std::fmt::Debug,
-{
-    fn on_new_span(
-        &self,
-        attrs: &tracing::span::Attributes,
-        id: &tracing::Id,
-        _ctx: tracing_subscriber::layer::Context<S>,
-    ) {
-        self.dbg(format!("on_new_span: {id:?} {attrs:#?}"));
-    }
-
-    fn on_event(&self, event: &tracing::Event, _ctx: tracing_subscriber::layer::Context<S>) {
-        self.dbg(format!("on_event: {event:#?}"));
-    }
-
-    fn on_enter(&self, id: &tracing::Id, _ctx: tracing_subscriber::layer::Context<S>) {
-        self.dbg(format!("on_enter: {id:?}"));
-    }
-
-    fn on_exit(&self, id: &tracing::Id, _ctx: tracing_subscriber::layer::Context<S>) {
-        self.dbg(format!("on_exit: {id:?}"));
-    }
-
-    fn on_close(&self, id: tracing::Id, _ctx: tracing_subscriber::layer::Context<S>) {
-        self.dbg(format!("on_close: {id:?}"));
-    }
-}
-
-struct DurationDisplay(f64);
-
-// Taken from chrono
-impl std::fmt::Display for DurationDisplay {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let mut t = self.0;
-        for unit in ["ns", "µs", "ms", "s"] {
-            if t < 10.0 {
-                return write!(f, "{:.2}{}", t, unit);
-            } else if t < 100.0 {
-                return write!(f, "{:.1}{}", t, unit);
-            } else if t < 1000.0 {
-                return write!(f, "{:.0}{}", t, unit);
-            }
-            t /= 1000.0;
-        }
-        write!(f, "{:.0}s", t * 1000.0)
-    }
 }
---
+++ fastn-observer/src/tree.rs
@@ -0,0 +1,45 @@
+#[derive(Debug)]
+pub enum Tree {
+    Event(Event),
+    Span(Span),
+}
+
+#[derive(Debug)]
+pub struct Event {
+    pub(crate) shared: Shared,
+    pub(crate) message: Option<String>,
+}
+
+#[derive(Debug)]
+pub struct Span {
+    pub(crate) shared: Shared,
+    pub(crate) name: &'static str,
+    pub(crate) total_duration: std::time::Duration,
+    pub(crate) inner_duration: std::time::Duration,
+    pub(crate) nodes: Vec<Tree>,
+}
+
+#[derive(Debug)]
+pub struct Shared {
+    pub(crate) level: tracing::Level,
+    pub(crate) fields: fastn_observer::FieldSet,
+    /// when did this event occur, with respect to immediate parent start
+    pub(crate) on: std::time::Duration,
+}
+
+impl Span {
+    pub(crate) fn new(shared: Shared, name: &'static str) -> Self {
+        Span {
+            shared,
+            name,
+            total_duration: std::time::Duration::ZERO,
+            inner_duration: std::time::Duration::ZERO,
+            nodes: Vec::new(),
+        }
+    }
+
+    /// Returns the duration this span was entered, but not in any child spans.
+    pub fn base_duration(&self) -> std::time::Duration {
+        self.total_duration - self.inner_duration
+    }
+}
