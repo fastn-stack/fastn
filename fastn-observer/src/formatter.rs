b7dfde52f5d7e77520fc37426d0a7a62529af9c6
Amit Upadhyay <upadhyay@gmail.com>
2023-07-16 15:09:37 +0530

    debug cleanups

--- fastn-observer/src/formatter.rs
+++ fastn-observer/src/formatter.rs
@@ -1,13 +1,13 @@
 // borrowed from https://github.com/QnnOkabayashi/tracing-forest/ (license: MIT)
 
 pub fn write_immediate<S>(
-    event: &fastn_observer::Event,
+    _event: &fastn_observer::Event,
     _current: Option<&tracing_subscriber::registry::SpanRef<S>>,
 ) -> std::io::Result<()>
 where
     S: for<'a> tracing_subscriber::registry::LookupSpan<'a>,
 {
-    dbg!(event);
+    // dbg!(event);
     Ok(())
 }
 
