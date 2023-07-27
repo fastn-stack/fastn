f70c806cdf7e1c4aeb4677d79ab97aea58820c5c
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-07-28 00:47:43 +0530

    Fix interpreter: inherited for ftd::interpreter::PropertyValue

--- ftd/src/interpreter/things/value.rs
+++ ftd/src/interpreter/things/value.rs
@@ -1010,19 +1010,54 @@ impl PropertyValue {
                 if expression
                     .starts_with(format!("${}.", ftd::interpreter::FTD_INHERITED).as_str()) =>
             {
-                if let Some(kind) = expected_kind {
-                    Ok(ftd::interpreter::StateWithThing::new_thing(Some(
-                        ftd::interpreter::PropertyValue::Reference {
-                            name: expression.trim_start_matches('$').to_string(),
-                            kind: kind.to_owned(),
-                            source: PropertyValueSource::Global,
-                            is_mutable: false,
-                            line_number: 0,
-                        },
-                    )))
-                } else {
-                    ftd::interpreter::utils::e2("Kind not found", doc.name, value.line_number())
+                let reference = expression
+                    .trim_start_matches(ftd::interpreter::utils::REFERENCE)
+                    .to_string();
+
+                let (_, found_kind, _) = try_ok_state!(doc.get_kind_with_argument(
+                    reference.as_str(),
+                    value.line_number(),
+                    definition_name_with_arguments,
+                    loop_object_name_and_kind,
+                )?);
+
+                match expected_kind {
+                    Some(ekind)
+                        if !ekind.kind.is_same_as(&found_kind.kind)
+                            && (ekind.kind.ref_inner().is_record()
+                                || ekind.kind.ref_inner().is_or_type()) =>
+                    {
+                        return Ok(PropertyValue::value_from_ast_value(
+                            value,
+                            doc,
+                            mutable,
+                            expected_kind,
+                            definition_name_with_arguments,
+                            loop_object_name_and_kind,
+                        )?
+                        .map(Some));
+                    }
+                    Some(ekind) if !ekind.kind.is_same_as(&found_kind.kind) => {
+                        return ftd::interpreter::utils::e2(
+                            format!("3 Expected kind `{:?}`, found: `{:?}`", ekind, found_kind)
+                                .as_str(),
+                            doc.name,
+                            value.line_number(),
+                        )
+                    }
+                    _ => {}
                 }
+                let kind = get_kind(expected_kind, &found_kind);
+
+                Ok(ftd::interpreter::StateWithThing::new_thing(Some(
+                    ftd::interpreter::PropertyValue::Reference {
+                        name: reference,
+                        kind: kind.to_owned(),
+                        source: PropertyValueSource::Global,
+                        is_mutable: false,
+                        line_number: 0,
+                    },
+                )))
             }
             Ok(expression) if expression.eq(ftd::interpreter::FTD_SPECIAL_VALUE) => {
                 Ok(ftd::interpreter::StateWithThing::new_thing(Some(
