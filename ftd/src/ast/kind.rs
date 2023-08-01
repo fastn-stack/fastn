2a04d4027b916090fc5c59ffae62a2d63dee221d
heulitig <rithik@fifthtry.com>
2023-08-01 15:44:52 +0530

    minor fix

--- ftd/src/ast/kind.rs
+++ ftd/src/ast/kind.rs
@@ -674,7 +674,7 @@ impl VariableValue {
     pub fn has_request_data_header(&self) -> bool {
         if let Some(ftd::ast::VariableValue::Record { headers, .. }) = self.inner() {
             for h in headers.0.iter() {
-                if h.key.contains("processor") {
+                if h.key.trim_end_matches('$').eq("processor") {
                     if let ftd::ast::VariableValue::String { ref value, .. } = h.value {
                         if value.contains("request-data") {
                             return true;
