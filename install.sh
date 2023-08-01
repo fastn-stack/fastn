b284b3daafbb71d23627e046e2fa8aff2d5a4c0f
Harsh Singh <harshsingh.js@gmail.com>
2023-08-01 10:55:48 +0530

    Better error handling

--- install.sh
+++ install.sh
@@ -126,25 +126,29 @@ setup() {
         mv fastn_linux_musl_x86_64.d "${DESTINATION_PATH}/fastn.d"
     fi
 
-
     echo ""
 
-    chmod +x "${DESTINATION_PATH}/fastn"*
-
-    # Add fastn to PATH if not already done
-    update_path
-
-    echo "${FMT_GREEN}╭────────────────────────────────────────╮"
-    echo "│                                        │"
-    echo "│   fastn installation completed         │"
-    echo "│                                        │"
-    echo "│   Restart your terminal to apply       │"
-    echo "│   the changes.                         │"
-    echo "│                                        │"
-    echo "│   Get started with fastn at:           │"
-    echo "│   ${FMT_BLUE}https://fastn.com${FMT_RESET}${FMT_GREEN}                    │"
-    echo "│                                        │"
-    echo "╰────────────────────────────────────────╯${FMT_RESET}"
+    # Check if the destination files are moved successfully before setting permissions
+    if [[ -e "${DESTINATION_PATH}/fastn" ]]; then
+        chmod +x "${DESTINATION_PATH}/fastn"*
+    
+        # Add fastn to PATH if not already done
+        update_path
+    
+        echo "${FMT_GREEN}╭────────────────────────────────────────╮"
+        echo "│                                        │"
+        echo "│   fastn installation completed         │"
+        echo "│                                        │"
+        echo "│   Restart your terminal to apply       │"
+        echo "│   the changes.                         │"
+        echo "│                                        │"
+        echo "│   Get started with fastn at:           │"
+        echo "│   ${FMT_BLUE}https://fastn.com${FMT_RESET}${FMT_GREEN}                    │"
+        echo "│                                        │"
+        echo "╰────────────────────────────────────────╯${FMT_RESET}"
+    else
+        echo "${FMT_RED}ERROR:${FMT_RESET} Installation failed. Please check if you have sufficient permissions to install in $DESTINATION_PATH."
+    fi
 }
 
 main() {
