3b1a67b96b9731b81009116ec809f8cca01118c7
Harsh Singh <harshsingh.js@gmail.com>
2023-08-08 18:18:48 +0530

    toggle-mode function in ftd

--- fastn-js/js/ftd.js
+++ fastn-js/js/ftd.js
@@ -223,3 +223,12 @@ ftd.http = function (url, method, ...request_data) {
     };
     xhr.send(JSON.stringify(json));
 };
+
+ftd.toggle_mode = function () {
+    const is_dark_mode = ftd.get(ftd.dark_mode);
+    if(is_dark_mode) {
+        enable_light_mode();
+    } else {
+        enable_dark_mode();
+    }
+};
