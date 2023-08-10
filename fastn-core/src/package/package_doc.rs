fc6190267765ed2865bf318b0c53b77c7616c463
heulitig <rithik@fifthtry.com>
2023-08-10 14:46:19 +0530

    added buggy js test

--- fastn-core/src/package/package_doc.rs
+++ fastn-core/src/package/package_doc.rs
@@ -512,7 +512,6 @@ pub(crate) async fn read_ftd_2023(
     let js_ast_data = ftd::js::document_into_js_ast(main_ftd_doc);
     let js_document_script = fastn_js::to_js(js_ast_data.asts.as_slice(), true);
     let js_ftd_script = fastn_js::to_js(ftd::js::default_bag_into_js_ast().as_slice(), false);
-    // dbg!(&js_document_script);
     let ssr_body =
         fastn_js::ssr_with_js_string(format!("{js_ftd_script}\n{js_document_script}").as_str());
 
