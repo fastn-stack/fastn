5b1d2bf5da4426da2b137eb512ee99111c7e2d0c
Amit Upadhyay <upadhyay@gmail.com>
2023-08-27 13:57:39 +0530

    fixing tests

--- ftd/src/p1/test.rs
+++ ftd/src/p1/test.rs
@@ -454,15 +454,17 @@ fn with_dash_dash() {
         "#
         ),
         &ftd::p1::Section::with_name("hello")
-            .add_sub_section(ftd::p1::Section::with_name("realm.rr.step.body").and_body(
-                r#"
+            .add_sub_section(
+                ftd::p1::Section::with_name("realm.rr.step.body").and_body(&indoc!(
+                    r#"
                         {
                           "body": "-- h0: Hello World\n\n-- markup:\n\ndemo cr 1\n",
                           "kind": "content",
                           "track": "amitu/index",
                           "version": "2020-11-16T04:13:14.642892+00:00"
                         }"#,
-            ))
+                )),
+            )
             .list(),
     );
 }
@@ -470,7 +472,8 @@ fn with_dash_dash() {
 #[test]
 fn indented_body() {
     p(
-        "
+        &indoc!(
+            "
                  -- markup:
 
                  hello world is
@@ -478,7 +481,8 @@ fn indented_body() {
                      not enough
 
                      lol
-            ",
+            "
+        ),
         &ftd::p1::Section::with_name("markup")
             .and_body("hello world is\n\n    not enough\n\n    lol")
             .list(),
