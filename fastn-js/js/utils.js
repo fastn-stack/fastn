0c355c2d7bc37007c518acb7fd830cb6ce85b0d0
heulitig <rithik@fifthtry.com>
2023-08-30 15:17:04 +0530

    escaped special html characters

--- fastn-js/js/utils.js
+++ fastn-js/js/utils.js
@@ -378,7 +378,28 @@ let fastn_utils = {
 
     flattenArray(arr) {
         return fastn_utils.private.flattenArray([arr]);
-    }
+    },
+
+    escapeSpecialHtmlCharacters(str) {
+        return str.replace(/[&<>"'/]/g, function (match) {
+            switch (match) {
+                case "&":
+                    return "&amp;";
+                case "<":
+                    return "&lt;";
+                case ">":
+                    return "&gt;";
+                case '"':
+                    return "&quot;";
+                case "'":
+                    return "&#39;";
+                case "/":
+                    return "&#x2F;";
+                default:
+                    return match;
+            }
+        });
+    },
 }
 
 
@@ -494,9 +515,6 @@ fastn_utils.private = {
         }
         return text;
     },
-    escapeSpecialCharacters(text) {
-        return text.replaceAll("<", "&lt;");
-    }
 }
 
 
