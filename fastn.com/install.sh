4d2763eb82c07e166007a88a514588b9ce3f1532
Harsh Singh <64768386+harshdoesdev@users.noreply.github.com>
2023-09-13 13:29:23 +0530

    Fixed issues related to fastn installation script on Mac and Linux (#408)
    * Fixed issues related to fastn installation on Mac and Linux
    
    * typo fix
    
    * minor fix
    
    * handle path error
    
    * fixed wrong return type
    
    * log error message once
    
    * handle error in update_path itself
    
    * minor fixes
    
    * error handling
    
    * message to restart term
    
    * message to restart term
    
    * Made the script POSIX compatible

--- fastn.com/install.sh
+++ fastn.com/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # TODO: //
 # This script should be run via curl:
@@ -30,13 +30,13 @@ setup_colors() {
         FMT_ORANGE=""
         FMT_RESET=""
     else
-        FMT_RED=$(printf '\033[31m')
-        FMT_GREEN=$(printf '\033[32m')
-        FMT_YELLOW=$(printf '\033[33m')
-        FMT_BLUE=$(printf '\033[34m')
-        FMT_BOLD=$(printf '\033[1m')
-        FMT_ORANGE=$(printf '\033[38;5;208m')
-        FMT_RESET=$(printf '\033[0m')
+        FMT_RED="$(printf '\033[31m')"
+        FMT_GREEN="$(printf '\033[32m')"
+        FMT_YELLOW="$(printf '\033[33m')"
+        FMT_BLUE="$(printf '\033[34m')"
+        FMT_BOLD="$(printf '\033[1m')"
+        FMT_ORANGE="$(printf '\033[38;5;208m')"
+        FMT_RESET="$(printf '\033[0m')"
     fi
 }
 
@@ -54,9 +54,23 @@ print_fastn_logo() {
     echo "   ?&@@@@B.     :P&@@@@@&BG5YG@@@@@?.     :!YG#&@@@@@@@@@G:    ^P@@@@&^       P@@@@#?      :5&@@@@! "
     echo "   ?&@@@@B.    .Y@@@@&?:    .!&@@@@?.           ..^J#@@@@@Y.   ^P@@@@&^       P@@@@#?      :5&@@@@! "
     echo "   ?&@@@@B.    :P@@@@B:     ?#@@@@@?.  ~#@&@&Y^    :Y&@@@@Y.   :5@@@@@J:.     P@@@@#?      :5&@@@@! "
-    echo "   ?&@@@@B.     7@@@@@&B55G&&@@@@@@?.  .P@@@@@@BGPB&@@@@@B^     !&@@@@@@&P~   P@@@@#?      :5&@@@@! "
-    echo "   ?&@@@@#.      ~P&@@@@@@@B7!@@@@@J.    :Y#@@@@@@@@@@#5~        ~P&@@@@@&5   G@@@@&J      :P@@@@@! "
+    echo "   ?&@@@@B.     7@@@@@&B55G&&@@@@@@?.  .P@@@@@@BGPB&@@@@@B^     !&@@@@@@&P~   P@@@@#?      :P@@@@@! "
+    echo "   ?&@@@@#.      ~P&@@@@@@@B7!@@@@@J.    :Y#@@@@@@@@@@#5~        ~P&@@@@@&5   G@@@@&J      :B@@@@@! "
     echo "    ......         .:^~~^:.   .....         .:^~!!~^:.             .:^~~^..   ......        ......  ${FMT_RESET}"
+}
+
+# Function for logging informational messages
+log_message() {
+    echo "${FMT_GREEN}$1${FMT_RESET}"
+}
+
+# Function for logging error messages
+log_error() {
+    echo "${FMT_RED}ERROR:${FMT_RESET} $1"
+}
+
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
 }
 
 update_path() {
@@ -69,52 +83,73 @@ update_path() {
         shell_config_file="${HOME}/.profile"
     fi
 
+    echo ""
+
     # Check if the path is already added to the shell config file
     if ! grep -qF "export PATH=\"\$PATH:${DESTINATION_PATH}\"" "$shell_config_file"; then
-        echo "export PATH=\"\$PATH:${DESTINATION_PATH}\"" >> "$shell_config_file"
-        echo "Updated the PATH variable in $shell_config_file"
-        echo "Please restart your terminal session to start using fastn."
+        if [ -w "$shell_config_file" ]; then
+            # Add the destination path to the PATH variable in the shell config file
+            echo "export PATH=\"\$PATH:${DESTINATION_PATH}\"" >> "$shell_config_file" &&
+            log_message "✔ Updated the PATH variable in $shell_config_file"
+            return 0
+        else
+            log_error "Failed to add '${DESTINATION_PATH}' to PATH. Insufficient permissions for '$shell_config_file'."
+            return 1
+        fi
+    else
+        log_message "✔ Path is already added to the shell config file: $shell_config_file"
+        return 0
     fi
-}
-
-command_exists() {
-  command -v "$@" >/dev/null 2>&1
 }
 
 setup() {
     print_fastn_logo
 
+    PRE_RELEASE=""
+    CONTROLLER=""
+
     # Parse arguments
     while [ $# -gt 0 ]; do
         case $1 in
             --pre-release) PRE_RELEASE=true ;;
-            --controller) CONTROLLER=true;;
+            --controller) CONTROLLER=true ;;
         esac
     shift
     done
 
-    if [[ $PRE_RELEASE ]]; then
+    echo ""
+
+    if [ -n "$PRE_RELEASE" ]; then
         URL="https://api.github.com/repos/fastn-stack/fastn/releases"
-        echo "Downloading the latest pre-release binaries"
+        log_message "Downloading the latest pre-release binaries"
     else
         URL="https://api.github.com/repos/fastn-stack/fastn/releases/latest"
-        echo "Downloading the latest production ready binaries"
+        log_message "Downloading the latest production-ready binaries"
     fi
 
     DESTINATION_PATH="/usr/local/bin"
 
-    if [ -d "$DESTINATION_PATH" ]; then
+    if [ -d "$DESTINATION_PATH" ] && [ -w "$DESTINATION_PATH" ]; then
         DESTINATION_PATH=$DESTINATION_PATH
     else
         DESTINATION_PATH="${HOME}/.fastn/bin"
-        mkdir -p $DESTINATION_PATH
+        mkdir -p "$DESTINATION_PATH"
     fi
 
-    if [[ $CONTROLLER ]]; then 
+    # Remove temporary files from previous install attempts
+    rm -f fastn_macos_x86_64 fastn_linux_musl_x86_64 fastn_controller_linux_musl_x86_64
+
+    echo ""
+
+    log_message "✔ Removed temporary files"
+
+    echo ""
+
+    if [ -n "$CONTROLLER" ]; then 
         curl -# -L "$URL" | grep ".*\/releases\/download\/.*\/fastn_controller_linux.*" | head -2 | cut -d : -f 2,3 | tee /dev/tty | xargs -I % curl -# -O -J -L % > /dev/null
         mv fastn_controller_linux_musl_x86_64 "${DESTINATION_PATH}/fastn"
         mv fastn_controller_linux_musl_x86_64.d "${DESTINATION_PATH}/fastn.d"
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
+    elif [ "$(uname)" = "Darwin" ]; then
         curl -# -L "$URL" | grep ".*\/releases\/download\/.*\/fastn_macos.*" | head -1 | cut -d : -f 2,3 | tee /dev/tty | xargs -I % curl -# -O -J -L % > /dev/null
         mv fastn_macos_x86_64 "${DESTINATION_PATH}/fastn"
     else
@@ -123,25 +158,32 @@ setup() {
         mv fastn_linux_musl_x86_64.d "${DESTINATION_PATH}/fastn.d"
     fi
 
-
     echo ""
 
-    chmod +x "${DESTINATION_PATH}/fastn"*
+    # Check if the destination files are moved successfully before setting permissions
+    if [ -e "${DESTINATION_PATH}/fastn" ]; then
+        chmod +x "${DESTINATION_PATH}/fastn"*
 
-    # Add fastn to PATH if not already done
-    update_path
+        log_message "✔ Successfully moved binaries to destination $DESTINATION_PATH"
 
-    echo "${FMT_GREEN}╭────────────────────────────────────────╮"
-    echo "│                                        │"
-    echo "│   fastn installation completed         │"
-    echo "│                                        │"
-    echo "│   Restart your terminal to apply       │"
-    echo "│   the changes.                         │"
-    echo "│                                        │"
-    echo "│   Get started with fastn at:           │"
-    echo "│   ${FMT_BLUE}https://fastn.com${FMT_RESET}${FMT_GREEN}                    │"
-    echo "│                                        │"
-    echo "╰────────────────────────────────────────╯${FMT_RESET}"
+        echo ""
+
+        if update_path; then
+            log_message "╭────────────────────────────────────────╮"
+            log_message "│                                        │"
+            log_message "│   fastn installation completed         │"
+            log_message "│                                        │"
+            log_message "│   Restart your terminal to apply       │"
+            log_message "│   the changes.                         │"
+            log_message "│                                        │"
+            log_message "│   Get started with fastn at:           │"
+            log_message "│   ${FMT_BLUE}https://fastn.com${FMT_RESET}                    │"
+            log_message "│                                        │"
+            log_message "╰────────────────────────────────────────╯"
+        fi
+    else
+        log_error "Installation failed. Please check if you have sufficient permissions to install in $DESTINATION_PATH."
+    fi
 }
 
 main() {
