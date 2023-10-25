bb4ab965f44b7b11e1c888ce4659f1b546c4501f
Amit Upadhyay <upadhyay@gmail.com>
2023-10-25 16:07:10 +0530

    /-/shutdown/

--- fastn-core/src/tutor.rs
+++ fastn-core/src/tutor.rs
@@ -2,7 +2,17 @@ pub async fn pwd() -> fastn_core::Result<fastn_core::http::Response> {
     if !is_tutor() {
         return Ok(fastn_core::not_found!("this only works in tutor mode"));
     }
+
     fastn_core::http::api_ok(std::env::current_dir()?.to_string_lossy())
+}
+
+pub async fn shutdown() -> fastn_core::Result<fastn_core::http::Response> {
+    if !is_tutor() {
+        return Ok(fastn_core::not_found!("this only works in tutor mode"));
+    }
+
+    println!("/-/shutdown/ called, shutting down");
+    std::process::exit(0);
 }
 
 pub fn is_tutor() -> bool {
