0573623028e4fcde7c8fa55d38819caf8414e181
Amit Upadhyay <upadhyay@gmail.com>
2023-10-25 15:42:22 +0530

    /-/pwd/ in tutor mode

---
+++ fastn-core/src/tutor.rs
@@ -0,0 +1,12 @@
+pub async fn pwd() -> fastn_core::Result<fastn_core::http::Response> {
+    if !is_tutor() {
+        return Ok(fastn_core::not_found!("this only works in tutor mode"));
+    }
+    fastn_core::http::api_ok(std::env::current_dir()?.to_string_lossy())
+}
+
+pub fn is_tutor() -> bool {
+    // https://github.com/orgs/fastn-stack/discussions/1414
+    // with either of these are passed we allow APIs like /-/shutdown/, `/-/start/` etc
+    std::env::args().any(|e| e == "tutor" || e == "--tutor")
+}
