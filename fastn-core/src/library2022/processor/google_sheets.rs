2e59d74c1acb033f8586fcff6e1289a86968257d
Harsh Singh <harshsingh.js@gmail.com>
2023-11-10 11:54:15 +0530

    named param support

--- fastn-core/src/library2022/processor/google_sheets.rs
+++ fastn-core/src/library2022/processor/google_sheets.rs
@@ -54,17 +54,213 @@ pub fn parse_csv(
     Ok(result)
 }
 
-// Todo: Resolve variable
+fn resolve_variable_from_doc(
+    var: &str,
+    doc: &ftd::interpreter::TDoc,
+    line_number: usize,
+) -> ftd::interpreter::Result<String> {
+    let thing = match doc.get_thing(var, line_number) {
+        Ok(ftd::interpreter::Thing::Variable(v)) => v.value.resolve(doc, line_number)?,
+        Ok(v) => {
+            return ftd::interpreter::utils::e2(
+                format!("{var} is not a variable, it's a {v:?}"),
+                doc.name,
+                line_number,
+            )
+        }
+        Err(e) => {
+            return ftd::interpreter::utils::e2(
+                format!("${var} not found in the document: {e:?}"),
+                doc.name,
+                line_number,
+            )
+        }
+    };
+
+    let param_value: String = match thing {
+        ftd::interpreter::Value::String { text } => format!("\"{}\"", text),
+        ftd::interpreter::Value::Integer { value } => value.to_string(),
+        ftd::interpreter::Value::Decimal { value } => value.to_string(),
+        ftd::interpreter::Value::Boolean { value } => value.to_string(),
+        _ => unimplemented!(), // Handle other types as needed
+    };
+
+    Ok(param_value)
+}
+
+fn resolve_variable_from_headers(
+    var: &str,
+    param_type: &str,
+    doc: &ftd::interpreter::TDoc,
+    headers: &ftd::ast::HeaderValues,
+    line_number: usize,
+) -> ftd::interpreter::Result<String> {
+    let header = match headers.optional_header_by_name(var, doc.name, line_number)? {
+        Some(v) => v,
+        None => return Ok("null".to_string()),
+    };
+
+    if let ftd::ast::VariableValue::String { value, .. } = &header.value {
+        if let Some(stripped) = value.strip_prefix('$') {
+            return resolve_variable_from_doc(stripped, doc, line_number);
+        }
+    }
+
+    let param_value: String = match (param_type, &header.value) {
+        ("STRING", ftd::ast::VariableValue::String { value, .. }) => format!("\"{}\"", value),
+        ("INTEGER", ftd::ast::VariableValue::String { value, .. }) => value.to_string(),
+        ("DECIMAL", ftd::ast::VariableValue::String { value, .. }) => value.to_string(),
+        ("BOOLEAN", ftd::ast::VariableValue::String { value, .. }) => value.to_string(),
+        _ => unimplemented!(), // Handle other types as needed
+    };
+
+    Ok(param_value)
+}
+
+fn resolve_param(
+    param_name: &str,
+    param_type: &str,
+    doc: &ftd::interpreter::TDoc,
+    headers: &ftd::ast::HeaderValues,
+    line_number: usize,
+) -> ftd::interpreter::Result<String> {
+    resolve_variable_from_headers(param_name, param_type, doc, headers, line_number)
+        .or_else(|_| resolve_variable_from_doc(param_name, doc, line_number))
+}
+
+#[derive(Debug, PartialEq)]
+enum State {
+    OutsideParam,
+    InsideParam,
+    InsideStringLiteral,
+    InsideEscapeSequence(usize),
+    ConsumeEscapedChar,
+    StartTypeHint,
+    InsideTypeHint,
+    PushParam,
+    ParseError(String),
+}
+
+fn parse_query(
+    query: &str,
+    doc: &ftd::interpreter::TDoc,
+    headers: ftd::ast::HeaderValues,
+    line_number: usize,
+) -> ftd::interpreter::Result<String> {
+    let mut output = String::new();
+    let mut param_name = String::new();
+    let mut param_type = String::new();
+    let mut state = State::OutsideParam;
+
+    for c in query.chars() {
+        match state {
+            State::OutsideParam => {
+                if c == '$' {
+                    state = State::InsideParam;
+                    param_name.clear();
+                    param_type.clear();
+                } else if c == '"' {
+                    state = State::InsideStringLiteral;
+                }
+            }
+            State::InsideStringLiteral => {
+                if c == '"' {
+                    state = State::OutsideParam;
+                } else if c == '\\' {
+                    state = State::InsideEscapeSequence(0);
+                }
+            }
+            State::InsideEscapeSequence(escape_count) => {
+                if c == '\\' {
+                    state = State::InsideEscapeSequence(escape_count + 1);
+                } else {
+                    state = if escape_count % 2 == 0 {
+                        State::InsideStringLiteral
+                    } else {
+                        State::ConsumeEscapedChar
+                    };
+                }
+            }
+            State::ConsumeEscapedChar => {
+                state = State::InsideStringLiteral;
+            }
+            State::StartTypeHint => {
+                if c == ':' {
+                    state = State::InsideTypeHint;
+                } else {
+                    state = State::ParseError("Type hint must start with `::`".to_string());
+                }
+            }
+            State::InsideParam => {
+                if c == ':' {
+                    state = State::StartTypeHint;
+                } else if c.is_alphanumeric() {
+                    param_name.push(c);
+                } else if c == ',' || c == ';' || c.is_whitespace() && !param_name.is_empty() {
+                    state = State::PushParam;
+                }
+            }
+            State::InsideTypeHint => {
+                if c.is_alphanumeric() {
+                    param_type.push(c);
+                } else {
+                    state = State::PushParam;
+                }
+            }
+            State::PushParam => {
+                state = State::OutsideParam;
+
+                let param_value =
+                    resolve_param(&param_name, &param_type, doc, &headers, line_number)?;
+
+                output.push_str(&param_value);
+
+                param_name.clear();
+                param_type.clear();
+            }
+            State::ParseError(error) => {
+                return Err(ftd::interpreter::Error::ParseError {
+                    message: format!("Failed to parse SQL Query: {}", error),
+                    doc_id: doc.name.to_string(),
+                    line_number,
+                });
+            }
+        }
+
+        if ![
+            State::InsideParam,
+            State::PushParam,
+            State::InsideTypeHint,
+            State::StartTypeHint,
+        ]
+        .contains(&state)
+        {
+            output.push(c);
+        }
+    }
+
+    // Handle the last param if there was no trailing comma or space
+    if [State::InsideParam, State::PushParam, State::InsideTypeHint].contains(&state)
+        && !param_name.is_empty()
+    {
+        let param_value = resolve_param(&param_name, &param_type, doc, &headers, line_number)?;
+        output.push_str(&param_value);
+    }
+
+    Ok(output)
+}
 
 pub async fn process(
     value: ftd::ast::VariableValue,
     kind: ftd::interpreter::Kind,
     doc: &ftd::interpreter::TDoc<'_>,
     db_config: &fastn_core::library2022::processor::sql::DatabaseConfig,
-    _headers: ftd::ast::HeaderValues,
+    headers: ftd::ast::HeaderValues,
     query: &str,
 ) -> ftd::interpreter::Result<ftd::interpreter::Value> {
-    let request_url = prepare_query_url(&db_config.db_url, query);
+    let query = parse_query(query, doc, headers, value.line_number())?;
+
+    let request_url = prepare_query_url(&db_config.db_url, query.as_str());
 
     let response = match fastn_core::http::http_get_str(&request_url).await {
         Ok(v) => v,
@@ -92,7 +288,7 @@ pub async fn process(
             kind,
             doc,
             &value,
-            super::sql::STATUS_ERROR,
+            fastn_core::library2022::processor::sql::STATUS_ERROR,
         ),
     }
 }
