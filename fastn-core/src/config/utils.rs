f6ca6e0d37f5783bd487f9d78d16272ce1bd9900
heulitig <rithik@fifthtry.com>
2023-11-15 15:38:25 +0530

    minor adjustments

--- fastn-core/src/config/utils.rs
+++ fastn-core/src/config/utils.rs
@@ -114,7 +114,14 @@ pub fn get_clean_url(
     if let Some(endpoint) = config.package.endpoint.as_ref() {
         return Ok((
             Some(config.package.name.to_string()),
-            url::Url::parse(format!("{}/{}", endpoint.trim_end_matches('/'), url).as_str())?,
+            url::Url::parse(
+                format!(
+                    "{}/{}",
+                    endpoint.trim_end_matches('/'),
+                    url.trim_start_matches('/')
+                )
+                .as_str(),
+            )?,
             std::collections::HashMap::new(),
         ));
     }
