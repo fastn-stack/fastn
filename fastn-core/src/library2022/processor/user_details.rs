37c4d0a4a8c0d8f4fb968af723c4e3155c367041
siddhantCodes <siddhantk232@gmail.com>
2023-11-20 15:18:49 +0530

    use full paths

--- fastn-core/src/library2022/processor/user_details.rs
+++ fastn-core/src/library2022/processor/user_details.rs
@@ -1,5 +1,3 @@
-use fastn_core::auth;
-
 /// currently returns the github user details
 pub async fn process(
     value: ftd::ast::VariableValue,
@@ -10,16 +8,16 @@ pub async fn process(
     let mut ud = Default::default();
 
     if let Some(gh_cookie) = req_config.request.cookie("github") {
-        if let Ok(user_detail) = auth::decrypt(&gh_cookie)
+        if let Ok(user_detail) = fastn_core::auth::decrypt(&gh_cookie)
             .await
             .map_err(|e| tracing::info!("[user-details]: Failed to decrypt cookie: {e}"))
             .and_then(|decrypted_cookie| {
-                serde_json::from_str::<auth::github::UserDetail>(decrypted_cookie.as_str()).map_err(
+                serde_json::from_str::<fastn_core::auth::github::UserDetail>(decrypted_cookie.as_str()).map_err(
                     |e| tracing::info!("[user-details]: Serde deserialization failed {e}:"),
                 )
             })
         {
-            match auth::github::user_details(user_detail.access_token.as_str()).await {
+            match fastn_core::auth::github::user_details(user_detail.access_token.as_str()).await {
                 Ok(user) => {
                     ud = UserDetails {
                         is_logged_in: true,
@@ -38,5 +36,6 @@ pub async fn process(
 struct UserDetails {
     #[serde(rename = "is-logged-in")]
     is_logged_in: bool,
-    user: Option<auth::github::GhUserDetails>,
+    user: Option<fastn_core::auth::github::GhUserDetails>,
 }
+
