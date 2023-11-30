093ec529d8e91a610c433bdac474102f7f8e8f99
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-11-30 18:20:14 +0530

    Remove logs

--- fastn-core/src/auth/utils.rs
+++ fastn-core/src/auth/utils.rs
@@ -24,9 +24,9 @@ pub async fn decrypt(encrypted_str: &String) -> Result<String, MagicCryptError> 
     let mc_obj = magic_crypt::new_magic_crypt!(&secret_key, 256);
     if encrypted_str.starts_with('"') {
         // django adds quotes to the cookie value
-        dbg!(mc_obj.decrypt_base64_to_string(&encrypted_str[1..encrypted_str.len() - 1]))
+        mc_obj.decrypt_base64_to_string(&encrypted_str[1..encrypted_str.len() - 1])
     } else {
-        dbg!(mc_obj.decrypt_base64_to_string(encrypted_str))
+        mc_obj.decrypt_base64_to_string(encrypted_str)
     }
 }
 
