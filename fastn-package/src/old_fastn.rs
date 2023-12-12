e07b626c34cf34888af27ec0928198cce92d133d
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-12-12 18:26:52 +0530

    System and capabilities related functions added

--- fastn-package/src/old_fastn.rs
+++ fastn-package/src/old_fastn.rs
@@ -98,5 +98,6 @@ pub struct PackageTemp {
     // This will contain the module name through which this package can
     // be accessed when considered as a system's package
     pub system: Option<String>,
+    #[serde(rename = "system-is-confidential")]
     pub system_is_confidential: Option<bool>,
 }
