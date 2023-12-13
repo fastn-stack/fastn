a474d572d3cef30b70a485b0ee490851ce07e632
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-12-14 00:06:39 +0530

    Cookie: fastn_lang -> fastn-lang

--- fastn-core/src/commands/serve.rs
+++ fastn-core/src/commands/serve.rs
@@ -30,7 +30,7 @@ async fn serve_file(
     if let Err(e) = config
         .config
         .package
-        .auto_import_language(config.request.cookie("fastn_lang"), None)
+        .auto_import_language(config.request.cookie("fastn-lang"), None)
     {
         return fastn_core::not_found!("fastn-Error: path: {}, {:?}", path, e);
     }
