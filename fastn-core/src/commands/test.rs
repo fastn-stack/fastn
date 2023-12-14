40ccc8ce849a71be85da119d39e081f4306d5e17
Arpita-Jaiswal <ayushipujaa@gmail.com>
2023-12-14 17:23:09 +0530

    Calling html for test

--- fastn-core/src/commands/test.rs
+++ fastn-core/src/commands/test.rs
@@ -1,3 +1,5 @@
+use hyper::http::request;
+
 pub(crate) const TEST_FOLDER: &str = "_tests";
 pub(crate) const TEST_FILE_EXTENSION: &str = ".test.ftd";
 
@@ -66,27 +68,12 @@ async fn read_ftd_test_file(
     ftd_document: fastn_core::Document,
     config: &fastn_core::Config,
 ) -> fastn_core::Result<()> {
-    /*let parsed_document = ftd::interpreter::ParsedDocument::parse(
-        ftd_document.id.as_str(),
-        ftd_document.content.as_str(),
-    )?;
-    dbg!(&parsed_document.name, &parsed_document.ast);*/
     let req = fastn_core::http::Request::default();
     let base_url = "/";
     let mut req_config =
         fastn_core::RequestConfig::new(config, &req, ftd_document.id.as_str(), base_url);
     req_config.current_document = Some(ftd_document.id.to_string());
-    /* let resp = fastn_core::package::package_doc::process_ftd(
-        &mut req_config,
-        doc,
-        base_url,
-        build_static_files,
-        false,
-        file_path.as_str(),
-    )
-        .await;*/
-
-    let main_ftd_doc = fastn_core::doc::interpret_helper(
+    let mut main_ftd_doc = fastn_core::doc::interpret_helper(
         ftd_document.id_with_package().as_str(),
         ftd_document.content.as_str(),
         &mut req_config,
@@ -96,9 +83,103 @@ async fn read_ftd_test_file(
     )
     .await?;
     dbg!(&main_ftd_doc.tree);
+    let doc = ftd::interpreter::TDoc::new(
+        &main_ftd_doc.name,
+        &main_ftd_doc.aliases,
+        &main_ftd_doc.data,
+    );
+
+    for instruction in main_ftd_doc.tree {
+        execute_instruction(&instruction, &doc, config).await?;
+    }
     Ok(())
+}
+
+async fn execute_instruction(
+    instruction: &ftd::interpreter::Component,
+    doc: &ftd::interpreter::TDoc<'_>,
+    config: &fastn_core::Config,
+) -> fastn_core::Result<()> {
+    match instruction.name.as_str() {
+        "fastn#get" => execute_get_instruction(instruction, doc, config).await,
+        "fastn#post" => todo!(),
+        t => fastn_core::usage_error(format!(
+            "Unknown instruction {}, line number: {}",
+            t, instruction.line_number
+        )),
+    }
+}
+
+async fn execute_get_instruction(
+    instruction: &ftd::interpreter::Component,
+    doc: &ftd::interpreter::TDoc<'_>,
+    config: &fastn_core::Config,
+) -> fastn_core::Result<()> {
+    let property_values = instruction.get_interpreter_property_value_of_all_arguments(&doc);
+    let url = get_value_ok("url", &property_values, instruction.line_number)?
+        .to_string()
+        .unwrap();
+    let title = get_value_ok("title", &property_values, instruction.line_number)?
+        .to_string()
+        .unwrap();
+    let test = get_value_ok("test", &property_values, instruction.line_number)?
+        .to_string()
+        .unwrap();
+
+    get_js_for_id(url.as_str(), config).await?;
+    Ok(())
+}
+
+async fn get_js_for_id(id: &str, config: &fastn_core::Config) -> fastn_core::Result<()> {
+    let mut request = fastn_core::http::Request::default();
+    request.path = id.to_string();
+    let request = fastn_core::commands::serve::serve(config, request).await?;
+    dbg!(&request.body());
+    Ok(())
+}
+
+fn get_value_ok(
+    key: &str,
+    property_values: &ftd::Map<ftd::interpreter::PropertyValue>,
+    line_number: usize,
+) -> fastn_core::Result<ftd::interpreter::Value> {
+    get_value(key, property_values).ok_or(fastn_core::Error::NotFound(format!(
+        "Key '{}' not found, line number: {}",
+        key, line_number
+    )))
+}
+
+fn get_value(
+    key: &str,
+    property_values: &ftd::Map<ftd::interpreter::PropertyValue>,
+) -> Option<ftd::interpreter::Value> {
+    let property_value = if let Some(property_value) = property_values.get(key) {
+        property_value
+    } else {
+        return None;
+    };
+    match property_value {
+        ftd::interpreter::PropertyValue::Value { value, .. } => Some(value.clone()),
+        _ => unimplemented!(),
+    }
 }
 
 pub fn test_fastn_ftd() -> &'static str {
     include_str!("../../test_fastn.ftd")
 }
+
+/*fn get_asts(document: ftd::interpreter::Document) -> Vec<fastn_js::Ast> {
+    let mut js_ast_data = ftd::js::document_into_js_ast(document);
+    // Remove the fastn asts. This will come from test_fastn.js
+    js_ast_data.asts = js_ast_data
+        .asts
+        .into_iter()
+        .filter(|ast| {
+            ast.get_name()
+                .map(|name| !name.starts_with("fastn#"))
+                .unwrap_or(true)
+        })
+        .collect();
+
+    js_ast_data.asts
+}*/
