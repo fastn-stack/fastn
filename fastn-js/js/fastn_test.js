8b6b7b52403503b9e1643e00b5a5b1ff651408aa
heulitig <rithik@fifthtry.com>
2023-12-29 12:09:49 +0530

    added not_empty and exists assert functions

--- fastn-js/js/fastn_test.js
+++ fastn-js/js/fastn_test.js
@@ -4,11 +4,25 @@ fastn.assert = {
     eq: function (a, b){
         a = fastn_utils.getStaticValue(a);
         b = fastn_utils.getStaticValue(b);
-        fastn.test_result.push(a === b)
+        fastn.test_result.push(a === b);
     },
     ne: function (a, b){
         a = fastn_utils.getStaticValue(a);
         b = fastn_utils.getStaticValue(b);
-        fastn.test_result.push(a !== b)
+        fastn.test_result.push(a !== b);
+    },
+    exists: function(a) {
+        a = fastn_utils.getStaticValue(a);
+        fastn.test_result.push(a !== undefined);
+    },
+    not_empty: function(a) {
+        a = fastn_utils.getStaticValue(a);
+        if (Array.isArray(a)) {
+            fastn.test_result.push(a.length > 0);
+        }
+        if (a instanceof String) {
+            fastn.test_result.push(a.length > 0);
+        }
+        fastn.test_result.push(a !== undefined);
     }
 };
