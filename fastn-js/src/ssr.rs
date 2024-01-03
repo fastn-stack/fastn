487fcc1886ace677a998a80f7b59a2e1ac71a329
siddhantCodes <siddhantk232@gmail.com>
2024-01-03 12:35:08 +0530

    use quick-js on non windows platforms
    .dockerignore to reduce docker image size

--- fastn-js/src/ssr.rs
+++ fastn-js/src/ssr.rs
@@ -1,16 +1,48 @@
 pub fn run_test(js: &str) -> Vec<bool> {
-    rquickjs::Context::full(&rquickjs::Runtime::new().unwrap())
-        .unwrap()
-        .with(|ctx| ctx.eval::<Vec<bool>, _>(js).unwrap())
+    #[cfg(target_os = "windows")]
+    {
+        rquickjs::Context::full(&rquickjs::Runtime::new().unwrap())
+            .unwrap()
+            .with(|ctx| ctx.eval::<Vec<bool>, _>(js).unwrap())
+    }
+    #[cfg(not(target_os = "windows"))]
+    {
+        // Added logging support from console from within context
+        let context = quick_js::Context::builder()
+            .console(
+                |level: quick_js::console::Level, args: Vec<quick_js::JsValue>| {
+                    eprintln!("{}: {:?}", level, args);
+                },
+            )
+            .build()
+            .unwrap();
+        context.eval_as::<Vec<bool>>(js).unwrap()
+    }
 }
 
 pub fn ssr_str(js: &str) -> String {
     let all_js = fastn_js::all_js_with_test();
     let js = format!("{all_js}{js}");
 
-    rquickjs::Context::full(&rquickjs::Runtime::new().unwrap())
-        .unwrap()
-        .with(|ctx| ctx.eval::<String, _>(js).unwrap())
+    #[cfg(target_os = "windows")]
+    {
+        rquickjs::Context::full(&rquickjs::Runtime::new().unwrap())
+            .unwrap()
+            .with(|ctx| ctx.eval::<String, _>(js).unwrap())
+    }
+    #[cfg(not(target_os = "windows"))]
+    {
+        // Added logging support from console from within context
+        let context = quick_js::Context::builder()
+            .console(
+                |level: quick_js::console::Level, args: Vec<quick_js::JsValue>| {
+                    eprintln!("{}: {:?}", level, args);
+                },
+            )
+            .build()
+            .unwrap();
+        context.eval_as::<String>(js.as_str()).unwrap()
+    }
 }
 
 pub fn ssr(ast: &[fastn_js::Ast]) -> String {
