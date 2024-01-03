8ae70a4d5354686ced9598bc8dc450d95c2764a9
Harsh Singh <harshsingh.js@gmail.com>
2024-01-03 14:48:35 +0530

    create basic manifest.json

--- fastn-update/src/lib.rs
+++ fastn-update/src/lib.rs
@@ -25,13 +25,19 @@ async fn resolve_dependency_from_gh(
         "https://api.github.com/repos/{}/{}/zipball",
         username, repository
     );
-    let tar_gz = fastn_core::http::http_get(&url).await?;
-    Ok(tar_gz)
+    let zipball = fastn_core::http::http_get(&url).await?;
+    Ok(zipball)
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct Manifest {
     pub packages: Vec<Package>,
+}
+
+impl Manifest {
+    pub fn new(packages: Vec<Package>) -> Self {
+        Manifest { packages }
+    }
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
@@ -43,39 +49,28 @@ pub struct Package {
     pub dependencies: Vec<String>,
 }
 
-pub async fn unpack_package_archive<R>(
-    config: &fastn_core::Config,
-    target_dir: camino::Utf8PathBuf,
-    mut archive: tar::Archive<flate2::read::GzDecoder<R>>,
-) -> fastn_core::Result<()>
-where
-    R: std::io::Read,
-{
-    while let Some(entry) = archive.entries()?.next() {
-        let mut entry = entry?;
-        let entry_path = {
-            let entry_path = entry.path()?;
-            let path_str = entry_path.to_string_lossy();
-            format!("{}/{}", &target_dir, path_str)
-        };
-        let mut buffer = Vec::new();
-        std::io::Read::read_to_end(&mut entry, &mut buffer)?;
-        config.ds.write_content(&entry_path, buffer).await?;
+impl Package {
+    pub fn new(name: String, version: Option<String>, source: String, checksum: String, dependencies: Vec<String>) -> Self {
+        Package { name, version, source, checksum, dependencies }
     }
-
-    Ok(())
 }
 
 pub async fn resolve_dependencies(config: &fastn_core::Config) -> fastn_core::Result<()> {
-    use std::io::{Seek, Write};
+    use std::io::Seek;
+    use sha2::{Digest, Sha256};
+
+    let mut packages: Vec<Package> = vec![];
 
     for dependency in &config.package.dependencies {
         if let Some((username, repo)) = extract_github_details(dependency.package.name.as_str()) {
             let zipball = resolve_dependency_from_gh(username.as_str(), repo.as_str()).await?;
-            let mut temp_file = tempfile::tempfile()?;
-            temp_file.write_all(&zipball)?;
-            temp_file.seek(std::io::SeekFrom::Start(0))?;
-            let mut archive = zip::ZipArchive::new(temp_file)?;
+            let mut zipball_cursor = std::io::Cursor::new(zipball);
+            let mut hasher = Sha256::new();
+            let mut buffer = Vec::new();
+            let _ = &zipball_cursor.clone().take(u64::MAX.into()).read_to_end(&mut buffer)?;
+            hasher.update(&buffer);
+            zipball_cursor.seek(std::io::SeekFrom::Start(0))?;
+            let mut archive = zip::ZipArchive::new(zipball_cursor)?;
             for i in 0..archive.len() {
                 let mut entry = archive.by_index(i)?;
 
@@ -88,8 +83,29 @@ pub async fn resolve_dependencies(config: &fastn_core::Config) -> fastn_core::Re
                         .await?;
                 }
             }
+
+            let source = format!("git+https://github.com/{}/{}", username, repo);
+
+            let checksum = format!("{:x}", hasher.finalize());
+
+            let package = Package::new(
+                dependency.package.name.clone(),
+                None, // todo: fix this when versioning is available
+                source,
+                checksum,
+                dependency.package.dependencies.iter().map(|d| d.package.name.clone()).collect(),
+            );
+
+            packages.push(package);
         }
     }
+
+    let manifest = Manifest::new(packages);
+    let dot_fastn_dir = config.ds.root().join(".fastn");
+
+    config.ds.write_content(dot_fastn_dir.join("manifest.json"), serde_json::to_vec_pretty(&manifest)?).await?;
+
+    println!("Wrote manifest.json");
 
     Ok(())
 }
