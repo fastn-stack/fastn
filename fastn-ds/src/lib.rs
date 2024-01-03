60531c3c0cd35eb547904e05296325f788046563
Amit Upadhyay <upadhyay@gmail.com>
2024-01-03 11:41:38 +0530

    open path signature

--- fastn-ds/src/lib.rs
+++ fastn-ds/src/lib.rs
@@ -1,6 +1,25 @@
 #[derive(Debug, Clone)]
 pub struct DocumentStore {
-    root: camino::Utf8PathBuf,
+    root: Path,
+}
+
+#[derive(Debug, Clone)]
+pub struct Path {
+    path: camino::Utf8PathBuf,
+}
+
+impl Path {
+    pub fn new<T: AsRef<str>>(path: T) -> Self {
+        Self {
+            path: camino::Utf8PathBuf::from(path.as_ref()),
+        }
+    }
+
+    pub fn join<T: AsRef<str>>(&self, path: T) -> Self {
+        Self {
+            path: self.path.join(path.as_ref()),
+        }
+    }
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -32,40 +51,36 @@ impl DocumentStore {
         }
     }
 
-    pub fn root(&self) -> &camino::Utf8PathBuf {
+    pub fn root(&self) -> &Path {
         &self.root
     }
 
-    pub async fn read_content<T: AsRef<str>>(&self, path: T) -> Result<Vec<u8>, ReadError> {
+    pub async fn read_content(&self, path: &Path) -> Result<Vec<u8>, ReadError> {
         use tokio::io::AsyncReadExt;
 
-        let mut file = tokio::fs::File::open(self.root.join(path.as_ref())).await?;
+        let mut file = tokio::fs::File::open(self.root.join(&path.path)).await?;
         let mut contents = vec![];
         file.read_to_end(&mut contents).await?;
         Ok(contents)
     }
 
-    pub async fn read_to_string<T: AsRef<str>>(&self, path: T) -> Result<String, ReadStringError> {
+    pub async fn read_to_string(&self, path: &Path) -> Result<String, ReadStringError> {
         self.read_content(path)
             .await
             .map_err(ReadStringError::ReadError)
             .and_then(|v| String::from_utf8(v).map_err(ReadStringError::UTF8Error))
     }
 
-    pub async fn write_content<T: AsRef<str>>(
-        &self,
-        path: T,
-        data: Vec<u8>,
-    ) -> Result<(), WriteError> {
+    pub async fn write_content(&self, path: &Path, data: Vec<u8>) -> Result<(), WriteError> {
         use tokio::io::AsyncWriteExt;
 
-        let mut file = tokio::fs::File::create(self.root.join(path.as_ref())).await?;
+        let mut file = tokio::fs::File::create(self.root.join(&path.path)).await?;
         file.write_all(&data).await?;
         Ok(())
     }
 
-    pub async fn read_dir<T: AsRef<str>>(&self, path: T) -> std::io::Result<tokio::fs::ReadDir> {
+    pub async fn read_dir(&self, path: &Path) -> std::io::Result<tokio::fs::ReadDir> {
         // Todo: Return type should be ftd::interpreter::Result<Vec<fastn_ds::Dir>> not ftd::interpreter::Result<tokio::fs::ReadDir>
-        tokio::fs::read_dir(path.as_ref()).await
+        tokio::fs::read_dir(&path.path).await
     }
 }
