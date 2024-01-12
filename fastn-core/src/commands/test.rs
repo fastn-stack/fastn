a040525f0beedc58a8cfea5e61bd072d94228318
heulitig <rithik@fifthtry.com>
2024-01-12 13:47:13 +0530

    removed children from fastn.test

--- fastn-core/src/commands/test.rs
+++ fastn-core/src/commands/test.rs
@@ -6,7 +6,6 @@ pub(crate) const FIXTURE_FILE_EXTENSION: &str = ".test.ftd";
 // mandatory test parameters
 pub(crate) const TEST_TITLE_HEADER: &str = "title";
 pub(crate) const TEST_URL_HEADER: &str = "url";
-pub(crate) const TESTS_CHILDREN_HEADER: &str = "tests";
 
 // optional test parameters
 pub(crate) const FIXTURE_HEADER: &str = "fixtures";
@@ -274,13 +273,11 @@ async fn get_all_instructions(
                 );
             }
             "fastn#get" | "fastn#post" => {
-                if found_test_component {
+                if !found_test_component {
                     return fastn_core::usage_error(format!(
-                        "fastn.test exists which can't have sibling instruction {}, doc: {} \
+                        "fastn.test doesn't exist for this test, doc: {} \
                         line_number: {}",
-                        instruction.name.as_str(),
-                        doc.name,
-                        instruction.line_number
+                        doc.name, instruction.line_number
                     ));
                 }
                 rest_instructions.push(instruction.clone())
@@ -352,23 +349,7 @@ async fn get_instructions_from_test(
     let fixture_instructions =
         get_fixture_instructions(config, fixtures, included_fixtures).await?;
 
-    let test_instructions = if let Some(test_instructions) =
-        get_optional_value_list(TESTS_CHILDREN_HEADER, &property_values, doc)?
-    {
-        let mut instructions = vec![];
-        for instruction in test_instructions.iter() {
-            if let ftd::interpreter::Value::UI { component, .. } = instruction {
-                instructions.push(component.clone());
-            }
-        }
-        instructions
-    } else {
-        vec![]
-    };
-
-    let mut all_instructions = fixture_instructions;
-    all_instructions.extend(test_instructions);
-
+    let all_instructions = fixture_instructions;
     Ok(all_instructions)
 }
 
