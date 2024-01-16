bcfd69a5b69b35d5e834358e778b9a7a836ad3fe
Harsh Singh <harshsingh.js@gmail.com>
2024-01-16 14:46:49 +0530

    fbt test fixes

--- fastn-core/src/commands/build.rs
+++ fastn-core/src/commands/build.rs
@@ -21,9 +21,7 @@ pub async fn build(
     {
         let documents = get_documents_for_current_package(config).await?;
 
-        if let Some(zip_url) = zip_url {
-            write_manifest_file(config, &build_dir, zip_url).await?;
-        }
+        fastn_core::manifest::write_manifest_file(config, &build_dir, zip_url).await?;
 
         match only_id {
             Some(id) => {
@@ -66,69 +64,6 @@ pub async fn build(
     if check_build {
         return fastn_core::post_build_check(config).await;
     }
-
-    Ok(())
-}
-
-pub async fn write_manifest_file(
-    config: &fastn_core::Config,
-    build_dir: &fastn_ds::Path,
-    zip_url: &str,
-) -> fastn_core::Result<()> {
-    use sha2::digest::FixedOutput;
-    use sha2::Digest;
-
-    let start = std::time::Instant::now();
-
-    println!(
-        "Processing {}/{}",
-        &config.package.name.as_str(),
-        fastn_core::manifest::MANIFEST_FILE
-    );
-
-    let mut hasher = sha2::Sha256::new();
-    let mut files: std::collections::HashMap<String, fastn_core::manifest::File> =
-        std::collections::HashMap::new();
-
-    for file in config.get_files(&config.package).await? {
-        if file.get_id().eq(fastn_core::manifest::MANIFEST_FILE) {
-            continue;
-        }
-
-        let name = file.get_id().to_string();
-        let content = &config.ds.read_content(&file.get_full_path()).await?;
-        let hash = fastn_core::utils::generate_hash(content);
-        let size = content.len();
-
-        hasher.update(content);
-
-        files.insert(
-            name.clone(),
-            fastn_core::manifest::File::new(name, hash, size),
-        );
-    }
-
-    let checksum = format!("{:X}", hasher.finalize_fixed());
-
-    let manifest = fastn_core::Manifest::new(files, zip_url.to_string(), checksum);
-
-    let _ = &config
-        .ds
-        .write_content(
-            &build_dir.join(fastn_core::manifest::MANIFEST_FILE),
-            serde_json::ser::to_vec_pretty(&manifest)?,
-        )
-        .await?;
-
-    fastn_core::utils::print_end(
-        format!(
-            "Processed {}/{}",
-            &config.package.name.as_str(),
-            fastn_core::manifest::MANIFEST_FILE
-        )
-        .as_str(),
-        start,
-    );
 
     Ok(())
 }
--- fastn-core/src/manifest/mod.rs
+++ fastn-core/src/manifest/mod.rs
@@ -1,15 +1,17 @@
+pub mod utils;
+
 pub const MANIFEST_FILE: &str = "manifest.json";
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 pub struct Manifest {
-    pub files: std::collections::HashMap<String, File>,
+    pub files: std::collections::BTreeMap<String, File>,
     pub zip_url: String,
     pub checksum: String,
 }
 
 impl Manifest {
     pub fn new(
-        files: std::collections::HashMap<String, File>,
+        files: std::collections::BTreeMap<String, File>,
         zip_url: String,
         checksum: String,
     ) -> Self {
@@ -32,4 +34,84 @@ impl File {
     pub fn new(name: String, hash: String, size: usize) -> Self {
         File { name, hash, size }
     }
+}
+
+pub async fn write_manifest_file(
+    config: &fastn_core::Config,
+    build_dir: &fastn_ds::Path,
+    zip_url: Option<&str>,
+) -> fastn_core::Result<()> {
+    use sha2::digest::FixedOutput;
+    use sha2::Digest;
+
+    let start = std::time::Instant::now();
+
+    println!(
+        "Processing {}/{}",
+        &config.package.name.as_str(),
+        fastn_core::manifest::MANIFEST_FILE
+    );
+
+    let zip_url = match zip_url {
+        Some(zip_url) => zip_url.to_string(),
+        None => {
+            match fastn_core::manifest::utils::get_gh_zipball_url(config.package.name.clone()) {
+                Some(gh_zip_url) => gh_zip_url,
+                None => {
+                    return Err(fastn_core::error::Error::UsageError {
+                        message: format!(
+                            "Could not find zip url for package \"{}\".",
+                            &config.package.name
+                        ),
+                    })
+                }
+            }
+        }
+    };
+
+    let mut hasher = sha2::Sha256::new();
+    let mut files: std::collections::BTreeMap<String, fastn_core::manifest::File> =
+        std::collections::BTreeMap::new();
+
+    for file in config.get_files(&config.package).await? {
+        if file.get_id().eq(fastn_core::manifest::MANIFEST_FILE) {
+            continue;
+        }
+
+        let name = file.get_id().to_string();
+        let content = &config.ds.read_content(&file.get_full_path()).await?;
+        let hash = fastn_core::utils::generate_hash(content);
+        let size = content.len();
+
+        hasher.update(content);
+
+        files.insert(
+            name.clone(),
+            fastn_core::manifest::File::new(name, hash, size),
+        );
+    }
+
+    let checksum = format!("{:X}", hasher.finalize_fixed());
+
+    let manifest = fastn_core::Manifest::new(files, zip_url.to_string(), checksum);
+
+    let _ = &config
+        .ds
+        .write_content(
+            &build_dir.join(fastn_core::manifest::MANIFEST_FILE),
+            serde_json::ser::to_vec_pretty(&manifest)?,
+        )
+        .await?;
+
+    fastn_core::utils::print_end(
+        format!(
+            "Processed {}/{}",
+            &config.package.name.as_str(),
+            fastn_core::manifest::MANIFEST_FILE
+        )
+        .as_str(),
+        start,
+    );
+
+    Ok(())
 }
