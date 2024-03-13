a0dde8940d2578fcd24da4a8a5777e6cef140761
heulitig <rithik@fifthtry.com>
2024-03-13 13:56:29 +0530

    minor error changes

--- ftd/src/interpreter/mod.rs
+++ ftd/src/interpreter/mod.rs
@@ -106,6 +106,9 @@ pub enum Error {
 
     #[error("Invalid access: {message}, line_number: {line_number}")]
     InvalidAccessError { message: String, line_number: usize },
+
+    #[error("DSError: {message}")]
+    DSError { message: String },
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
