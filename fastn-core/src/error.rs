370fd974b051bf649419123239b48c30cf8c35a7
heulitig <rithik@fifthtry.com>
2024-03-13 13:47:39 +0530

    moved http processor functions to fastn_ds

--- fastn-core/src/error.rs
+++ fastn-core/src/error.rs
@@ -131,8 +131,8 @@ pub enum Error {
     #[error("ds::RenameError: {}", _0)]
     DSRenameError(#[from] fastn_ds::RenameError),
 
-    #[error("ds::HttpError: {}", _0)]
-    DSHttpError(#[from] fastn_ds::HttpError),
+    #[error("ds::Error: {}", _0)]
+    DSError(#[from] fastn_ds::DSError),
 
     #[error("AssertError: {message}")]
     AssertError { message: String },
