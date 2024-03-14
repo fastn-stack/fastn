b8e0c81c8d6bb65562b870d8e5c46281ef6cb7f6
heulitig <rithik@fifthtry.com>
2024-03-14 12:13:01 +0530

    fixed error type

--- fastn-core/src/commands/serve.rs
+++ fastn-core/src/commands/serve.rs
@@ -547,7 +547,7 @@ async fn handle_endpoints(
                 &std::collections::HashMap::new(),
             )
             .await
-            .map_err(fastn_core::Error::DSError),
+            .map_err(fastn_core::Error::DSHttpError),
     )
 }
 
