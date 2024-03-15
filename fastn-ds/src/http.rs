d828f3b8c373d00aab6d7d7f2e603a6279b7f62d
heulitig <rithik@fifthtry.com>
2024-03-15 12:40:25 +0530

    using default client

--- fastn-ds/src/http.rs
+++ fastn-ds/src/http.rs
@@ -1,22 +1,6 @@
-fn proxy_client_builder() -> reqwest::Client {
-    // TODO: Connection Pool, It by default holds the connection pool internally
-    reqwest::ClientBuilder::new()
-        .http2_adaptive_window(true)
-        .tcp_keepalive(std::time::Duration::new(150, 0))
-        .tcp_nodelay(true)
-        .connect_timeout(std::time::Duration::new(150, 0))
-        .connection_verbose(true)
-        .redirect(reqwest::redirect::Policy::default())
-        .build()
-        .unwrap()
-}
-
 fn default_client_builder() -> reqwest::Client {
     reqwest::ClientBuilder::default().build().unwrap()
 }
 
 pub static DEFAULT_CLIENT: once_cell::sync::Lazy<std::sync::Arc<reqwest::Client>> =
     once_cell::sync::Lazy::new(|| std::sync::Arc::new(default_client_builder()));
-
-pub static PROXY_CLIENT: once_cell::sync::Lazy<std::sync::Arc<reqwest::Client>> =
-    once_cell::sync::Lazy::new(|| std::sync::Arc::new(proxy_client_builder()));
