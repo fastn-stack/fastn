7e99cb910a3afeca2f27b7a6da4cf8cc78981681
Amit Upadhyay <upadhyay@gmail.com>
2024-03-15 11:18:37 +0530

    reqwest_util::to_http_response

--- fastn-ds/src/reqwest_util.rs
+++ fastn-ds/src/reqwest_util.rs
@@ -1,9 +1,7 @@
-pub fn to_http_response(_r: reqwest::Response) -> http::Response<bytes::Bytes> {
-    // http::Response::builder()
-    //     .header("Content-Type", "application/json")
-    //     .body(bytes::Bytes::from(
-    //         serde_json::to_string(&self).unwrap(),
-    //     ))
-    //     .unwrap()
-    todo!()
+pub async fn to_http_response(r: reqwest::Response) -> Result<http::Response<bytes::Bytes>, reqwest::Error> {
+    let mut b = http::Response::builder().status(r.status().as_u16());
+    for (k, v) in r.headers().iter() {
+        b = b.header(k.as_str(), v.as_bytes());
+    }
+    Ok(b.body(r.bytes().await?).unwrap()) // unwrap is okay
 }
