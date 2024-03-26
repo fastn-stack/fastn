678b44073e47970aa5dc352638a69cbf0be4bb00
siddhantCodes <siddhantk232@gmail.com>
2024-03-26 12:53:29 +0530

    use rquickjs for release builds
    to build with deno_core use the `deno-core` feature flag

--- fastn-js/src/ssr.rs
+++ fastn-js/src/ssr.rs
@@ -1,3 +1,4 @@
+#[cfg(not(feature = "quickjs"))]
 const DELETE_DENO: &str = "delete Deno;";
 
 #[derive(thiserror::Error, Debug)]
@@ -12,23 +13,76 @@ pub enum SSRError {
 type Result<T> = std::result::Result<T, SSRError>;
 
 pub fn run_test(js: &str) -> Result<Vec<bool>> {
-    let mut runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions::default());
-
-    eval::<Vec<bool>>(
-        &mut runtime,
-        deno_core::FastString::from(format!("{DELETE_DENO}{js}")),
-    )
+    #[cfg(feature = "quickjs")]
+    {
+        #[cfg(target_os = "windows")]
+        {
+            Ok(rquickjs::Context::full(&rquickjs::Runtime::new().unwrap())
+                .unwrap()
+                .with(|ctx| ctx.eval::<Vec<bool>, _>(js).unwrap()))
+        }
+        #[cfg(not(target_os = "windows"))]
+        {
+            // Added logging support from console from within context
+            let context = quick_js::Context::builder()
+                .console(
+                    |level: quick_js::console::Level, args: Vec<quick_js::JsValue>| {
+                        eprintln!("{}: {:?}", level, args);
+                    },
+                )
+                .build()
+                .unwrap();
+            Ok::<Vec<bool>, SSRError>(context.eval_as::<Vec<bool>>(js).unwrap())
+        }
+    }
+    #[cfg(not(feature = "quickjs"))]
+    {
+        let mut runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions::default());
+        eval::<Vec<bool>>(
+            &mut runtime,
+            deno_core::FastString::from(format!("{DELETE_DENO}{js}")),
+        )
+    }
 }
 
 pub fn ssr_str(js: &str) -> Result<String> {
-    let mut runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions::default());
-
     let all_js = fastn_js::all_js_with_test();
-    let js = format!("{DELETE_DENO}{all_js}{js}");
 
-    eval::<String>(&mut runtime, deno_core::FastString::from(js))
+    #[cfg(feature = "quickjs")]
+    {
+        let js = format!("{all_js}{js}");
+
+        #[cfg(target_os = "windows")]
+        {
+            Ok(rquickjs::Context::full(&rquickjs::Runtime::new().unwrap())
+                .unwrap()
+                .with(|ctx| ctx.eval::<String, _>(js).unwrap()))
+        }
+        #[cfg(not(target_os = "windows"))]
+        {
+            // Added logging support from console from within context
+            let context = quick_js::Context::builder()
+                .console(
+                    |level: quick_js::console::Level, args: Vec<quick_js::JsValue>| {
+                        eprintln!("{}: {:?}", level, args);
+                    },
+                )
+                .build()
+                .unwrap();
+            Ok::<std::string::String, SSRError>(context.eval_as::<String>(js.as_str()).unwrap())
+        }
+    }
+    #[cfg(not(feature = "quickjs"))]
+    {
+        let mut runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions::default());
+
+        let js = format!("{DELETE_DENO}{all_js}{js}");
+
+        eval::<String>(&mut runtime, deno_core::FastString::from(js))
+    }
 }
 
+#[cfg(not(feature = "quickjs"))]
 fn eval<T: deno_core::serde::Deserialize<'static>>(
     context: &mut deno_core::JsRuntime,
     code: deno_core::FastString,
