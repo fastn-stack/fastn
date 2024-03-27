1215b25c6b6deea77a08ed02ee9b523b6bdba99c
Amit Upadhyay <upadhyay@gmail.com>
2024-03-27 17:28:52 +0530

    some more type changes for ftd-tc

--- ftd-tc/src/lib.rs
+++ ftd-tc/src/lib.rs
@@ -2,7 +2,7 @@
 
 extern crate self as ftd_tc;
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct State {
     /// These are the things we need to resolve.
     ///
@@ -11,13 +11,14 @@ pub struct State {
     /// We do this in a recursive manner.
     continuable_things: Vec<ContinuableThing>,
     /// Raw symbols from all documents are stored here
-    symbols: ftd_p1::Map<ftd_ast::Ast>,
+    symbols: ftd_p1::Map<Lined<ftd_ast::Ast>>,
     /// any type we have already resolved is stored here
-    global_types: ftd_p1::Map<Type>,
+    global_types: ftd_p1::Map<Qualified<Type>>,
     /// js_buffer contains the generated JS when we resolve any symbol
     js_buffer: String,
 }
 
+#[derive(Debug)]
 enum ContinuableThing {
     RI(RI),
     CI(CI),
@@ -25,26 +26,30 @@ enum ContinuableThing {
 }
 
 impl ContinuableThing {
-    fn from_component_invocation(c: ftd_ast::ComponentInvocation) -> Self {
+    fn from_component_invocation(c: ftd_ast::ComponentInvocation, document_id: DocumentID) -> Self {
         ContinuableThing::CI(CI {
             inner: c,
             local_types: ftd_p1::Map::new(),
             js_buffer: String::new(),
             to_resolve: vec![ComponentResolvable::Name],
+            document_id,
         })
     }
 }
 
+#[derive(Debug)]
 struct FI {
     //
 }
 
+#[derive(Debug)]
 struct RI {
     pub inner: ftd_ast::VariableDefinition,
     pub r: Record,
     pub current_field: i32,
 }
 
+#[derive(Debug)]
 enum ComponentResolvable {
     Name,
     Id,
@@ -56,11 +61,13 @@ enum ComponentResolvable {
     Child(i32),
 }
 
+#[derive(Debug)]
 struct CI {
     inner: ftd_ast::ComponentInvocation,
     to_resolve: Vec<ComponentResolvable>,
     local_types: ftd_p1::Map<Type>,
     js_buffer: String,
+    document_id: DocumentID,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -72,7 +79,12 @@ pub enum Error {
 type Result<T> = std::result::Result<T, Error>;
 
 impl State {
-    fn merge_ast(&mut self, extend_continuable_things: bool, ast: Vec<ftd_ast::Ast>, doc_id: &str) {
+    fn merge_ast(
+        &mut self,
+        extend_continuable_things: bool,
+        ast: Vec<ftd_ast::Ast>,
+        doc_id: DocumentID,
+    ) {
         for ast in ast {
             match ast {
                 ftd_ast::Ast::Import(_)
@@ -82,21 +94,31 @@ impl State {
                 | ftd_ast::Ast::ComponentDefinition(_)
                 | ftd_ast::Ast::FunctionDefinition(_)
                 | ftd_ast::Ast::WebComponentDefinition(_) => {
-                    self.symbols.insert(format!("{doc_id}#{}", ast.name()), ast);
+                    self.symbols.insert(
+                        format!("{}#{}", doc_id.logical, ast.name()),
+                        Lined {
+                            line_number: ast.line_number(),
+                            v: ast,
+                            doc_id: doc_id.clone(),
+                        },
+                    );
                 }
                 ftd_ast::Ast::VariableInvocation(_) => unreachable!(),
                 ftd_ast::Ast::ComponentInvocation(c) => {
                     if extend_continuable_things {
                         self.continuable_things
-                            .push(ContinuableThing::from_component_invocation(c.clone()))
+                            .push(ContinuableThing::from_component_invocation(
+                                c.clone(),
+                                doc_id.clone(),
+                            ))
                     }
                 }
             }
         }
     }
 
-    pub fn from_document(source: &str, doc_id: &str) -> ftd_tc::Result<Self> {
-        let ast = parse_document_to_ast(source, doc_id)?;
+    pub fn from_document(source: &str, doc_id: DocumentID) -> ftd_tc::Result<Self> {
+        let ast = parse_document_to_ast(source, &doc_id)?;
 
         let mut s = Self::default();
         s.merge_ast(false, ast, doc_id);
@@ -117,16 +139,22 @@ impl State {
                 // component definition. If the module is also not yet loaded we return
                 // module name to load.
                 match self.global_types.get(&c.inner.name) {
-                    Some(Type::Component(c)) => {
+                    Some(Qualified {
+                        v: Type::Component(c),
+                        ..
+                    }) => {
                         todo!()
                     }
-                    Some(t) => todo!(),
+                    Some(t) => todo!("syntax error, foo is not a component"),
                     None => match self.symbols.get(&c.inner.name) {
-                        Some(ftd_ast::Ast::ComponentDefinition(c)) => {
+                        Some(Lined {
+                            v: ftd_ast::Ast::ComponentDefinition(c),
+                            ..
+                        }) => {
                             todo!()
                         }
                         Some(t) => {
-                            todo!()
+                            todo!("syntax error, foo is not a component")
                         }
                         None => todo!(),
                     },
@@ -163,7 +191,7 @@ impl State {
         }
     }
 
-    fn start(mut self) -> ftd_tc::Result<TCState> {
+    fn r#continue(mut self) -> ftd_tc::Result<TCState> {
         while let Some(mut thing) = self.continuable_things.pop() {
             if let Some(document) = self.handle_thing(&mut thing)? {
                 self.continuable_things.push(thing);
@@ -177,51 +205,106 @@ impl State {
         Ok(TCState::Done(self))
     }
 
-    fn continue_after_import(mut self, doc_id: &str, source: &str) -> ftd_tc::Result<TCState> {
-        let ast = parse_document_to_ast(source, doc_id)?;
+    fn continue_after_import(
+        mut self,
+        doc_id: DocumentID,
+        source: &str,
+    ) -> ftd_tc::Result<TCState> {
+        let ast = parse_document_to_ast(source, &doc_id)?;
         self.merge_ast(false, ast, doc_id);
-        self.start()
+        self.r#continue()
     }
 }
 
+#[derive(Debug)]
 enum TCState {
     Processing(State),
     StuckOnImport { document: String, state: State },
     Done(State),
 }
 
+#[derive(Debug)]
 enum Type {
-    Integer(VarMeta),
-    MutableInteger(VarMeta),
+    Integer,
+    MutableInteger,
     Record(Record),
     Component(Component),
 }
 
-struct VarMeta {
-    /// if the field has a default value, we can skip passing this field in the invocation
-    has_default: bool,
-    /// we can show the precise line number where the field is defined
-    line: usize,
+#[derive(Debug)]
+enum AccessibleIn {
+    /// accessible in the same document
+    Module(DocumentID),
+    /// accessible in the same package
+    Package(DocumentID),
+    /// accessible to anyone who adds this package as a direct dependency
+    Public,
+}
+
+#[derive(Debug)]
+struct Qualified<T> {
+    v: T,
+    line_number: usize,
+    doc_id: DocumentID,
+    accessible_in: AccessibleIn,
+}
+
+#[derive(Debug)]
+struct Lined<T> {
+    v: T,
+    line_number: usize,
+    doc_id: DocumentID,
+}
+
+#[derive(Debug, Clone)]
+pub struct DocumentID {
+    /// logical id is what we use to refer to a document in the code, eg `amitu.com/foo`
+    logical: String,
+    /// physical id is the file name, eg `.packages/amitu.com/foo/index.ftd`
+    physical: String,
+}
+
+impl DocumentID {
+    pub fn new(logical: &str, physical: &str) -> Self {
+        Self {
+            logical: logical.to_string(),
+            physical: physical.to_string(),
+        }
+    }
+
+    pub fn new0(logical: &str) -> Self {
+        Self {
+            logical: logical.to_string(),
+            physical: logical.to_string(),
+        }
+    }
 }
 
 /// we use field to model component arguments, record fields, and function arguments etc
+#[derive(Debug)]
 struct Field {
     name: String,
     type_: Type,
-    meta: VarMeta,
+    /// if the field has a default value, we can skip passing this field in the invocation
+    has_default: bool,
 }
 
+#[derive(Debug)]
 struct Component {
     args: Vec<Field>,
 }
 
+#[derive(Debug)]
 struct Record {
     fields: Vec<Field>,
 }
 
-pub fn parse_document_to_ast(source: &str, doc_id: &str) -> ftd_ast::Result<Vec<ftd_ast::Ast>> {
-    let sections = ftd_p1::parse(source, doc_id)?;
-    let ast = ftd_ast::Ast::from_sections(sections.as_slice(), doc_id)?;
+pub fn parse_document_to_ast(
+    source: &str,
+    doc_id: &DocumentID,
+) -> ftd_ast::Result<Vec<ftd_ast::Ast>> {
+    let sections = ftd_p1::parse(source, doc_id.logical.as_str())?;
+    let ast = ftd_ast::Ast::from_sections(sections.as_slice(), doc_id.logical.as_str())?;
     println!("{:?}", ast);
 
     Ok(ast)
--- ftd-tc/src/main.rs
+++ ftd-tc/src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    ftd_tc::parse_document_to_ast("-- import: foo", "foo").unwrap();
+    ftd_tc::parse_document_to_ast("-- import: foo", &ftd_tc::DocumentID::new0("foo")).unwrap();
 }
