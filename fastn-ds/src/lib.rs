3efa52db9ed9ce8cc11afb23bcb1e83afc71bfdc
Amit Upadhyay <upadhyay@gmail.com>
2024-04-07 17:47:05 +0530

    centralizing create-pool, so we can migrate

--- fastn-ds/src/lib.rs
+++ fastn-ds/src/lib.rs
@@ -1,12 +1,13 @@
 extern crate self as fastn_ds;
 
-use deadpool_postgres::tokio_postgres;
-
+mod create_pool;
 pub mod http;
 pub mod mail;
 pub mod reqwest_util;
 mod utils;
 pub mod wasm;
+
+pub use create_pool::create_pool;
 
 #[derive(Debug, Clone)]
 pub struct DocumentStore {
@@ -173,100 +174,6 @@ pub static WASM_ENGINE: once_cell::sync::Lazy<wasmtime::Engine> =
         wasmtime::Engine::new(wasmtime::Config::new().async_support(true)).unwrap()
     });
 
-// async fn create_pool(
-//     req_config: &fastn_core::RequestConfig,
-// ) -> Result<deadpool_postgres::Pool, deadpool_postgres::CreatePoolError> {
-//     let mut cfg = deadpool_postgres::Config::new();
-//     cfg.libpq_style_connection_string = match req_config.config.ds.env("FASTN_DB_URL").await {
-//         Ok(v) => Some(v),
-//         Err(_) => {
-//             fastn_core::warning!("FASTN_DB_URL is not set");
-//             return Err(deadpool_postgres::CreatePoolError::Config(
-//                 deadpool_postgres::ConfigError::ConnectionStringInvalid,
-//             ));
-//         }
-//     };
-//     cfg.manager = Some(deadpool_postgres::ManagerConfig {
-//         // TODO: make this configurable
-//         recycling_method: deadpool_postgres::RecyclingMethod::Verified,
-//     });
-//     let runtime = Some(deadpool_postgres::Runtime::Tokio1);
-//
-//     if let Ok(true) = req_config
-//         .config
-//         .ds
-//         .env_bool("FASTN_PG_DANGER_ENABLE_SSL", false)
-//         .await
-//     {
-//         fastn_core::warning!(
-//             "FASTN_PG_DANGER_DISABLE_SSL is set to false, this is not recommended for production use",
-//         );
-//         cfg.ssl_mode = Some(deadpool_postgres::SslMode::Disable);
-//         return cfg.create_pool(runtime, tokio_postgres::NoTls);
-//     }
-//
-//     let mut connector = native_tls::TlsConnector::builder();
-//
-//     match req_config
-//         .config
-//         .ds
-//         .env("FASTN_PG_SSL_MODE")
-//         .await
-//         .as_deref()
-//     {
-//         Err(_) | Ok("require") => {
-//             cfg.ssl_mode = Some(deadpool_postgres::SslMode::Require);
-//         }
-//         Ok("prefer") => {
-//             fastn_core::warning!(
-//                 "FASTN_PG_SSL_MODE is set to prefer, which roughly means \"I don't care about \
-//                 encryption, but I wish to pay the overhead of encryption if the server supports it.\"\
-//                 and is not recommended for production use",
-//             );
-//             cfg.ssl_mode = Some(deadpool_postgres::SslMode::Prefer);
-//         }
-//         Ok(v) => {
-//             // TODO: openssl also allows `verify-ca` and `verify-full` but native_tls does not
-//             fastn_core::warning!(
-//                 "FASTN_PG_SSL_MODE is set to {}, which is invalid, only allowed values are prefer and require",
-//                 v,
-//             );
-//             return Err(deadpool_postgres::CreatePoolError::Config(
-//                 deadpool_postgres::ConfigError::ConnectionStringInvalid,
-//             ));
-//         }
-//     }
-//
-//     if let Ok(true) = req_config
-//         .config
-//         .ds
-//         .env_bool("FASTN_PG_DANGER_ALLOW_UNVERIFIED_CERTIFICATE", false)
-//         .await
-//     {
-//         fastn_core::warning!(
-//             "FASTN_PG_DANGER_ALLOW_UNVERIFIED_CERTIFICATE is set to true, this is not \
-//             recommended for production use",
-//         );
-//         connector.danger_accept_invalid_certs(true);
-//     }
-//
-//     if let Ok(cert) = req_config.config.ds.env("FASTN_PG_CERTIFICATE").await {
-//         // TODO: This does not work with Heroku certificate.
-//         let cert = req_config
-//             .config
-//             .ds
-//             .read_content(&fastn_ds::Path::new(cert))
-//             .await
-//             .unwrap();
-//         // TODO: We should allow DER formatted certificates too, maybe based on file extension?
-//         let cert = native_tls::Certificate::from_pem(&cert).unwrap();
-//         connector.add_root_certificate(cert);
-//     }
-//
-//     let tls = postgres_native_tls::MakeTlsConnector::new(connector.build().unwrap());
-//     cfg.create_pool(runtime, tls)
-// }
-
 #[derive(thiserror::Error, Debug)]
 pub enum CreatePoolError {
     #[error("pool error {0}")]
@@ -296,14 +203,7 @@ impl DocumentStore {
             return Ok(p.clone());
         }
 
-        let pool = deadpool_postgres::Config {
-            url: Some(db_url.to_string()),
-            ..Default::default()
-        }
-        .create_pool(
-            Some(deadpool_postgres::Runtime::Tokio1),
-            tokio_postgres::NoTls,
-        )?; // TODO: use the older create_pool function
+        let pool = fastn_ds::create_pool(db_url.as_str(), true).await?;
         self.pg_pools.insert(db_url.to_string(), pool.clone());
         Ok(pool)
     }
--- fastn-ds/src/wasm/exports/pg/connect.rs
+++ fastn-ds/src/wasm/exports/pg/connect.rs
@@ -9,32 +9,25 @@ pub async fn connect(
 
 impl fastn_ds::wasm::Store {
     pub async fn pg_connect(&mut self, db_url: &str) -> wasmtime::Result<i32> {
-        let db_url = if db_url == "default" {
+        let r_db_url = if db_url == "default" {
             self.db_url.as_str()
         } else {
             db_url
         };
 
-        let pool = match self.wasm_pg_pools.get(db_url) {
+        let pool = match self.wasm_pg_pools.get(r_db_url) {
             Some(pool) => pool,
             None => {
-                let pool = deadpool_postgres::Config {
-                    url: Some(db_url.to_string()),
-                    ..Default::default()
-                }
-                .create_pool(
-                    Some(deadpool_postgres::Runtime::Tokio1),
-                    tokio_postgres::NoTls,
-                )?;
+                let pool = fastn_ds::create_pool(r_db_url, r_db_url == db_url).await?;
                 self.wasm_pg_pools.insert(db_url.to_string(), pool);
                 self.wasm_pg_pools.get(db_url).unwrap() // expect to be there
             }
         };
 
+        let client = pool.get().await?;
+
         let mut clients = self.clients.lock().await;
-        clients.push(fastn_ds::wasm::Conn {
-            client: pool.get().await?,
-        });
+        clients.push(fastn_ds::wasm::Conn { client });
         Ok(clients.len() as i32 - 1)
     }
 }
