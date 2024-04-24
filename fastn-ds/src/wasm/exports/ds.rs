c299e2b4fdafeb1b47ff30f2eac8ff9f031e52cc
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-04-24 17:51:43 +0530

    Move delete_document to dotcom

--- fastn-ds/src/wasm/exports/ds.rs
+++ fastn-ds/src/wasm/exports/ds.rs
@@ -14,6 +14,14 @@ pub async fn get_all_document(
     unimplemented!()
 }
 
+pub async fn tejar_write(
+    _caller: wasmtime::Caller<'_, fastn_ds::wasm::Store>,
+    _ptr: i32,
+    _len: i32,
+) -> wasmtime::Result<i32> {
+    unimplemented!()
+}
+
 pub async fn delete_file(
     _caller: wasmtime::Caller<'_, fastn_ds::wasm::Store>,
     _ptr: i32,
