d131886cbe1b59e20caa710b5db1df735fccd1a0
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-04-26 11:50:50 +0530

    Added get_all_documents

--- fastn-ds/src/wasm/exports/ds.rs
+++ fastn-ds/src/wasm/exports/ds.rs
@@ -1,11 +1,3 @@
-pub async fn get_all_document(
-    _caller: wasmtime::Caller<'_, fastn_ds::wasm::Store>,
-    _ptr: i32,
-    _len: i32,
-) -> wasmtime::Result<i32> {
-    unimplemented!()
-}
-
 pub async fn tejar_write(
     _caller: wasmtime::Caller<'_, fastn_ds::wasm::Store>,
     _ptr: i32,
