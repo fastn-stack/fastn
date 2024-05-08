2f95acb81d9d7cd2d1d0121cac878a28ee5e9f9d
Amit Upadhyay <upadhyay@gmail.com>
2024-05-08 14:21:04 +0530

    using http from dotcom

--- fastn-js/js/ftd.js
+++ fastn-js/js/ftd.js
@@ -180,100 +180,48 @@ const ftd = (function () {
         list.set(value);
     };
 
-    exports.http = function (url, opts, ...body) {
-        if ((!opts) instanceof fastn.recordInstanceClass) {
-            console.info(`opts must be a record instance of
-                -- record ftd.http-options:
-                string method: GET
-                string redirect: manual
-                string fastn-module:
-            `);
-            throw new Error("invalid opts");
-        }
-
-        let method = opts.get("method").get();
-        let fastn_module = opts.get("fastn_module").get();
-        let redirect = opts.get("redirect").get();
-
-        if (!["manual", "follow", "error"].includes(redirect)) {
-            throw new Error(
-                `redirect must be one of "manual", "follow", "error"`,
-            );
-        }
-
-        // change ftd.http-method and this function to add support for more
-        // http methods
-        if (!["GET", "POST"].includes(method)) {
-            throw new Error(
-                `${method} is invalid. Must be one of "GET", "POST"`,
-            );
-        }
-
+    exports.http = function (url, method, headers, ...body) {
         if (url instanceof fastn.mutableClass) url = url.get();
+        if (method instanceof fastn.mutableClass) method = method.get();
         method = method.trim().toUpperCase();
-        let request_json = {};
-
         const init = {
             method,
-            headers: { "Content-Type": "application/json" },
-            json: null,
-            redirect,
+            headers: {}
         };
-
-        if (body) {
-            if (body[0] instanceof fastn.recordInstanceClass) {
-                if (body.length !== 1) {
-                    console.warn(
-                        "body is a record instance, but has more than 1 element, ignoring",
-                    );
-                }
-                request_json = body[0].toObject();
-            } else {
-                let json = body[0];
-                if (
-                    body.length !== 1 ||
-                    (body[0].length === 2 && Array.isArray(body[0]))
-                ) {
-                    let new_json = {};
-                    // @ts-ignore
-                    for (let [header, value] of Object.entries(body)) {
-                        let [key, val] =
-                            value.length === 2 ? value : [header, value];
-                        new_json[key] = fastn_utils.getStaticValue(val);
-                    }
-                    json = new_json;
-                }
-                request_json = json;
-            }
+        if (headers && headers instanceof fastn.recordInstanceClass) {
+            Object.assign(init.headers, headers.toObject());
         }
-
-        if (method === "POST") {
-            init.body = JSON.stringify(request_json);
+        if (method !== 'GET') {
+            init.headers['Content-Type'] = 'application/json';
         }
+        if (body && body instanceof fastn.recordInstanceClass && method !== 'GET') {
+            init.body = JSON.stringify(body.toObject());
+        } else if (body && method !== 'GET') {
+            let json = body[0];
+            if (body.length !== 1 || (body[0].length === 2 && Array.isArray(body[0]))) {
+                let new_json = {};
+                // @ts-ignore
+                for (let [header, value] of Object.entries(body)) {
+                    let [key, val] = value.length == 2 ? value : [header, value];
 
-        if (method === "GET") {
-            url = new URL(url);
-
-            for (let [key, value] of Object.entries(request_json)) {
-                url.searchParams.set(key, value);
+                    new_json[key] = fastn_utils.getFlattenStaticValue(val);
+                }
+                json = new_json;
             }
+            init.body = JSON.stringify(json);
         }
 
         let json;
-        fetch(url, init)
-            .then((res) => {
-                if (res.redirected) {
-                    window.location.href = res.url;
-                    return;
-                }
 
+        fetch(url, init)
+            .then(res => {
                 if (!res.ok) {
-                    return new Error("[http]: Request failed", res);
+                    return new Error("[http]: Request failed", res)
                 }
 
                 return res.json();
             })
-            .then((response) => {
+            .then(response => {
                 console.log("[http]: Response OK", response);
                 if (response.redirect) {
                     window.location.href = response.redirect;
@@ -287,26 +235,21 @@ const ftd = (function () {
                             if (Array.isArray(value)) {
                                 // django returns a list of strings
                                 value = value.join(" ");
+                                // also django does not append `-error`
+                                key = key + "-error";
                             }
-                            // also django does not append `-error`
-                            key = key + "-error";
-                            key = fastn_module + "#" + key;
+                            // @ts-ignore
                             data[key] = value;
                         }
                     }
                     if (!!response.data) {
                         if (Object.keys(data).length !== 0) {
-                            console.log(
-                                "both .errors and .data are present in response, ignoring .data",
-                            );
+                            console.log("both .errrors and .data are present in response, ignoring .data");
                         } else {
-                            for (let key of Object.keys(response.data)) {
-                                const value = response.data[key];
-                                key = fastn_module + "#" + key;
-                                data[key] = value;
-                            }
+                            data = response.data;
                         }
                     }
+                    console.log(response)
                     for (let ftd_variable of Object.keys(data)) {
                         // @ts-ignore
                         window.ftd.set_value(ftd_variable, data[ftd_variable]);
