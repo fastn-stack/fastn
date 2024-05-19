2a86b1ecc66466b46069b22584fac034a6ff0a15
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-05-19 14:18:49 +0530

    run dprint

--- fastn-js/js/ftd.js
+++ fastn-js/js/ftd.js
@@ -198,7 +198,7 @@ const ftd = (function () {
         method = method.trim().toUpperCase();
         const init = {
             method,
-            headers: {"Content-Type": "application/json"},
+            headers: { "Content-Type": "application/json" },
         };
         if (headers && headers instanceof fastn.recordInstanceClass) {
             Object.assign(init.headers, headers.toObject());
@@ -499,7 +499,7 @@ const ftd = (function () {
             method: "POST",
             redirect: "error",
             // TODO: set credentials?
-            headers: {"Content-Type": "application/json"},
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data),
         };
 
