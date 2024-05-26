757ecf6ddbb3f96ff141b18e7bb6603ff74a0e5c
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-05-26 11:39:28 +0530

    Fix: integration test fix

--- .github/scripts/test-server.py
+++ .github/scripts/test-server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-import psycopg2
+import sqlite3
 import os
 
 app = Flask(__name__)
@@ -7,23 +7,23 @@ app = Flask(__name__)
 
 def fetch_data():
     # Connect to the PostgreSQL database
-    connection = psycopg2.connect(os.environ["FASTN_DB_URL"])
+    connection = sqlite3.connect(get_database_path(os.environ["FASTN_DB_URL"]))
 
     try:
         # Create a cursor object to execute SQL queries
-        with connection.cursor() as cursor:
-            # Execute a query to fetch data from the 'test' table
-            cursor.execute("SELECT * FROM test;")
+        cursor = connection.cursor()
+        # Execute a query to fetch data from the 'test' table
+        cursor.execute("SELECT * FROM test;")
 
-            # Fetch first row from the result set
-            row = cursor.fetchone()
+        # Fetch first row from the result set
+        row = cursor.fetchone()
 
-            data = dict()
-            if row is not None:
-                data = {
-                    "id": row[0],
-                    "data": row[1],
-                }
+        data = dict()
+        if row is not None:
+            data = {
+                "id": row[0],
+                "data": row[1],
+            }
 
     finally:
         # Close the database connection
@@ -41,6 +41,13 @@ def get_data():
     json_result = jsonify(data)
     print(json_result)
     return json_result
+
+
+def get_database_path(uri):
+    prefix = 'sqlite:///'
+    if uri.startswith(prefix):
+        return uri[len(prefix):]
+    return uri
 
 
 if __name__ == '__main__':
