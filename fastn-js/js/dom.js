9e46e7fcf3dc1a5a8133a9954f395b7b05cea5b9
siddhantCodes <siddhantk232@gmail.com>
2024-07-09 12:56:37 +0530

    fix: render `srcdoc` attribute for `ftd.iframe`
    Providing a caption (`srcdoc`) to `ftd.iframe` renders a `src`
    attribute. This is fixed now

--- fastn-js/js/dom.js
+++ fastn-js/js/dom.js
@@ -313,6 +313,7 @@ fastn_dom.PropertyKind = {
     TextInputValue: 122,
     FetchPriority: 123,
     Download: 124,
+    SrcDoc: 125,
 };
 
 fastn_dom.Loading = {
@@ -2430,6 +2431,8 @@ class Node2 {
             this.attachAttribute("loading", staticValue);
         } else if (kind === fastn_dom.PropertyKind.Src) {
             this.attachAttribute("src", staticValue);
+        } else if (kind === fastn_dom.PropertyKind.SrcDoc) {
+            this.attachAttribute("srcdoc", staticValue);
         } else if (kind === fastn_dom.PropertyKind.ImageSrc) {
             this.attachImageSrcClosures(staticValue);
             ftd.dark_mode.addClosure(
