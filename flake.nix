fa1b620721bab22d7ecc9b345f7a818ac15d7059
siddhantCodes <siddhantk232@gmail.com>
2024-08-23 19:48:45 +0530

    Use same rust version in the toolchain with nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,40 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs) {
           inherit system;
 
-          overlays = [ ];
+          overlays = [ (import rust-overlay) ];
         };
+
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in
       rec {
         # nix develop
         devShell = pkgs.mkShell {
           name = "fastn-shell";
           nativeBuildInputs = with pkgs; [
-            rustc
-            rustfmt
-            clippy
-            cargo
+            toolchain
             pkg-config
             openssl.dev
-            postgresql_14
             diesel-cli
-            rust-analyzer
+            rust-analyzer-unwrapped
           ];
 
           shellHook = ''
             export PATH="$PATH:$HOME/.cargo/bin"
           '';
+
+          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
         };
 
         formatter = pkgs.nixpkgs-fmt;
