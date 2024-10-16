bca242ff6342b6687d95c77636a88df95c104412
Amit Upadhyay <upadhyay@gmail.com>
2024-10-16 13:02:35 +0530

    some helpers

--- v0.5/p1/src/parse.rs
+++ v0.5/p1/src/parse.rs
@@ -1,15 +1,29 @@
 #[derive(Default)]
 pub struct Engine {
     pub doc_name: String,
-    pub edits: Vec<String>,
+    pub edits: Vec<Edit>,
 }
 
-pub struct Edit<'a> {
+impl Engine {
+    pub fn new(doc_name: String) -> Self {
+        Self {
+            doc_name,
+            edits: vec![],
+        }
+    }
+
+    pub fn add_edit(&mut self, from: usize, to: usize, text: String) -> &Edit {
+        self.edits.push(Edit { from, to, text });
+        self.edits.last().unwrap()
+    }
+}
+
+pub struct Edit {
     pub from: usize,
     pub to: usize,
-    pub text: &'a str,
+    pub text: String,
 }
 
-pub fn parse_edit<'a>(_old: &mut fastn_p1::ParseOutput<'a>, _e: Edit<'a>) {
+pub fn parse_edit(_old: &mut fastn_p1::ParseOutput, _e: &Edit) {
     todo!()
 }
--- v0.5/p1/src/test/mod.rs
+++ v0.5/p1/src/test/mod.rs
@@ -19,15 +19,9 @@ fn test_all() {
 fn p1(file: impl AsRef<std::path::Path> + std::fmt::Debug, fix: bool) {
     let json = file.as_ref().with_extension("json");
     let s = std::fs::read_to_string(&file).unwrap();
-    let mut engine = fastn_p1::Engine::default();
-    engine.edits.push(s);
-    let s = engine.edits.last().unwrap();
+    let mut engine = fastn_p1::Engine::new("foo".to_string());
     let mut output = fastn_p1::ParseOutput::default();
-    let edit = fastn_p1::Edit {
-        from: 0,
-        to: s.len(),
-        text: s,
-    };
+    let edit = engine.add_edit(0, s.len(), s);
     fastn_p1::parse_edit(&mut output, edit);
     let expected_json =
         fastn_p1::test::sorted_json::to_json(&serde_json::to_value(&output).unwrap());
