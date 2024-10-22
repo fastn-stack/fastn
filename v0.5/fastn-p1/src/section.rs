95e72f0cf752f7c65149379648f86a9cd5e2f829
Amit Upadhyay <upadhyay@gmail.com>
2024-10-22 12:04:47 +0530

    one failed attempt to get data from tokens

--- v0.5/fastn-p1/src/section.rs
+++ v0.5/fastn-p1/src/section.rs
@@ -1,12 +1,14 @@
 impl fastn_p1::Section<'_> {
     pub fn with_name<'a>(_name: fastn_p1::Token) -> fastn_p1::Section<'a> {
+        use logos::Source;
+
         fastn_p1::Section {
             name: fastn_p1::KindedName {
                 kind: None,
                 name: fastn_p1::Sourced {
                     from: 0,
                     to: 0,
-                    value: "foo",
+                    value: _name.slice(),
                 },
             },
             ..Default::default()
