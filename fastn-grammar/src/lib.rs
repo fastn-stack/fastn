6ab1a10597cfcbe94f1113f3bcb92a44db791cea
Amit Upadhyay <upadhyay@gmail.com>
2024-11-06 19:29:25 +0530

    minor

--- fastn-grammar/src/lib.rs
+++ fastn-grammar/src/lib.rs
@@ -3,18 +3,3 @@
 extern crate self as fastn_grammar;
 
 pub mod evalexpr;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
