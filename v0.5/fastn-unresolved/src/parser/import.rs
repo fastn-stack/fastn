8d4f8e48d4d07b7f9038febdbdd763d9b05d30be
Amit Upadhyay <upadhyay@gmail.com>
2024-11-12 13:17:57 +0530

    started testing for import

--- v0.5/fastn-unresolved/src/parser/import.rs
+++ v0.5/fastn-unresolved/src/parser/import.rs
@@ -85,3 +85,36 @@ fn parse_exposing(
 ) {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    #[track_caller]
+    fn t1(source: &str, expected: serde_json::Value) {
+        let (mut document, sections) =
+            fastn_unresolved::Document::new(fastn_section::Document::parse(source));
+
+        let section = {
+            assert_eq!(sections.len(), 1);
+            sections.into_iter().next().unwrap()
+        };
+
+        super::import(source, section, &mut document);
+        assert_eq!(serde_json::to_value(document.imports).unwrap(), expected);
+    }
+
+    macro_rules! t {
+        ($source:expr, $debug:tt) => {
+            t1($source, serde_json::json!($debug));
+        };
+    }
+
+    #[test]
+    fn test_import() {
+        t!("-- import: foo", { "import": "foo" });
+        t!("-- import: foo.fifthtry.site/bar", { "import": "foo.fifthtry.site/bar" });
+        t!("-- import: foo as f", { "import": "foo=>f" });
+        t!("-- import: foo as f\nexposing: x", { "import": "foo=>f", "exposing": ["x"] });
+        t!("-- import: foo\nexposing: x", { "import": "foo", "exposing": ["x"] });
+        t!("-- import: foo as f\nexposing: x as y", { "import": "foo as f", "exposing": ["x=>y"] });
+    }
+}
