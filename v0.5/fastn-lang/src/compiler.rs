fe741c525522d51fae96078f4a75403ebc6fd8f5
Amit Upadhyay <upadhyay@gmail.com>
2024-11-15 13:03:36 +0530

    some more design sketch

--- v0.5/fastn-lang/src/compiler.rs
+++ v0.5/fastn-lang/src/compiler.rs
@@ -8,7 +8,7 @@
 /// earlier we had strict mode here, but to simplify things, now we let the caller convert non-empty
 /// warnings from OK part as error, and discard the generated JS.
 pub async fn compile<'input>(
-    _symbols: &mut Box<dyn fastn_lang::SymbolStore<'input>>,
+    symbols: &mut Box<dyn fastn_lang::SymbolStore<'input>>,
     document_id: &str,
     source: &str,
     _auto_imports: &[fastn_section::AutoImport],
@@ -18,25 +18,46 @@ pub async fn compile<'input>(
     let mut d = fastn_unresolved::parse(document_id, source);
     let mut bag = std::collections::HashMap::new();
 
+    // we only make 10 attempts to resolve the document
     for _ in 1..10 {
-        // we only make 10 attempts to resolve the document
-        let mut symbols = resolve_document(&mut d, &mut bag);
-        if symbols.is_empty() {
+        // resolve_document can internally run in parallel.
+        let mut unresolved_symbols = resolve_document(&mut d, &mut bag);
+        if unresolved_symbols.is_empty() {
             break;
         }
+
+        update_bag(symbols, &mut d, &mut bag, &mut unresolved_symbols).await;
         // this itself has to happen in a loop
         for _ in 1..10 {
-            // TODO: fetch symbols from
-            resolve_symbols(&mut d, &mut bag, &mut symbols);
-            if symbols.is_empty() {
+            // resolve_document can internally run in parallel.
+            resolve_symbols(&mut d, &mut bag, &mut unresolved_symbols);
+            if unresolved_symbols.is_empty() {
                 break;
             }
+            update_bag(symbols, &mut d, &mut bag, &mut unresolved_symbols).await;
+        }
+
+        if !unresolved_symbols.is_empty() {
+            // we were not able to resolve all symbols
         }
     }
 
     todo!()
 }
 
+async fn update_bag<'input>(
+    _symbols: &mut Box<dyn fastn_lang::SymbolStore<'input>>,
+    _d: &mut fastn_unresolved::Document,
+    _bag: &mut std::collections::HashMap<String, fastn_lang::LookupResult<'input>>,
+    _symbols_to_fetch: &mut [fastn_unresolved::SymbolName],
+) {
+    todo!()
+}
+
+/// try to resolve as many symbols as possible, and return the ones that could not be resolved
+/// because we do not have data in the bag.
+///
+/// this function should be called in a loop, until it returns an empty list of symbols.
 fn resolve_symbols(
     _d: &mut fastn_unresolved::Document,
     _bag: &mut std::collections::HashMap<String, fastn_lang::LookupResult>,
@@ -45,6 +66,10 @@ fn resolve_symbols(
     todo!()
 }
 
+/// try to make as much progress as possibly by resolving as many symbols as possible, and return
+/// the vec of ones that could not be resolved.
+///
+/// if this returns an empty list of symbols, we can go ahead and generate the JS.
 fn resolve_document(
     d: &mut fastn_unresolved::Document,
     _bag: &mut std::collections::HashMap<String, fastn_lang::LookupResult>,
@@ -64,7 +89,8 @@ pub struct Output {
     #[expect(unused)]
     warnings: Vec<fastn_section::Warning>,
     #[expect(unused)]
-    resolved: Vec<fastn_unresolved::Definition>,
+    resolved: Vec<fastn_type::Definition>,
+    // should we also return / cache partially resolved symbols?
 }
 
 pub struct Error {
@@ -76,8 +102,8 @@ pub struct Error {
     /// we are not returning vec string (dependencies here), because `Definition::dependencies()` is
     /// going to do that.
     #[expect(unused)]
-    resolved: Vec<fastn_unresolved::Definition>,
-    /// while parsing we found some symbols are wrong, e.g., say the document tried to use component `
+    resolved: Vec<fastn_type::Definition>,
+    /// while parsing, we found some symbols are wrong, e.g., say the document tried to use component `
     /// foo` but `foo` internally is calling `bar`, and there is no such component, and say `foo` is
     /// trying to use `baz` as a type, but there is no such type. Anyone else trying to use `foo`
     /// will also fail, so we store these errors here.
@@ -91,9 +117,9 @@ pub struct Error {
     ///
     /// -- end: foo
     ///
-    /// we will store x here. but what if x is actually a type alias to y, and it is y that is changing.
-    /// we have to make sure that we revalidate x when y changes. if we cant do this our entire
-    /// dependency tracking system is useless.
+    /// we will store x here. but what if x is actually a type alias to y, and it is y that is
+    /// changing. we have to make sure that we revalidate x when y changes. if we cant do this, our
+    /// entire dependency tracking system is useless.
     #[expect(unused)]
     symbol_errors: Vec<SymbolError>,
 }
