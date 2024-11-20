38a16798439adb3e9ffe11ab51ea1a39304b7c9a
Amit Upadhyay <upadhyay@gmail.com>
2024-11-20 13:57:01 +0530

    why its okay to use lock for builtins

--- v0.5/fastn-compiler/src/builtins.rs
+++ v0.5/fastn-compiler/src/builtins.rs
@@ -1,4 +1,12 @@
-// TODO: can we avoid the lock here?
+/// We are using a LazyLock, which[1]:
+///
+/// > This method will block the calling thread if another initialization routine is currently
+/// > running.
+///
+/// So the lock only blocks threads when the first time initialization is happening, so it is fine
+/// to use it in the global scope.
+///
+/// [1]: https://doc.rust-lang.org/beta/std/sync/struct.LazyLock.html#method.deref
 pub static BUILTINS: std::sync::LazyLock<
     std::collections::HashMap<string_interner::DefaultSymbol, fastn_type::Definition>,
 > = std::sync::LazyLock::new(builtins);
