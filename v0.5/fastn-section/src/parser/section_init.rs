126c752db02671118f475edb4da6e1564cf15877
siddhantCodes <siddhantk232@gmail.com>
2024-11-21 19:17:40 +0530

    Allow spaces between "(" and ")" in section_init parser

--- v0.5/fastn-section/src/parser/section_init.rs
+++ v0.5/fastn-section/src/parser/section_init.rs
@@ -7,9 +7,17 @@ pub fn section_init(
     scanner.skip_spaces();
     let name = fastn_section::parser::kinded_name(scanner)?;
     scanner.skip_spaces();
-    let function_marker = scanner.token("()");
+
+    let function_marker = scanner.token("(");
+
+    if function_marker.is_some() {
+        scanner.skip_spaces();
+        scanner.token(")")?;
+    }
+
     scanner.skip_spaces();
     let colon = scanner.token(":")?;
+
     Some(fastn_section::SectionInit {
         dashdash,
         name,
