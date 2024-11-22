73fff0ed4b8fff7d83757ff9fc2af683beb96a7e
Amit Upadhyay <upadhyay@gmail.com>
2024-11-22 17:01:34 +0530

    main.rs

---
+++ fastn-resolved-to-js/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let c = fastn_resolved::ComponentInvocation {
+        id: None,
+        name: "ftd#text".to_string(),
+        properties: vec![], // add hello-world caption etc.
+        iteration: Box::new(None),
+        condition: Box::new(None),
+        events: vec![],
+        children: vec![],
+        source: Default::default(),
+        line_number: 0,
+    };
+
+    // this main should create a HTML file, and store it in current folder as index.html etc.
+}
