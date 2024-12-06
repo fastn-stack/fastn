952ce4cfdaed34c32970c923bc839dc77fa79b87
Amit Upadhyay <upadhyay@gmail.com>
2024-12-06 23:44:38 +0530

    some thought on the package, systems, capabilities, apps etc

---
+++ v0.5/fastn-package/src/lib.rs
@@ -0,0 +1,55 @@
+#![allow(clippy::derive_partial_eq_without_eq, clippy::get_first)]
+#![deny(unused_crate_dependencies)]
+#![warn(clippy::used_underscore_binding)]
+#![allow(dead_code)]
+
+extern crate self as fastn_package;
+
+pub struct Package {
+    name: String,
+    systems: Vec<System>,
+    dependencies: Vec<Dependency>,
+    auto_imports: AutoImport,
+    apps: Vec<App>,
+}
+
+pub type AutoImport = Vec<(String, String)>;
+
+// -- system: design-system.com
+// via: amitu.com/ds
+// alias: some alias ;; if alias is not provided, this is globally passed
+pub struct System {
+    via: String,
+    sensitive: bool,
+    alias: Option<SystemAlias>,
+}
+
+pub struct SystemAlias(String);
+
+pub struct Dependency {
+    name: String,
+    // vector of alias of the systems this dependency and everything downstream
+    capabilities: Vec<SystemAlias>,
+    dependencies: Vec<Dependency>,
+    auto_imports: AutoImport,
+}
+
+// -- path: /blog/
+// allow: colorful-ds
+pub struct CapabilityOverride {
+    // capabilities for any url prefix can be overridden using this
+    path: String,
+    // if this is set, the global capabilities will be merged into .capabilities, else only
+    // .capabilities will be used.
+    inherit_global: bool,
+    capabilities: Vec<SystemAlias>,
+}
+
+pub struct App {
+    // this must already be added as a Dependency (not a system) and is its name
+    name: String,
+    mount_point: String,
+    apps: Vec<App>,
+    // Dependency.capabilities will be merged with this when serving these routes
+    capabilities: Vec<SystemAlias>,
+}
