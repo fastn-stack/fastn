b876594a98620ac6a6d45bda9f0dd9a42e4fb292
Amit Upadhyay <upadhyay@gmail.com>
2024-12-20 15:16:56 +0530

    fastn_router::Route::NotFound includes 404 document info

--- v0.5/fastn-router/src/lib.rs
+++ v0.5/fastn-router/src/lib.rs
@@ -20,7 +20,8 @@ pub enum Method {
 #[derive(Debug)]
 // the router will depend on fastn-section.
 pub enum Route {
-    NotFound,
+    /// not found tells you which ftd document to serve as not found page
+    NotFound(Document),
     // String contains the path, the data may contain more than that was passed to route, e.g., it
     // can extract some extra path-specific data from FASTN.ftd file
     Document(Document),
