e64e1c02450c3bd65a67858c264a666f82e41e8f
Arpita-Jaiswal <ayushipujaa@gmail.com>
2025-01-15 19:44:34 +0100

    Header tests added

--- v0.5/fastn-section/src/parser/headers.rs
+++ v0.5/fastn-section/src/parser/headers.rs
@@ -2,6 +2,7 @@ pub fn headers(
     scanner: &mut fastn_section::Scanner<fastn_section::Document>,
 ) -> Vec<fastn_section::Header> {
     let mut headers = vec![];
+    let start_index = scanner.index();
 
     loop {
         let index = scanner.index();
@@ -31,7 +32,14 @@ pub fn headers(
             condition: None,
             value,
             is_commented: false,
-        })
+        });
+
+        let new_line = scanner.token("\n");
+        if new_line.is_none() {
+            // TODO: throw error
+            scanner.reset(start_index);
+            break;
+        }
     }
 
     headers
@@ -59,5 +67,55 @@ pub fn header_kinded_name(
 impl From<fastn_section::Kind> for Option<fastn_section::Identifier> {
     fn from(value: fastn_section::Kind) -> Self {
         value.to_identifier()
+    }
+}
+
+impl fastn_section::JDebug for fastn_section::Header {
+    fn debug(&self) -> serde_json::Value {
+        let mut o = serde_json::Map::new();
+        if let Some(kind) = &self.kind {
+            o.insert("kind".into(), kind.debug());
+        }
+        o.insert("name".into(), self.name.debug());
+        if !self.value.0.is_empty() {
+            o.insert("value".into(), self.value.debug());
+        }
+        serde_json::Value::Object(o)
+    }
+}
+
+mod test {
+    fastn_section::tt!(super::headers);
+
+    #[test]
+    fn tes() {
+        t!("greeting: hello\n", [{"name": "greeting", "value": ["hello"]}]);
+        t!(
+            "greeting: hello\nwishes: Happy New Year\n",
+            [
+                {
+                    "name": "greeting",
+                    "value": ["hello"]
+                },
+                {
+                    "name": "wishes",
+                    "value": ["Happy New Year"]
+                }
+            ]
+        );
+        t!(
+            "greeting: hello\nwishes: Happy New Year\n\nI am not header",
+            [
+                {
+                    "name": "greeting",
+                    "value": ["hello"]
+                },
+                {
+                    "name": "wishes",
+                    "value": ["Happy New Year"]
+                }
+            ],
+            "\nI am not header"
+        );
     }
 }
