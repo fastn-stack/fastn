beb5243e62c6656e96014a64ff149df8608a7964
Ayush Deolasee <pypdeveloper@deolasee.org>
2025-07-01 11:55:30 +0530

    making print_help function public and allowed additional command inputs

--- fastn-xtask/src/lib.rs
+++ fastn-xtask/src/lib.rs
@@ -23,39 +23,45 @@ pub fn main() -> Result<(), String> {
         Some("update-www") => update_www::update_www().map_err(|e| e.to_string())?,
         Some("run-www") => run_www::run_www().map_err(|e| e.to_string())?,
         Some("update-template") => update_template::update_template().map_err(|e| e.to_string())?,
-        _ => print_help(),
+        _ => print_help(None),
     }
 
     Ok(())
 }
 
-fn print_help() {
-    eprintln!(
-        r#"fastn xtask CLI
-
-USAGE:
-    cargo xtask <COMMAND>
-
-COMMANDS:
-    build-wasm: Builds the WASM target from backend.
-
-    run-ui: Builds and serves the UI for the lets-XXX app, which is served on port 8002.
-
-    update-ui: Updates UI dependencies for the lets-XXX app, run this only when modifying dependencies in lets-XXX.fifthtry.site/FASTN.ftd or during the initial setup.
-
-    run-template: Runs the backend and tests end-to-end functionality of the lets-XXX app.
-
-    update-template: Updates dependencies for the lets-XXX app's backend template. Run this only when modifying dependencies or during the initial setup.
-
-    run-www: Serves and tests the public website for the lets-XXX app.
-
-    update-www: Updates dependencies for the lets-XXX app's public website. Run this only when modifying dependencies or during the initial setup.
-
-    optimise-wasm: Optimises the generated WASM binary.
-
-    publish-app: Publishes the lets-XXX app.
-
-    help: Prints this help message.
-"#
-    )
+pub fn print_help(additional_commands: Option<&[(&str, &str)]>) {
+    eprintln!("fastn xtask CLI");
+    eprintln!();
+    eprintln!("USAGE:");
+    eprintln!("    cargo xtask <COMMAND>");
+    eprintln!();
+    eprintln!("COMMANDS:");
+    
+    eprintln!("    build-wasm: Builds the WASM target from backend.");
+    eprintln!();
+    eprintln!("    run-ui: Builds and serves the UI for the lets-XXX app, which is served on port 8002.");
+    eprintln!();
+    eprintln!("    update-ui: Updates UI dependencies for the lets-XXX app, run this only when modifying dependencies in lets-XXX.fifthtry.site/FASTN.ftd or during the initial setup.");
+    eprintln!();
+    eprintln!("    run-template: Runs the backend and tests end-to-end functionality of the lets-XXX app.");
+    eprintln!();
+    eprintln!("    update-template: Updates dependencies for the lets-XXX app's backend template. Run this only when modifying dependencies or during the initial setup.");
+    eprintln!();
+    eprintln!("    run-www: Serves and tests the public website for the lets-XXX app.");
+    eprintln!();
+    eprintln!("    update-www: Updates dependencies for the lets-XXX app's public website. Run this only when modifying dependencies or during the initial setup.");
+    eprintln!();
+    eprintln!("    optimise-wasm: Optimises the generated WASM binary.");
+    eprintln!();
+    eprintln!("    publish-app: Publishes the lets-XXX app.");
+    eprintln!();
+    
+    if let Some(additional) = additional_commands {
+        for (command, description) in additional {
+            eprintln!("    {}: {}", command, description);
+            eprintln!();
+        }
+    }
+    
+    eprintln!("    help: Prints this help message.");
 }
